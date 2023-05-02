cfeeb25035e331f3982785049f885e18bd7dd380
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-02 14:36:52 +0200

    chore(color & storybook): moved color package and inited storybook (#1)
    * chore(color): added color package from sdds repo
    
    * chore(package.json): created and updated package.json for @scania/tegel
    
    * docs(storybook): added story files for color package, removed readme
    
    * docs(storybook): installed and addded storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+let addons = [
+  "@storybook/addon-links",
+  "@storybook/addon-essentials",
+  "storybook-dark-mode",
+  "@storybook/addon-notes/register",
+];
+
+if (process.env.STORYBOOK_ENV === "development") {
+  addons = [
+    ...addons,
+    "storybook-addon-designs",
+    "@storybook/addon-a11y",
+    "addon-screen-reader",
+  ];
+}
+
+module.exports = {
+  stories: ["../color/stories/*.stories.@(js|jsx|ts|tsx)"],
+
+  framework: "@storybook/html",
+  staticDirs: ["../dist", "../public"],
+};
