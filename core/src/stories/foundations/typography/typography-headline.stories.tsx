f9a4c89233d8050c04a9ef3715372a42665c8ba2
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-09 12:04:14 +0200

    feat(typography): prefix change (#29)
    * fix(typography): changed prefix to tds
    
    changed prefix to tds
    
    BREAKING CHANGE: prefix was changed to tds instead of sdds
    
    * docs(typography): updated storybook after typography prefix change

--- core/src/stories/foundations/typography/typography-headline.stories.tsx
+++ core/src/stories/foundations/typography/typography-headline.stories.tsx
@@ -1,18 +1,18 @@
-import { formatHtmlPreview } from "../../../utils/utils";
+import { formatHtmlPreview } from '../../../utils/utils';
 
 export default {
-  title: "Foundations/Typography",
+  title: 'Foundations/Typography',
   parameters: {
-    layout: "fullscreen",
+    layout: 'fullscreen',
   },
   argTypes: {
     type: {
-      name: "Type",
-      description: "The type of headline. Functional/Expressive.",
+      name: 'Type',
+      description: 'The type of headline. Functional/Expressive.',
       control: {
-        type: "radio",
+        type: 'radio',
       },
-      options: { Functional: "functional", Expressive: "expressive" },
+      options: { Functional: 'functional', Expressive: 'expressive' },
     },
   },
 };
@@ -28,32 +28,28 @@ const Template = ({ type }) =>
     </style>
 
     <div class="demo-wrapper">
-        <h1 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
-        }headline-01">Headline</h1>
-        <h2 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
-        }headline-02">Headline</h2>
-        <h3 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
+        <h1 class="tds-${type === 'expressive' ? `${type}-` : ''}headline-01">Headline</h1>
+        <h2 class="tds-${type === 'expressive' ? `${type}-` : ''}headline-02">Headline</h2>
+        <h3 class="tds-${
+          type === 'expressive' ? `${type}-` : ''
         }headline-03">A headline for page layouts</h3>
-        <h4 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
+        <h4 class="tds-${
+          type === 'expressive' ? `${type}-` : ''
         }headline-04">A headline for page layouts</h4>
-        <h5 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
+        <h5 class="tds-${
+          type === 'expressive' ? `${type}-` : ''
         }headline-05">A sub headline, which is most commonly paired with body-01</h5>
-        <h6 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
+        <h6 class="tds-${
+          type === 'expressive' ? `${type}-` : ''
         }headline-06">A sub headline, which is most commonly paired with body-02</h6>
-        <h7 class="sdds-${
-          type === "expressive" ? `${type}-` : ""
+        <h7 class="tds-${
+          type === 'expressive' ? `${type}-` : ''
         }headline-07">A sub headline, which is most commonly paired with detail-02 </h7>
     </div>
-  `
+  `,
   );
 
 export const Headlines = Template.bind({});
 Headlines.args = {
-  type: "functional",
+  type: 'functional',
 };
