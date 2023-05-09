ae69290e4d8a1db80823919461f1d8de2c627a3f
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-09 12:04:14 +0200

    feat(typography): prefix change (#29)
    * fix(typography): changed prefix to tds
    
    changed prefix to tds
    
    BREAKING CHANGE: prefix was changed to tds instead of sdds
    
    * docs(typography): updated storybook after typography prefix change

--- core/src/stories/foundations/typography/typography-paragraph.stories.tsx
+++ core/src/stories/foundations/typography/typography-paragraph.stories.tsx
@@ -1,18 +1,18 @@
-import { formatHtmlPreview } from "../../../utils/utils";
+import { formatHtmlPreview } from '../../../utils/utils';
 
 export default {
-  title: "Foundations/Typography",
+  title: 'Foundations/Typography',
   parameters: {
-    layout: "fullscreen",
+    layout: 'fullscreen',
   },
 };
 
 const Template = () =>
   formatHtmlPreview(
     `
-        <p class='sdds-paragraph-01'>Our large paragraph, which should be used for introduction texts on page layouts.</p>
-        <p class='sdds-paragraph-02'>Our smaller paragraph, mainly present on mobile breakpoints, which should be used for introduction texts on page layouts.</p>
-  `
+        <p class='tds-paragraph-01'>Our large paragraph, which should be used for introduction texts on page layouts.</p>
+        <p class='tds-paragraph-02'>Our smaller paragraph, mainly present on mobile breakpoints, which should be used for introduction texts on page layouts.</p>
+  `,
   );
 
 export const Paragraph = Template.bind({});
