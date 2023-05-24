a6bbd4c5dde43539ae8be0c181189af52949e544
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-24 08:48:49 +0200

    removed unused class
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

--- core/src/components/popover-menu/popover-menu.stories.tsx
+++ core/src/components/popover-menu/popover-menu.stories.tsx
@@ -131,7 +131,7 @@ const Template = ({ menuPosition, icons }) => {
       <span class="tds-u-mr2">Click icon for Popover Menu</span>
       
       <sdds-button aria-label="menu" onlyIcon id="my-popover-button" type="ghost" size="sm">
-        <tds-icon slot="icon" class="sdds-btn-icon" size="16px" name="kebab"></tds-icon>
+        <tds-icon slot="icon" size="16px" name="kebab"></tds-icon>
       </sdds-button>
     </div>
 
