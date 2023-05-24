1b4aac9a62e4f06ce9b7c01762105cf87bbffe74
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2023-05-24 14:22:55 +0200

    chore: move storybook to core folder (#99)
    * chore: move storybook to core folder
    
    * fix: prettier missing

--- core/.storybook/preview.js
+++ core/.storybook/preview.js
@@ -1,74 +1,74 @@
-import React from "react";
-import { DocsContainer } from "@storybook/addon-docs";
-import { defineCustomElements } from "../core/loader";
-import { useDarkMode } from "storybook-dark-mode";
+import React from 'react';
+import { DocsContainer } from '@storybook/addon-docs';
+import { defineCustomElements } from '../loader';
+import { useDarkMode } from 'storybook-dark-mode';
 
-import { addons } from "@storybook/addons";
-import { UPDATE_GLOBALS } from "@storybook/core-events";
-import "./preview.css";
-import "../core/dist/tegel/tegel.css";
+import { addons } from '@storybook/addons';
+import { UPDATE_GLOBALS } from '@storybook/core-events';
+import './preview.css';
+import '../dist/tegel/tegel.css';
 
 // https://github.com/storybookjs/storybook/issues/6364
 // - Look for: sarangk-hotstar commented on 23 Jun 2021
 // - Look for: Cochonours commented on 14 May
 // Test below one in Netlify/Amplify build
-import ScaniaThemeDark from "./ScaniaThemeDark";
-import ScaniaThemeLight from "./ScaniaThemeLight";
+import ScaniaThemeDark from './ScaniaThemeDark';
+import ScaniaThemeLight from './ScaniaThemeLight';
 
 const customViewports = {
   xs: {
-    name: "grid-xs",
+    name: 'grid-xs',
     styles: {
-      width: "607px",
-      height: "963px",
+      width: '607px',
+      height: '963px',
     },
   },
   sm: {
-    name: "grid-sm",
+    name: 'grid-sm',
     styles: {
-      width: "608px",
-      height: "963px",
+      width: '608px',
+      height: '963px',
     },
   },
   md: {
-    name: "grid-md",
+    name: 'grid-md',
     styles: {
-      width: "800px",
-      height: "801px",
+      width: '800px',
+      height: '801px',
     },
   },
   lg: {
-    name: "grid-lg",
+    name: 'grid-lg',
     styles: {
-      width: "992px",
-      height: "1200px",
+      width: '992px',
+      height: '1200px',
     },
   },
   xlg: {
-    name: "grid-xlg",
+    name: 'grid-xlg',
     styles: {
-      width: "1184px",
-      height: "1400px",
+      width: '1184px',
+      height: '1400px',
     },
   },
   xxlg: {
-    name: "grid-xxlg",
+    name: 'grid-xxlg',
     styles: {
-      width: "1367px",
-      height: "1600px",
+      width: '1367px',
+      height: '1600px',
     },
   },
   max: {
-    name: "grid-max",
+    name: 'grid-max',
     styles: {
-      width: "1584px",
-      height: "1680px",
+      width: '1584px',
+      height: '1680px',
     },
   },
 };
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -91,56 +91,56 @@ export const parameters = {
       return React.createElement(DocsContainer, props);
     },
     source: {
-      state: "open",
+      state: 'open',
     },
   },
   // storybook-dark-mode plugin
   darkMode: {
-    current: "light",
+    current: 'light',
     // Override the default dark theme
     dark: ScaniaThemeDark,
     // Override the default light theme
     light: ScaniaThemeLight,
-    darkClass: "sdds-mode-dark",
-    lightClass: "sdds-mode-light",
+    darkClass: 'sdds-mode-dark',
+    lightClass: 'sdds-mode-light',
     stylePreview: true,
   },
   backgrounds: {
-    default: "white",
+    default: 'white',
     values: [
       {
-        name: "grey-50",
-        value: "#F9FAFB",
+        name: 'grey-50',
+        value: '#F9FAFB',
       },
       {
-        name: "white",
-        value: "#FFFFFF",
+        name: 'white',
+        value: '#FFFFFF',
       },
       {
-        name: "grey-900",
-        value: "#1d2229",
+        name: 'grey-900',
+        value: '#1d2229',
       },
       {
-        name: "grey-958",
-        value: "#0d0f13",
+        name: 'grey-958',
+        value: '#0d0f13',
       },
     ],
   },
   options: {
     storySort: {
-      method: "alphabetical",
+      method: 'alphabetical',
       order: [
-        "Intro",
-        ["Tegel", "Migrating from components v4", "Installation", "Events"],
-        "Foundations",
-        "Utilities",
-        "Patterns",
-        "Components",
+        'Intro',
+        ['Tegel', 'Migrating from components v4', 'Installation', 'Events'],
+        'Foundations',
+        'Utilities',
+        'Patterns',
+        'Components',
       ],
     },
     enableShortcuts: false,
   },
-  layout: "padded",
+  layout: 'padded',
 };
 
 // Below is some hacky code that changes selected background when the theme changes
@@ -151,20 +151,17 @@ export const parameters = {
 if (!window.SDDS_DID_SUBSCRIBE_DARK_BG) {
   window.SDDS_DID_SUBSCRIBE_DARK_BG = true;
   const darkModeBgColor = parameters.backgrounds.values.find(
-    ({ name }) => name === "grey-958"
+    ({ name }) => name === 'grey-958',
   ).value;
   const lightModeBgColor = parameters.backgrounds.values.find(
-    ({ name }) => name === "grey-50"
+    ({ name }) => name === 'grey-50',
   ).value;
   const channel = addons.getChannel();
   channel.emit(UPDATE_GLOBALS, {
     globals: { backgrounds: { value: lightModeBgColor } },
   });
-  channel.on("DARK_MODE", (isDarkMode) => {
-    if (
-      (isDarkMode && !window.SDDS_DARK_BG) ||
-      (!isDarkMode && window.SDDS_DARK_BG)
-    ) {
+  channel.on('DARK_MODE', (isDarkMode) => {
+    if ((isDarkMode && !window.SDDS_DARK_BG) || (!isDarkMode && window.SDDS_DARK_BG)) {
       channel.emit(UPDATE_GLOBALS, {
         globals: {
           backgrounds: {
