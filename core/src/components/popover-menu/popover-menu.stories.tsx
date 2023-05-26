1eec0e98b6c08cdb8d30519d49ea2718c049a2a5
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-26 16:24:32 +0200

    fix(prefix): cleanup on the prefix change, some stories that needed updated prefixes for button (#118)

--- core/src/components/popover-menu/popover-menu.stories.tsx
+++ core/src/components/popover-menu/popover-menu.stories.tsx
@@ -130,9 +130,9 @@ const Template = ({ menuPosition, icons }) => {
     <div class="demo-wrapper">
       <span class="tds-u-mr2">Click icon for Popover Menu</span>
       
-      <sdds-button aria-label="menu" only-icon id="my-popover-button" type="ghost" size="sm">
+      <tds-button aria-label="menu" only-icon id="my-popover-button" type="ghost" size="sm">
         <tds-icon slot="icon" size="16px" name="kebab"></tds-icon>
-      </sdds-button>
+      </tds-button>
     </div>
 
     <script>
