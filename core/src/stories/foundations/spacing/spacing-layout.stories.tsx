2e95498366ba83fde8a1fd7975e634abc55f4c15
mJarsater <martin.jarsater@hotmail.com>
2023-05-26 16:31:15 +0200

    docs(spacing): updated spacing stories with tds prefix

--- core/src/stories/foundations/spacing/spacing-layout.stories.tsx
+++ core/src/stories/foundations/spacing/spacing-layout.stories.tsx
@@ -1,9 +1,9 @@
-import { formatHtmlPreview } from "../../../utils/utils";
+import { formatHtmlPreview } from '../../../utils/utils';
 
 export default {
-  title: "Foundations/Spacing",
+  title: 'Foundations/Spacing',
   parameters: {
-    layout: "fullscreen",
+    layout: 'fullscreen',
   },
 };
 
@@ -13,7 +13,7 @@ const Template = () =>
   <style>
    /* Demo code for presentation purposes */
     .demo-wrapper {
-      background-color: var(--sdds-blue-500);
+      background-color: var(--tds-blue-500);
       display: inline-block;
     }
     table th, table td {
@@ -35,65 +35,65 @@ const Template = () =>
     <tr>
       <td>8px</td>
       <td><code>$spacing-layout-8</code></td>
-      <td><code>.sdds-spacing-layout-8</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-8); height: var(--sdds-spacing-layout-8)"></span></td>
+      <td><code>.tds-spacing-layout-8</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-8); height: var(--tds-spacing-layout-8)"></span></td>
     </tr>
     <tr>
       <td>16px</td>
       <td><code>$spacing-layout-16</code></td>
-      <td><code>.sdds-spacing-layout-16</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-16); height: var(--sdds-spacing-layout-16);"></span></td>
+      <td><code>.tds-spacing-layout-16</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-16); height: var(--tds-spacing-layout-16);"></span></td>
     </tr>
     <tr>
       <td>24px</td>
       <td><code>$spacing-layout-24</code></td>
-      <td><code>.sdds-spacing-layout-24</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-24); height: var(--sdds-spacing-layout-24);"></span></td>
+      <td><code>.tds-spacing-layout-24</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-24); height: var(--tds-spacing-layout-24);"></span></td>
     </tr>
     <tr>
       <td>32px</td>
       <td><code>$spacing-layout-32</code></td>
-      <td><code>.sdds-spacing-layout-32</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-32); height: var(--sdds-spacing-layout-32);"></span></td>
+      <td><code>.tds-spacing-layout-32</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-32); height: var(--tds-spacing-layout-32);"></span></td>
     </tr>
     <tr>
       <td>48px</td>
       <td><code>$spacing-layout-48</code></td>
-      <td><code>.sdds-spacing-layout-48</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-48); height: var(--sdds-spacing-layout-48);"></span></td>
+      <td><code>.tds-spacing-layout-48</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-48); height: var(--tds-spacing-layout-48);"></span></td>
     </tr>
     <tr>
       <td>64px</td>
       <td><code>$spacing-layout-64</code></td>
-      <td><code>.sdds-spacing-layout-64</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-64); height: var(--sdds-spacing-layout-64);"></span></td>
+      <td><code>.tds-spacing-layout-64</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-64); height: var(--tds-spacing-layout-64);"></span></td>
     </tr>
     <tr>
       <td>72px</td>
       <td><code>$spacing-layout-72</code></td>
-      <td><code>.sdds-spacing-layout-72</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-72); height: var(--sdds-spacing-layout-72);"></span></td>
+      <td><code>.tds-spacing-layout-72</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-72); height: var(--tds-spacing-layout-72);"></span></td>
     </tr>
     <tr>
       <td>96px</td>
       <td><code>$spacing-layout-96</code></td>
-      <td><code>.sdds-spacing-layout-96</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-96); height: var(--sdds-spacing-layout-96);"></span></td>
+      <td><code>.tds-spacing-layout-96</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-96); height: var(--tds-spacing-layout-96);"></span></td>
     </tr>
     <tr>
       <td>128px</td>
       <td><code>$spacing-layout-128</code></td>
-      <td><code>.sdds-spacing-layout-128</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-128); height: var(--sdds-spacing-layout-128);"></span></td>
+      <td><code>.tds-spacing-layout-128</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-128); height: var(--tds-spacing-layout-128);"></span></td>
     </tr>
     <tr>
       <td>160px</td>
       <td><code>$spacing-layout-160</code></td>
-      <td><code>.sdds-spacing-layout-160</code></td>
-      <td><span class="demo-wrapper" style="width: var(--sdds-spacing-layout-160); height: var(--sdds-spacing-layout-160);"></span></td>
+      <td><code>.tds-spacing-layout-160</code></td>
+      <td><span class="demo-wrapper" style="width: var(--tds-spacing-layout-160); height: var(--tds-spacing-layout-160);"></span></td>
     </tr>
   </table>
-  `
+  `,
   );
 
 export const SpacingLayout = Template.bind({});
