c837433d22923973d457dc1f31c8af834569c747
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-06-01 08:05:35 +0200

    docs(prefix change): announce prefix change (#124)
    * docs(announcements): introduced announcements structure and prefix change announcement
    
    * docs(prefix change): prefix change
    
    * docs(prefix-change): prefix change announcemnet
    
    * docs(prefix-change): announcement
    
    * feat(landing page): redid the landing page using our components
    
    * docs(annoucements): updated announcements to use our components and moved away from MDX
    
    * build(stories): exclude stories from build
    
    * docs(stories): update paragraphs to use tds-body-02 class
    
    * docs(stories): updated stories to use correct typography class -> body-01
    
    * docs(stories): updated links to use tds-link
    
    * fix(missing <): missing <
    
    * docs(landing page): updated landing page styles
    
    * fix(storbook): style of announcements fixed
    
    * fix(hide panel): hide control panel on stories
    
    * fix(hide toolbar): hide toolbar on stories
    
    * docs(storybook): testing for panel/toolbar hiding
    
    * fix(root): removed 100% on root
    
    * fix(scrollbar): added tds scrollbar for storie
    
    * fix(stories): corrected css styling for stories
    
    * fix(stories): correct css selectors
    
    * docs: fix start page design issues
    
    * docs: fix announcement page design issues
    
    * docs: fix typo in tegel announcement
    
    Co-authored-by: Tim <tim.romberg@scania.com>
    
    * docs: add missing space in tegel announcement
    
    ---------
    
    Co-authored-by: Alexander Eneroth <alexandereneroth@users.noreply.github.com>
    Co-authored-by: Tim <tim.romberg@scania.com>

---
+++ core/src/stories/announcements/prefix-change.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta } from '@storybook/html';
+
+const meta: Meta = {
+  title: 'Intro/Announcements',
+  parameters: {
+    layout: 'fullscreen',
+    options: {
+      showPanel: false,
+      showToolbar: false,
+    },
+  },
+};
+
+export default meta;
+export const PrefixChange = {
+  render: () => `<style>
+    article {
+        box-sizing: border-box;
+        max-width: 688px;
+        padding: 32px;
+
+        > * {
+            margin-bottom: 72px;
+        }
+    }
+
+    section > p,
+    section > ul,
+    section > tds-link {
+        margin-bottom: 32px;
+    }
+
+    tds-link {
+        display: inline-block;
+    }
+
+    .mb-72 {
+        margin-bottom: 72px;
+    }
+    </style>
+
+    <article class="tds-u-p2 tds-body-01">
+        <section>
+            <p class="tds-body-01"><strong>Published: 2022-05-30</strong></p>
+            <h1 class="tds-headline-02">Prefix change 🔁</h1>
+            <p>
+                In a previous announcement, we mentioned that @scania/tegel would undergo a prefix change. We have been diligently
+                working on this for the past few weeks, and we are pleased to inform you that it is now complete. Previously, all
+                components, utility classes, and CSS variables were prefixed with 'sdds'. However, we have decided to introduce a
+                different prefix for @scania/tegel, which will be 'tds'. As a result, all our components, utility classes, and CSS
+                variables will now be prefixed with 'tds', as shown below:
+            </p>
+            <ul class="tds-body-01">
+                <li> &lt;tds-button/&gt; </li>
+                <li>--tds-1button-color</li>
+                <li>.tds-u-flex</li>
+            </ul>
+            <p>
+                We have made this change to establish a clear distinction between @scania/components and @scania/tegel. Additionally, it
+                will facilitate a smoother transition to the new package for you, the users. You can now install @scania/tegel alongside
+                @scania/components, allowing for an incremental transition.</p>
+            </p>
+        </section>
+        <section>
+            <h3 class="tds-u-mb1">
+                Want to get in touch? 📞
+            </h3>
+
+
+            <p>
+                Awesome! We are available via teams and have two support channels. One for
+                <tds-link>
+                    <a
+                        href="https://teams.microsoft.com/l/channel/19%3a5e33f67fe502441f914fbcdc6e2548f5%40thread.skype/Development%2520support?groupId=79f9bfeb-73e2-424d-9477-b236191ece5e&tenantId=3bc062e4-ac9d-4c17-b4dd-3aad637ff1ac">development</a>
+                </tds-link>
+                and one for
+                <tds-link>
+                    <a
+                        href="https://teams.microsoft.com/l/channel/19%3A8d30ded56af14672b471062ec1e66345%40thread.skype/Design%2520support?groupId=79f9bfeb-73e2-424d-9477-b236191ece5e&tenantId=3bc062e4-ac9d-4c17-b4dd-3aad637ff1ac">design.</a>
+
+                </tds-link>
+                If you want to submit a bug report or feature request, please do so via our
+                <tds-link>
+                    <a href="https://github.com/scania-digital-design-system/tegel/issues">GitHub</a>
+                </tds-link>.
+
+            <p>
+                All the best,<br />
+                The Tegel Team.
+            </p>
+            </p>
+        </section>
+
+    </article>
+    `,
+};
