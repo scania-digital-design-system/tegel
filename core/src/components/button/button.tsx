a71bdbffed849ce74aa847ecf85f381b386de7b7
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-07-19 17:38:16 +0200

    feat(button): submit | reset | button types, variant prop, label slot (#204)
    * feat(button): add native types, turn off shadowDOM, add scoped
    
    * docs: adding control for html type in story
    
    * fix(button): variant replacing type prop functionality
    
    * fix(button): slot as alternative to text

--- core/src/components/button/button.tsx
+++ core/src/components/button/button.tsx
@@ -2,12 +2,14 @@ import { Component, Element, h, Host, Prop, State } from '@stencil/core';
 import { hasSlot } from '../../utils/utils';
 
 /**
+ * @slot label - Slot for the text injection. Serves as alternative to text prop.
  * @slot icon - Slot used to display an Icon in the Button.
  */
 @Component({
   tag: 'tds-button',
   styleUrl: 'button.scss',
-  shadow: true,
+  shadow: false,
+  scoped: true,
 })
 export class TdsButton {
   @Element() host: HTMLElement;
@@ -15,9 +17,13 @@ export class TdsButton {
   /** Text displayed inside the Button */
   @Prop() text: string;
 
-  /** Type of Button */
-  @Prop() type: 'primary' | 'secondary' | 'ghost' | 'danger' = 'primary';
+  /** Button's type */
+  @Prop() type: 'button' | 'submit' | 'reset' = 'button';
 
+  /** Variation of Button's design */
+  @Prop() variant: 'primary' | 'secondary' | 'ghost' | 'danger' = 'primary';
+
+  /** Size of a Button */
   @Prop() size: 'xs' | 'sm' | 'md' | 'lg' = 'lg';
 
   /** Control for disabled state of a component */
@@ -39,28 +45,32 @@ export class TdsButton {
   }
 
   render() {
-    const usesIconSlot = hasSlot('icon', this.host);
+    const hasLabelSlot = hasSlot('label', this.host);
+    const hasIconSlot = hasSlot('icon', this.host);
+
     return (
       <Host class={`${this.modeVariant !== null ? `tds-mode-variant-${this.modeVariant}` : ''}`}>
         <button
+          type={this.type}
           disabled={this.disabled}
           class={{
-            'primary': this.type === 'primary',
-            'secondary': this.type === 'secondary',
-            'ghost': this.type === 'ghost',
-            'danger': this.type === 'danger',
+            'primary': this.variant === 'primary',
+            'secondary': this.variant === 'secondary',
+            'ghost': this.variant === 'ghost',
+            'danger': this.variant === 'danger',
             'lg': this.size === 'lg',
             'md': this.size === 'md',
             'sm': this.size === 'sm',
             'xs': this.size === 'xs',
             'disabled': this.disabled,
             'fullbleed': this.fullbleed,
-            'icon': usesIconSlot,
+            'icon': hasIconSlot,
             'only-icon': this.onlyIcon,
           }}
         >
           {this.text}
-          <slot name="icon" />
+          {hasLabelSlot && !this.onlyIcon && <slot name="label" />}
+          {hasIconSlot && <slot name="icon" />}
         </button>
       </Host>
     );
--- core/src/components/button/readme.md
+++ core/src/components/button/readme.md
@@ -12,16 +12,18 @@
 | `disabled`    | `disabled`     | Control for disabled state of a component | `boolean`                                         | `false`     |
 | `fullbleed`   | `fullbleed`    | When enabled, the Button takes 100% width | `boolean`                                         | `false`     |
 | `modeVariant` | `mode-variant` | Set the mode variant of the Button.       | `"primary" \| "secondary"`                        | `null`      |
-| `size`        | `size`         |                                           | `"lg" \| "md" \| "sm" \| "xs"`                    | `'lg'`      |
+| `size`        | `size`         | Size of a Button                          | `"lg" \| "md" \| "sm" \| "xs"`                    | `'lg'`      |
 | `text`        | `text`         | Text displayed inside the Button          | `string`                                          | `undefined` |
-| `type`        | `type`         | Type of Button                            | `"danger" \| "ghost" \| "primary" \| "secondary"` | `'primary'` |
+| `type`        | `type`         | Button's type                             | `"button" \| "reset" \| "submit"`                 | `'button'`  |
+| `variant`     | `variant`      | Variation of Button's design              | `"danger" \| "ghost" \| "primary" \| "secondary"` | `'primary'` |
 
 
 ## Slots
 
-| Slot     | Description                                 |
-| -------- | ------------------------------------------- |
-| `"icon"` | Slot used to display an Icon in the Button. |
+| Slot      | Description                                                      |
+| --------- | ---------------------------------------------------------------- |
+| `"icon"`  | Slot used to display an Icon in the Button.                      |
+| `"label"` | Slot for the text injection. Serves as alternative to text prop. |
 
 
 ----------------------------------------------
