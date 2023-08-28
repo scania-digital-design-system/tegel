ac9edc033cebad0839f61c9dfc8cb3f586c3b1b1
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-08-28 17:28:49 +0200

    fix: try using pack 3

--- react-test/pack-creation.sh
+++ react-test/pack-creation.sh
@@ -9,23 +9,35 @@ npm run build
 echo "Build done!"
 
 # Make a link
-npm link
-echo "Link done!"
+#npm link
+#echo "Link done!"
 
 # Make a pack
-#npm pack --pack-destination "../react-test"
-#echo "Packing done!"
+npm pack --pack-destination "../react-test"
+echo "Packing done!"
 
 cd ../react-test
 echo "Back to react-test!"
 
 # Rename a pack
-#mv "$(ls *.tgz | head -n 1)" scania-tegel.tgz
-#echo "Renaming done!"
+mv "$(ls *.tgz | head -n 1)" scania-tegel.tgz
+echo "Renaming done!"
 
 # Linking
-npm link @scania/tegel
-echo "Linking done!"
+#npm link @scania/tegel
+#echo "Linking done!"
+
+# Drop node modules
+rm -rf node-modules
+echo "Deleting node modules!"
+
+# Installation of tgz
+npm install scania-tegel.tgz
+echo "Installation of tgz done!"
+
+# General installation
+npm install
+echo "Installation everything else!"
 
 # Building
 npm run build
