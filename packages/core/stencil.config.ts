c111a13a86d7a64ca11e747364c4639bedeaad85
Tim <tim.romberg@scania.com>
2023-09-15 14:37:10 +0200

    feat: angular wrappers (#292)
    * feat: monorepo structure for angular wrappers
    
    * feat: monorepo structure for angular wrappers
    
    * feat: all config in place, wrappers and output target
    
    * feat(valueaccessorconfig): added valueAcccessorConfig
    
    * docs: adding some info in the readme
    
    * feat: added build components script
    
    * build(stencil): updated build config
    
    switched to standalone arch for angular wrappers, updated stencil config to put dist-custom-elements
    in correct folder and switched behaviour to auto-define-custom-elements
    
    * build(stencil): local build of stencil repo
    
    * build(tegel-angular): updated angular wrapper to use local build of @scania/tegel
    
    * build(tegel-angular): updated module export in tegel-angular
    
    * fix: could not find the '@angular-devkit/build-angular:ng-packagr'
    
    * build(stencil.config): switched to single-export-module
    
    * fix(packs): removed unused packs
    
    * build(eslintignore): added angular package to eslintignore
    
    * build(gitignore): update gitignore
    
    * fix(stencil-generated): removed stencil-generated folder
    
    * fix: removed pack and used @scania/tegel@1.0.2 to build wrappers
    
    * build: updatd build script for angular pack
    
    * build(.angular): removed build .anguilar folder
    
    * fix(gitignore): gitignored built .angular folder
    
    * fix(components): removed build components  folder
    
    * build(stencil.config): updated angularOutputTarget
    
    * fix(angular): updated angular build to use tegel 1.0.1
    
    * fix(angular): updated outpurTarget
    
    * fix(stencil.cofig): updated stencil config
    
    * fix(icons): updated path in icons gulpfile
    
    * build(@scania/tegel-angular): updated npm version
    
    * build: added build script for angular and bumped npm version
    
    * fix: removed pack
    
    ---------
    
    Co-authored-by: mJarsater <martin.jarsater@hotmail.com>
    Co-authored-by: Alexander Eneroth <alexandereneroth@users.noreply.github.com>

---
+++ packages/core/stencil.config.ts
@@ -0,0 +1,84 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { ValueAccessorConfig, angularOutputTarget } from '@stencil/angular-output-target';
+
+const angularValueAccessorBindings: ValueAccessorConfig[] = [
+  {
+    elementSelectors: ['tds-radio-button'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'radio',
+  },
+  {
+    elementSelectors: ['tds-checkbox'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'boolean',
+  },
+  {
+    elementSelectors: ['tds-text-field', 'tds-textarea'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'text',
+  },
+  {
+    elementSelectors: ['tds-dropdown'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'text',
+  },
+  {
+    elementSelectors: ['tds-slider'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'number',
+  },
+  {
+    elementSelectors: ['tds-chip'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'radio',
+  },
+  {
+    elementSelectors: ['tds-chip'],
+    event: 'tdsChange',
+    targetAttr: 'value',
+    type: 'boolean',
+  },
+];
+
+export const config: Config = {
+  namespace: 'tegel',
+  globalStyle: 'src/global/global.scss',
+  extras: {
+    enableImportInjection: true,
+    tagNameTransform: true,
+  },
+  plugins: [sass()],
+  sourceMap: false,
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    angularOutputTarget({
+      componentCorePackage: '@scania/tegel',
+      outputType: 'component',
+      directivesProxyFile: '../angular/projects/components/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular/projects/components/src/lib/stencil-generated/index.ts',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
+    {
+      type: 'dist-custom-elements',
+      generateTypeDeclarations: true,
+      customElementsExportBehavior: 'auto-define-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
