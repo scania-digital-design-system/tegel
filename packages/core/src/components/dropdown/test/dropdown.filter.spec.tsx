734cf6ee4dc8bcf2799703b453a013b85da9b9a8
mJarsater <martin.jarsater@hotmail.com>
2023-10-06 15:07:54 +0200

    test(dropdown): added folder for tests and seperated test files

---
+++ packages/core/src/components/dropdown/test/dropdown.filter.spec.tsx
@@ -0,0 +1,95 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { h } from '@stencil/core';
+import { TdsDropdown } from '../dropdown';
+import { TdsDropdownOption } from '../dropdown-option/dropdown-option';
+
+const options = [
+  {
+    value: 'option-1',
+    label: 'Option 1',
+    disabled: false,
+  },
+  {
+    value: 'option-2',
+    label: 'Option 2',
+    disabled: false,
+  },
+  {
+    value: 'option-3',
+    label: 'Option 3',
+    disabled: true,
+  },
+];
+describe('filter dropdown', () => {
+  it('should render children correctly', async () => {
+    const page = await newSpecPage({
+      components: [TdsDropdown, TdsDropdownOption],
+      template: () => (
+        <tds-dropdown filter>
+          {options.map((option) => (
+            <tds-dropdown-option value={option.value} disabled={option.disabled}>
+              {option.label}
+            </tds-dropdown-option>
+          ))}
+        </tds-dropdown>
+      ),
+    });
+
+    const children = await page.doc.querySelectorAll('tds-dropdown-option');
+    expect(children.length).toBe(3);
+  });
+  it('should set defaultValue correctly', async () => {
+    const page = await newSpecPage({
+      components: [TdsDropdown, TdsDropdownOption],
+      template: () => (
+        <tds-dropdown filter defaultValue="option-1">
+          {options.map((option) => (
+            <tds-dropdown-option value={option.value} disabled={option.disabled}>
+              {option.label}
+            </tds-dropdown-option>
+          ))}
+        </tds-dropdown>
+      ),
+    });
+
+    const dropdown = await page.doc.querySelector('tds-dropdown');
+    expect(dropdown.getAttribute('value')).toBe('option-1');
+  });
+  it('should set value using setValue() correctly', async () => {
+    const page = await newSpecPage({
+      components: [TdsDropdown, TdsDropdownOption],
+      template: () => (
+        <tds-dropdown filter defaultValue="option-3">
+          {options.map((option) => (
+            <tds-dropdown-option value={option.value} disabled={option.disabled}>
+              {option.label}
+            </tds-dropdown-option>
+          ))}
+        </tds-dropdown>
+      ),
+    });
+
+    const dropdown = await page.doc.querySelector('tds-dropdown');
+    await dropdown.setValue('option-1');
+    expect(dropdown.getAttribute('value')).toBe('option-1');
+  });
+
+  it('should remove value using removeValue() correctly', async () => {
+    const page = await newSpecPage({
+      components: [TdsDropdown, TdsDropdownOption],
+      template: () => (
+        <tds-dropdown filter defaultValue="option-3">
+          {options.map((option) => (
+            <tds-dropdown-option value={option.value} disabled={option.disabled}>
+              {option.label}
+            </tds-dropdown-option>
+          ))}
+        </tds-dropdown>
+      ),
+    });
+
+    const dropdown = await page.doc.querySelector('tds-dropdown');
+    await dropdown.removeValue('option-3');
+    expect(dropdown.getAttribute('value')).toBe('null');
+  });
+});
--- packages/core/src/components/dropdown/test/dropdown.spec.tsx
+++ packages/core/src/components/dropdown/test/dropdown.spec.tsx
@@ -1,7 +1,7 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { h } from '@stencil/core';
-import { TdsDropdown } from './dropdown';
-import { TdsDropdownOption } from './dropdown-option/dropdown-option';
+import { TdsDropdown } from '../dropdown';
+import { TdsDropdownOption } from '../dropdown-option/dropdown-option';
 
 const options = [
   {
@@ -95,82 +95,6 @@ describe('singleselect dropdown', () => {
     expect(dropdown.getAttribute('value')).toBe('null');
   });
 });
-describe('multiselect dropdown', () => {
-  it('should render children correctly', async () => {
-    const page = await newSpecPage({
-      components: [TdsDropdown, TdsDropdownOption],
-      template: () => (
-        <tds-dropdown multiselect>
-          {options.map((option) => (
-            <tds-dropdown-option value={option.value} disabled={option.disabled}>
-              {option.label}
-            </tds-dropdown-option>
-          ))}
-        </tds-dropdown>
-      ),
-    });
-
-    const children = await page.doc.querySelectorAll('tds-dropdown-option');
-    expect(children.length).toBe(3);
-  });
-  it('should set defaultValue correctly', async () => {
-    const page = await newSpecPage({
-      components: [TdsDropdown, TdsDropdownOption],
-      template: () => (
-        <tds-dropdown multiselect defaultValue="option-1,option-2">
-          {options.map((option) => (
-            <tds-dropdown-option value={option.value} disabled={option.disabled}>
-              {option.label}
-            </tds-dropdown-option>
-          ))}
-        </tds-dropdown>
-      ),
-    });
-
-    const dropdown = await page.doc.querySelector('tds-dropdown');
-    expect(dropdown.getAttribute('value')).toBe('option-1,option-2');
-  });
-  it('should set value using setValue() correctly', async () => {
-    const page = await newSpecPage({
-      components: [TdsDropdown, TdsDropdownOption],
-      template: () => (
-        <tds-dropdown multiselect defaultValue="option-3">
-          {options.map((option) => (
-            <tds-dropdown-option value={option.value} disabled={option.disabled}>
-              {option.label}
-            </tds-dropdown-option>
-          ))}
-        </tds-dropdown>
-      ),
-    });
-
-    const dropdown = await page.doc.querySelector('tds-dropdown');
-    await dropdown.setValue(['option-1', 'option-3']);
-    expect(dropdown.getAttribute('value')).toBe('option-1,option-3');
-  });
-
-  it('should remove value using removeValue() correctly', async () => {
-    const page = await newSpecPage({
-      components: [TdsDropdown, TdsDropdownOption],
-      template: () => (
-        <tds-dropdown multiselect defaultValue="option-3,option-1">
-          {options.map((option) => (
-            <tds-dropdown-option value={option.value} disabled={option.disabled}>
-              {option.label}
-            </tds-dropdown-option>
-          ))}
-        </tds-dropdown>
-      ),
-    });
-
-    const dropdown = await page.doc.querySelector('tds-dropdown');
-    await dropdown.removeValue('option-3');
-    expect(dropdown.getAttribute('value')).toBe('option-1');
-    await dropdown.removeValue('option-1');
-    expect(dropdown.getAttribute('value')).toBe('null');
-  });
-});
-
 describe('filter dropdown', () => {
   it('should render children correctly', async () => {
     const page = await newSpecPage({
