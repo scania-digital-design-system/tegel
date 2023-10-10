e76715f979ac6942262ac83e57c0a6ffc496d2c6
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-10-10 12:36:21 +0200

    feat(checkbox): indeterminate state (#352)
    * feat(checkbox): added variables for interminate state
    
    * feat(checkbox): added prop and styling for indeterminate state
    
    * docs(checkbox): added indeterminate control to story
    
    * docs(checkbox): added examples for indeterminate state
    
    * feat(checkbox): mutate indeterminate, prop over class
    
    * test(checkbox): added basic tests
    
    * fix(checkbox): marks checked prop as mutable since its being changed from whithin
    
    * test(checkbox): adds basic e2e test
    
    * test(checkbox): moved test files into seperate folder
    
    * test(checkbox): typo
    
    ---------
    
    Co-authored-by: Alexander Eneroth <alexandereneroth@users.noreply.github.com>

---
+++ packages/core/src/components/checkbox/test/checkbox.e2e.ts
@@ -0,0 +1,33 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('tds-checkbox', () => {
+  it('should toggle checked state when clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <tds-checkbox>
+        <span slot="label">Label</span>
+    </tds-checkbox>
+    `);
+    const el = await page.find('tds-checkbox');
+    expect(el).not.toBeNull();
+    await el.click();
+    await page.waitForChanges();
+    expect(await el.getProperty('checked')).toBe(true);
+    await el.click();
+    await page.waitForChanges();
+    expect(await el.getProperty('checked')).toBe(false);
+  });
+  it('should not toggle checked state when clicked if disabled', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <tds-checkbox disabled>
+        <span slot="label">Label</span>
+    </tds-checkbox>
+    `);
+    const el = await page.find('tds-checkbox');
+    expect(el).not.toBeNull();
+    await el.click();
+    await page.waitForChanges();
+    expect(await el.getProperty('checked')).toBe(false);
+  });
+});
