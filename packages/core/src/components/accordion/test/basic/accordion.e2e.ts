e19ecb451b1d4bf022fb03403cbb895bb23bee66
mJarsater <martin.jarsater@hotmail.com>
2023-10-20 12:55:02 +0200

    test(accordion): added basic testa for accordion

---
+++ packages/core/src/components/accordion/test/basic/accordion.e2e.ts
@@ -0,0 +1,20 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+test.describe('tds-accordiong', () => {
+  test('renders basic accordion correctly', async ({ page }) => {
+    await page.goto('src/components/accordion/test/basic/index.html');
+    const accordion = page.locator('tds-accordion');
+    await expect(accordion).toHaveClass(/hydrated/);
+    await expect(accordion).toContainText('First item');
+    await expect(accordion).toContainText('Second item');
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+  });
+  test('fires tdsToggle event on click', async ({ page }) => {
+    await page.goto('src/components/accordion/test/basic/index.html');
+    const accordionFirstItem = page.locator('tds-accordion-item[header="First item"]');
+    const myEventSpy = await page.spyOnEvent('tdsToggle');
+    await accordionFirstItem.click();
+    expect(myEventSpy).toHaveReceivedEvent();
+  });
+});
