a812dc22403256c914bcce1736b7354d7c5be4d8
mJarsater <martin.jarsater@hotmail.com>
2023-10-20 15:48:50 +0200

    test(badge): added basic test for badge

---
+++ packages/core/src/components/badge/test/basic/badge.e2e.ts
@@ -0,0 +1,11 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+test.describe('tds-badge', () => {
+  test('renders basic badge correctly', async ({ page }) => {
+    await page.goto('src/components/badge/test/basic/index.html');
+    const accordion = page.locator('tds-badge');
+    await expect(accordion).toHaveClass('hydrated');
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+  });
+});
