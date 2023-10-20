0a24b183cd8ecdfbc4623ea3687e60df216f7d07
mJarsater <martin.jarsater@hotmail.com>
2023-10-20 15:06:38 +0200

    test(accordion): added test for padding-reset

---
+++ packages/core/src/components/accordion/test/padding-reset/accordion.e2e.ts
@@ -0,0 +1,25 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+test.describe('tds-accordiong', () => {
+  test('renders padding-reset accordion correctly', async ({ page }) => {
+    await page.goto('src/components/accordion/test/expanded/index.html');
+    const accordion = page.locator('tds-accordion');
+    await expect(accordion).toHaveClass(/hydrated/);
+    await expect(accordion).toContainText('First item');
+    await expect(accordion).toContainText('Second item');
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+  });
+  test('does fire tdsToggle event on click on accordion', async ({ page }) => {
+    await page.goto('src/components/accordion/test/expanded/index.html');
+    const accordionFirstItem = page.locator('tds-accordion-item[header="First item"]');
+    const myEventSpy = await page.spyOnEvent('tdsToggle');
+    await accordionFirstItem.click({
+      position: {
+        x: 1,
+        y: 1,
+      },
+    });
+    expect(myEventSpy).toHaveReceivedEvent();
+  });
+});
