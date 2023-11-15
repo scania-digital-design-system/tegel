ad4283e343aa21716d516cb9c317d02f3ddeb525
alfredskeden <alfred.skedeback91@gmail.com>
2023-11-15 17:17:24 +0100

    test: button tests to check for colors and other things to find a good pattern

--- packages/core/src/components/button/test/disabled/button.e2e.ts
+++ packages/core/src/components/button/test/disabled/button.e2e.ts
@@ -16,4 +16,22 @@ test.describe('tds-button', () => {
     await button.click();
     expect(myEventSpy).not.toHaveReceivedEvent();
   });
+
+  test('Button color is aligned to disabled state', async ({ page }) => {
+    await page.goto('src/components/button/test/disabled/index.html');
+    const button = page.locator('tds-button');
+    await expect(button).toHaveCSS(
+      'background',
+      'rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box',
+    );
+    await expect(button).toHaveCSS('border-color', 'rgb(13, 15, 19)');
+  });
+
+  test('Text is displayed, style is aligned to disabled state', async ({ page }) => {
+    await page.goto('src/components/button/test/disabled/index.html');
+    const button = page.locator('tds-button');
+    const textAttribute = await button.textContent();
+    await expect(button).toHaveCSS('color', 'rgb(13, 15, 19)');
+    expect(textAttribute).toBe('Button');
+  });
 });
