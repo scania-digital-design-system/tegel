4b49693a2b11fdd9ca079e137090f0759b1ecc5a
Tim <tim.romberg@scania.com>
2023-11-20 08:41:59 +0100

    test(chip): added basic scenario

---
+++ packages/core/src/components/chip/test/basic/chip.e2e.ts
@@ -0,0 +1,21 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+test.describe('tds-chip', () => {
+  test('renders a chip correctly', async ({ page }) => {
+    await page.goto('src/components/chip/test/basic/index.html');
+    const chip = page.locator('tds-chip');
+
+    await expect(chip).toHaveClass(/hydrated/);
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('emits tdsClick event on click for button type', async ({ page }) => {
+    await page.goto('src/components/chip/test/basic/index.html');
+    const chip = page.locator('tds-chip[type="button"]');
+    const myEventSpy = await page.spyOnEvent('tdsClick');
+
+    await chip.click();
+    expect(myEventSpy).toHaveReceivedEvent();
+  });
+});
