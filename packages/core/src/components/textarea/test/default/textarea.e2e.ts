3b3cc06a94bbf046be99d573ceedbcf723b3812a
alfredskeden <alfred.skedeback91@gmail.com>
2023-11-21 16:18:21 +0100

    test(textarea): adding unit tests

---
+++ packages/core/src/components/textarea/test/default/textarea.e2e.ts
@@ -0,0 +1,37 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/textarea/test/default/index.html';
+
+test.describe('tds-textarea', () => {
+  test('renders default textarea correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea');
+    await expect(textarea).toHaveClass(/hydrated/);
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('default textarea - click on text area - cursor should appear', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea').locator('textarea');
+    const myEventSpy = await page.spyOnEvent('click');
+    await textarea.click();
+    expect(myEventSpy).toHaveReceivedEvent();
+    const textareaCursorState = await textarea.evaluate(
+      (button2) => getComputedStyle(button2).cursor,
+    );
+    expect(textareaCursorState).toBe('text');
+  });
+
+  test('default textarea - type text in textarea - should be able to add text', async ({
+    page,
+  }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea').locator('textarea');
+    const myEventSpy = await page.spyOnEvent('click');
+    await textarea.click();
+    expect(myEventSpy).toHaveReceivedEvent();
+    await textarea.fill('Adding some text');
+    expect(await textarea.inputValue()).toBe('Adding some text');
+  });
+});
---
+++ packages/core/src/components/textarea/test/read-only/textarea.e2e.ts
@@ -0,0 +1,32 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/textarea/test/read-only/index.html';
+
+test.describe('tds-textarea', () => {
+  test('renders read-only textarea correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea');
+    await expect(textarea).toHaveClass(/hydrated/);
+    await expect(textarea).toHaveAttribute('read-only');
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('read-only textarea - native textarea should have readonly attribute', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea').locator('textarea');
+    await expect(textarea).toHaveAttribute('readonly');
+    const readonlyIcon = page.locator('tds-icon[name="edit_inactive"]');
+    expect(readonlyIcon).not.toBeNull();
+  });
+
+  // Error: locator.fill: Target closed
+  // Not able to update the since we get Playwright error here when trying to input a readonly input.
+  // Skipping for now.
+  test.skip('Type text in textarea - no possibility to type text', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.locator('tds-textarea').locator('textarea');
+    await textarea.fill('Adding some text');
+    expect(await textarea.inputValue()).not.toBe('Adding some text');
+  });
+});
