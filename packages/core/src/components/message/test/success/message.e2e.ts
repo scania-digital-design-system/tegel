3843d3ebbc8b8dce9451dbdc587cf5459cd5979b
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-11-21 14:46:29 +0100

    test(message): added unit tests

---
+++ packages/core/src/components/message/test/success/message.e2e.ts
@@ -0,0 +1,41 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/message/test/success/index.html';
+
+test.describe('tds-message', () => {
+  test('is rendered correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Check if a component exists in the DOM */
+    const messageComponent = page.locator('tds-message');
+    expect(messageComponent).toBeTruthy();
+
+    /* Take screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('is in success variant', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const messageComponentDiv = page.locator('tds-message > div');
+    await expect(messageComponentDiv).toHaveClass(/success/);
+  });
+
+  test('has header text', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const messageComponent = page.locator('tds-message');
+    await expect(messageComponent).not.toBeEmpty();
+  });
+
+  test('has subheader text', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const messageComponent = page.locator('tds-message');
+    await expect(messageComponent).not.toBeEmpty();
+  });
+
+  test('has error icon', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const messageIconComponent = page.locator('tds-message tds-icon');
+    await expect(messageIconComponent).toHaveAttribute('name', 'tick');
+  });
+});
