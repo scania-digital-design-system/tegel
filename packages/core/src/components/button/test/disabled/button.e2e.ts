715d3995ae21b5e76aa7355d5f110c829839c476
Alfred Skedebäck <alfred.skedeback@umain.com>
2023-11-24 15:51:38 +0100

    test(button): added unit tests (#433)
    * feat(button): add unit tests for basic and disabled button
    
    * test: button tests to check for colors and other things to find a good pattern
    
    * test: adding more tests for more test cases in button
    
    * test: small update to button tests
    
    * test: make sure disabled button has the correct cursor when disabled test
    
    * fix: update pr with button without text and icon and check for width
    
    * chore(all tests): update to componentTestPath instead of componentName
    
    * test(button): update test text to be correct after pr comment
    
    * test(button): update small text
    
    ---------
    
    Co-authored-by: Lindqvist Nathalie <nathalie.lindqvist@scania.com>

---
+++ packages/core/src/components/button/test/disabled/button.e2e.ts
@@ -0,0 +1,49 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/button/test/disabled/index.html';
+
+test.describe('tds-button', () => {
+  test('renders disabled button correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.locator('tds-button');
+    await expect(button).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('that the component is disabled', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.locator('tds-button');
+    await expect(button).toHaveAttribute('disabled', '');
+    const myEventSpy = await page.spyOnEvent('click');
+    await button.click();
+    expect(myEventSpy).not.toHaveReceivedEvent();
+  });
+
+  test('Button color is aligned to disabled state', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.locator('tds-button');
+    const innerButton = button.locator('button');
+    const buttonBackgroundColor = await innerButton.evaluate(
+      (button2) => getComputedStyle(button2).backgroundColor,
+    );
+    expect(buttonBackgroundColor).toBe('rgb(249, 250, 251)');
+  });
+
+  test('Text is displayed, style is aligned to disabled state', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.locator('tds-button');
+    const textAttribute = await button.textContent();
+    await expect(button).toHaveCSS('color', 'rgb(13, 15, 19)');
+    expect(textAttribute).toBe('Button');
+  });
+
+  test('the cursor should be not-allowed', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.locator('tds-button');
+    const innerButton = button.locator('button');
+    const buttonCursorState = await innerButton.evaluate(
+      (button2) => getComputedStyle(button2).cursor,
+    );
+    expect(buttonCursorState).toBe('not-allowed');
+  });
+});
