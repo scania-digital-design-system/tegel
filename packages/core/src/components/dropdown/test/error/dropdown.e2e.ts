23614f10fab53fc0ad4267f12d85b53b3fb145d3
alfredskeden <alfred.skedeback91@gmail.com>
2023-11-28 14:45:15 +0100

    test(dropdown): test correctly

--- packages/core/src/components/dropdown/test/error/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/error/dropdown.e2e.ts
@@ -6,79 +6,34 @@ const componentTestPath = 'src/components/dropdown/test/error/index.html';
 test.describe('tds-dropdown-error', () => {
   test('renders error dropdown correctly', async ({ page }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    expect(dropdown).toBeTruthy();
+    const dropdown = page.getByTestId('tds-dropdown-testid');
+    await expect(dropdown).toHaveCount(1);
+
+    /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
-  test('should have a label outside with text', async ({ page }) => {
+  test('find helper text and check if visible and have icon', async ({ page }) => {
     await page.goto(componentTestPath);
-    const labelOutside = page.locator('tds-dropdown > div[class="label-outside"]');
-    await expect(labelOutside).toHaveText(/Label text/);
-  });
-
-  test('helper text with error icon', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const helperText = dropdown.locator('div[class="helper error"]');
-    expect(helperText).toBeTruthy();
-    const helperErrorIcon = helperText.locator('tds-icon[name="error"]');
-    expect(helperErrorIcon).toBeTruthy();
-  });
-
-  test('the placeholder="Placeholder" text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const button = dropdown.locator('button');
-    const placeholder = button.locator('div[class="placeholder lg"]');
-    const textAttribute = await placeholder.textContent();
-    expect(textAttribute).toBe('Placeholder');
-  });
-
-  test('has lg size', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    expect(dropdownSelect).toBeTruthy();
+    const helperText = page.getByText(/Helper text/);
+    await expect(helperText).toBeVisible();
+    const helperTextIcon = helperText.locator('tds-icon');
+    await expect(helperTextIcon).toHaveCount(1);
   });
 
   test('clicking the dropdown opens the dropdown-list', async ({ page }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    const myEventSpy = await page.spyOnEvent('click');
-    await button.click();
-    const dropdownList = dropdown.locator('div[class="dropdown-list lg down label-outside open"]');
-    expect(myEventSpy).toHaveReceivedEvent();
-    expect(dropdownList).toBeTruthy();
+    const dropdownOptionOne = page.getByTestId(/tds-dropdown-option-1/);
+    const placeholderElement = page.getByText(/Placeholder/);
+
+    /* before clicking dropdownlist should not be visible, the button should be */
+    await expect(placeholderElement).toBeVisible();
+    await expect(dropdownOptionOne).not.toBeVisible();
+    await placeholderElement.click();
+
+    /* after clicking dropdownlist should be visible, the button should also be */
+    await expect(placeholderElement).toBeVisible();
+    await expect(dropdownOptionOne).toBeVisible();
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
-
-  test('clicking the dropdown opens the dropdown-list, then click an option 1', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    await button.click();
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-1"]');
-    await dropdownOptionOne.click();
-    await expect(dropdown).toHaveAttribute('value', 'option-1');
-  });
-
-  test('clicking the dropdown opens the dropdown-list, then click an option 2 that is disabled should not close it', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    await button.click();
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-2"]');
-    const myEventSpy = await page.spyOnEvent('click');
-    await dropdownOptionOne.click();
-    expect(myEventSpy).not.toHaveReceivedEvent();
   });
 });
--- packages/core/src/components/dropdown/test/filter/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/filter/dropdown.e2e.ts
@@ -6,94 +6,48 @@ const componentTestPath = 'src/components/dropdown/test/filter/index.html';
 test.describe('tds-dropdown-filter', () => {
   test('renders filter dropdown correctly', async ({ page }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    expect(dropdown).toBeTruthy();
+    const dropdown = page.getByTestId('tds-dropdown-testid');
+    await expect(dropdown).toHaveCount(1);
+
+    /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
-
-  test('should have a label outside with text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const labelOutside = page.locator('tds-dropdown > div[class="label-outside"]');
-    await expect(labelOutside).toHaveText(/Label text/);
-  });
-
-  test('to have helper text with correct text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const helperText = page.locator('tds-dropdown > div[class="helper"]');
-    await expect(helperText).toHaveText(/Helper text/);
-  });
-
-  test('have the placeholder="Placeholder" text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const input = dropdownSelect.locator('input');
-    await expect(input).toHaveAttribute('placeholder', 'Placeholder');
-  });
-
-  test('has lg size', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    expect(dropdownSelect).toBeTruthy();
-  });
-
-  test('clicking the dropdown opens the dropdown-list', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const input = dropdownSelect.locator('input');
-    const myEventSpy = await page.spyOnEvent('click');
-    await input.click();
-    expect(myEventSpy).toHaveReceivedEvent();
-    const dropdownList = dropdown.locator('div[class="dropdown-list lg down label-outside open"]');
-    expect(dropdownList).toBeTruthy();
-  });
-
-  test('clicking the dropdown opens the dropdown-list, then click an option 1', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const input = dropdownSelect.locator('input');
-    await input.click();
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-1"]');
-    await dropdownOptionOne.click();
-    await expect(dropdown).toHaveAttribute('value', 'option-1');
-  });
-
-  test('clicking the dropdown opens the dropdown-list, then click an option 2 that is disabled should not close it', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const input = dropdownSelect.locator('input');
-    await input.click();
-    const dropdownOptionTwo = dropdown.locator('tds-dropdown-option[value="option-2"]');
-    const myEventSpy = await page.spyOnEvent('click');
-    await dropdownOptionTwo.click();
-    expect(myEventSpy).not.toHaveReceivedEvent();
   });
 
   test('clicking the dropdown opens the dropdown-list, then start typing "Option 1" to only show that option in the dropdown list', async ({
     page,
   }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const input = dropdownSelect.locator('input');
-    await input.click();
-    await input.fill('Option 1');
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-1"]');
-    await expect(dropdownOptionOne).not.toHaveAttribute('hidden');
-    const dropdownOptionTwo = dropdown.locator('tds-dropdown-option[value="option-2"]');
-    await expect(dropdownOptionTwo).toHaveAttribute('hidden');
-    const dropdownOptionThree = dropdown.locator('tds-dropdown-option[value="option-3"]');
-    await expect(dropdownOptionThree).toHaveAttribute('hidden');
-    const dropdownOptionFour = dropdown.locator('tds-dropdown-option[value="option-4"]');
-    await expect(dropdownOptionFour).toHaveAttribute('hidden');
+
+    const inputElement = page.getByRole('textbox');
+    const dropdownOptionOne = page.getByTestId(/tds-dropdown-option-1/);
+    const dropdownOptionTwo = page.getByTestId(/tds-dropdown-option-2/);
+    const dropdownOptionThree = page.getByTestId(/tds-dropdown-option-3/);
+    const dropdownOptionFour = page.getByTestId(/tds-dropdown-option-4/);
+
+    /* before clicking dropdownlist should not be visible, the button should be */
+    await expect(inputElement).toBeVisible();
+    await expect(dropdownOptionOne).not.toBeVisible();
+    await expect(dropdownOptionTwo).not.toBeVisible();
+    await expect(dropdownOptionThree).not.toBeVisible();
+    await expect(dropdownOptionFour).not.toBeVisible();
+
+    /* Clicking the input element should show the dropdown list and all its content */
+    await inputElement.click();
+    await expect(inputElement).toBeVisible();
+    await expect(dropdownOptionOne).toBeVisible();
+    await expect(dropdownOptionTwo).toBeVisible();
+    await expect(dropdownOptionThree).toBeVisible();
+    await expect(dropdownOptionFour).toBeVisible();
+
+    /* Add text and only Option 1 should be visible */
+    await inputElement.fill('Option 1');
+    await expect(inputElement).toBeVisible();
+    await expect(dropdownOptionOne).toBeVisible();
+    await expect(dropdownOptionTwo).not.toBeVisible();
+    await expect(dropdownOptionThree).not.toBeVisible();
+    await expect(dropdownOptionFour).not.toBeVisible();
+
+    /* Check diff on screenshot after adding text */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 });
