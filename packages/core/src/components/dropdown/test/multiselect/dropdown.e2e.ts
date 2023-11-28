a2a513778b76618890bf04efe8caff4bf14b0f88
alfredskeden <alfred.skedeback91@gmail.com>
2023-11-28 15:17:41 +0100

    test(dropdown): new tests for dropdown multiselect

--- packages/core/src/components/dropdown/test/multiselect/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/multiselect/dropdown.e2e.ts
@@ -6,105 +6,115 @@ const componentTestPath = 'src/components/dropdown/test/multiselect/index.html';
 test.describe('tds-dropdown-multiselect', () => {
   test('renders multiselect dropdown correctly', async ({ page }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    expect(dropdown).toBeTruthy();
+    const dropdown = page.getByTestId('tds-dropdown-testid');
+    await expect(dropdown).toHaveCount(1);
+
+    /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
-  test('should have a label outside with text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const labelOutside = page.locator('tds-dropdown > div[class="label-outside"]');
-    await expect(labelOutside).toHaveText(/Label text/);
-  });
-
-  test('to have helper text with correct text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const helperText = page.locator('tds-dropdown > div[class="helper"]');
-    await expect(helperText).toHaveText(/Helper text/);
-  });
-
-  test('have the placeholder="Placeholder" text', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const button = dropdown.locator('button');
-    const placeholder = button.locator('div[class="placeholder lg"]');
-    const textAttribute = await placeholder.textContent();
-    expect(textAttribute).toBe('Placeholder');
-  });
-
-  test('has lg size', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    expect(dropdownSelect).toBeTruthy();
-  });
-
-  test('clicking the dropdown opens the dropdown-list', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    const myEventSpy = await page.spyOnEvent('click');
-    await button.click();
-    const dropdownList = dropdown.locator('div[class="dropdown-list lg down label-outside open"]');
-    expect(myEventSpy).toHaveReceivedEvent();
-    expect(dropdownList).toBeTruthy();
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
-
-  test('clicking the dropdown opens the dropdown-list, then click on option 1', async ({
+  test('clicking the dropdown opens the dropdown-list, then click Option 1, should not close it', async ({
     page,
   }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    await button.click();
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-1"]');
+
+    /* click the button */
+    const placeholderElement = page.getByText(/Placeholder/);
+    await placeholderElement.click();
+
+    /* make sure dropdown list is visible */
+    const dropdownOptionOne = page.getByTestId(/tds-dropdown-option-1/);
+    await expect(dropdownOptionOne).toBeVisible();
+
+    /* check so only one "Option 1" text exists */
+    const option1Element = page.getByText(/Option 1/);
+    await expect(option1Element).toHaveCount(1);
+
+    /* Click Option 1 element */
     await dropdownOptionOne.click();
-    await expect(dropdown).toHaveAttribute('value', 'option-1');
+
+    /* Check so its dropdown list is not visible anymore and there now 2 "Option 1" texts in the dom */
+    await expect(dropdownOptionOne).toBeVisible();
+    await expect(option1Element).toHaveCount(2);
+
+    /* also check screenshot diff to make sure it says Option 1 */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
   test('clicking the dropdown opens the dropdown-list, then click on all the options', async ({
     page,
   }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    await button.click();
-    const dropdownOptionOne = dropdown.locator('tds-dropdown-option[value="option-1"]');
-    const dropdownOptionOneCheckbox = dropdownOptionOne.locator('tds-checkbox');
-    expect(dropdownOptionOneCheckbox).toBeTruthy();
+
+    /* click the button */
+    const placeholderElement = page.getByText(/Placeholder/);
+    await placeholderElement.click();
+
+    /* get all checkboxes */
+    const dropdownOptionOne = page.getByTestId(/tds-dropdown-option-1/).locator('tds-checkbox');
+    const dropdownOptionTwo = page.getByTestId(/tds-dropdown-option-2/).locator('tds-checkbox');
+    const dropdownOptionThree = page.getByTestId(/tds-dropdown-option-3/).locator('tds-checkbox');
+    const dropdownOptionFour = page.getByTestId(/tds-dropdown-option-4/).locator('tds-checkbox');
+    await expect(dropdownOptionOne).toHaveCount(1);
+    await expect(dropdownOptionTwo).toHaveCount(1);
+    await expect(dropdownOptionThree).toHaveCount(1);
+    await expect(dropdownOptionFour).toHaveCount(1);
+
+    /* check each one and see that it updates correctly */
     await dropdownOptionOne.click();
-    const dropdownOptionTwo = dropdown.locator('tds-dropdown-option[value="option-2"]');
-    const dropdownOptionTwoCheckbox = dropdownOptionTwo.locator('tds-checkbox');
-    expect(dropdownOptionTwoCheckbox).toBeTruthy();
+    await expect(dropdownOptionOne).toHaveAttribute('checked');
+    await expect(dropdownOptionTwo).not.toHaveAttribute('checked');
+    await expect(dropdownOptionThree).not.toHaveAttribute('checked');
+    await expect(dropdownOptionFour).not.toHaveAttribute('checked');
+    const inputText = page.getByText(/Option 1/);
+    await expect(inputText).toHaveCount(2);
+
     await dropdownOptionTwo.click();
-    const dropdownOptionThree = dropdown.locator('tds-dropdown-option[value="option-3"]');
-    const dropdownOptionThreeCheckbox = dropdownOptionThree.locator('tds-checkbox');
-    expect(dropdownOptionThreeCheckbox).toBeTruthy();
+    await expect(dropdownOptionOne).toHaveAttribute('checked');
+    await expect(dropdownOptionTwo).not.toHaveAttribute('checked');
+    await expect(dropdownOptionThree).not.toHaveAttribute('checked');
+    await expect(dropdownOptionFour).not.toHaveAttribute('checked');
+    await expect(inputText).toHaveCount(2);
+
     await dropdownOptionThree.click();
-    const dropdownOptionFour = dropdown.locator('tds-dropdown-option[value="option-4"]');
-    const dropdownOptionFourCheckbox = dropdownOptionFour.locator('tds-checkbox');
-    expect(dropdownOptionFourCheckbox).toBeTruthy();
-    await expect(dropdown).toHaveAttribute('value', 'option-1,option-3');
+    await expect(dropdownOptionOne).toHaveAttribute('checked');
+    await expect(dropdownOptionTwo).not.toHaveAttribute('checked');
+    await expect(dropdownOptionThree).toHaveAttribute('checked');
+    await expect(dropdownOptionFour).not.toHaveAttribute('checked');
+    const inputText2 = page.getByText(/Option 1,Option 3/, { exact: true });
+    await expect(inputText2).toHaveCount(1);
+
+    await dropdownOptionFour.click();
+    await expect(dropdownOptionOne).toHaveAttribute('checked');
+    await expect(dropdownOptionTwo).not.toHaveAttribute('checked');
+    await expect(dropdownOptionThree).toHaveAttribute('checked');
+    await expect(dropdownOptionFour).toHaveAttribute('checked');
+    const inputText3 = page.getByText(/Option 1,Option 3,Option 4/, { exact: true });
+    await expect(inputText3).toHaveCount(1);
+
+    /* also check screenshot diff to make sure */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
-  /** Skipping this test since the disabled option 2 still gets an event when its a tds-checkbox
-    TODO: CHECK ON THIS PROBLEM 
-  */
-  test.skip('multiselect clicking the dropdown opens the dropdown-list, then click an option 2 that is disabled should not close it', async ({
+  test('clicking the dropdown opens the dropdown-list, then click an option 2 that is disabled should not close it', async ({
     page,
   }) => {
     await page.goto(componentTestPath);
-    const dropdown = page.locator('tds-dropdown');
-    const dropdownSelect = dropdown.locator('div[class="dropdown-select lg"]');
-    const button = dropdownSelect.locator('button');
-    await button.click();
-    const dropdownOptionTwo = dropdown.locator('tds-dropdown-option[value="option-2"]');
-    const myEventSpy = await page.spyOnEvent('click');
+    const dropdownOptionTwo = page.getByTestId(/tds-dropdown-option-2/);
+    const placeholderElement = page.getByText(/Placeholder/);
+
+    /* before clicking dropdownlist should not be visible, the button should be */
+    await expect(placeholderElement).toBeVisible();
+    await expect(dropdownOptionTwo).not.toBeVisible();
+
+    /* after clicking dropdownlist should be visible, the button should also be */
+    await placeholderElement.click();
+    await expect(placeholderElement).toBeVisible();
+    await expect(dropdownOptionTwo).toBeVisible();
+
+    /* after clicking option 2 that is disabled list should be visible and also button should be */
     await dropdownOptionTwo.click();
-    expect(myEventSpy).not.toHaveReceivedEvent();
+    await expect(placeholderElement).toBeVisible();
+    await expect(dropdownOptionTwo).toBeVisible();
   });
 });
