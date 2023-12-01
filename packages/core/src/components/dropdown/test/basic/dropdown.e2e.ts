cca05b17f15d47c710d4bcc7bdce646d4f7fa480
Alfred Skedebäck <alfred.skedeback@umain.com>
2023-12-01 14:52:53 +0100

    test(dropdown): added unit tests (#435)
    * chore(dropdown): wip unit tests for no-result-text
    
    * test(dropdown): added tests
    
    * test(dropdown): updated tests after meeting to follow standards
    
    * test(dropdown): update so that label outside test work with also helpertext
    
    * test(dropdown): add some screenshot testing and update some test names
    
    * test(dropdown): update test to not use implementation details
    
    * test(dropdown): test correctly
    
    * test(dropdown): new tests for dropdown multiselect
    
    * test(dropdown): update all tests to have less implementation details
    
    * test(dropdown): update dropdown to use hidden and add basic dropdown
    
    * test(dropdown): update tests to be less implementation detail oriented
    
    ---------
    
    Co-authored-by: Default <alex.john.eneroth@gmail.om>

---
+++ packages/core/src/components/dropdown/test/basic/dropdown.e2e.ts
@@ -0,0 +1,50 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/dropdown/test/basic/index.html';
+
+test.describe('tds-dropdown-basic', () => {
+  test('renders basic dropdown correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const dropdown = page.getByTestId('tds-dropdown-testid');
+    await expect(dropdown).toHaveCount(1);
+
+    /* check diff in screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('should find label and not exist', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const labelText = page.getByText(/Label text/);
+    await expect(labelText).toHaveCount(0);
+  });
+
+  test('find helper text and check not exist', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const helperText = page.getByText(/Helper text/);
+    await expect(helperText).toHaveCount(0);
+  });
+
+  test('have the button to be visible', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const dropdownButton = page.getByRole('button').first();
+    await expect(dropdownButton).toBeVisible();
+  });
+
+  test('clicking the dropdown button opens the dropdown-list', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const dropdownButton = page.getByRole('button').first();
+    const dropdownListElementOne = page
+      .locator('tds-dropdown-option')
+      .filter({ hasText: 'Option 1' });
+    await expect(dropdownListElementOne).toBeHidden();
+    await dropdownButton.click();
+
+    /* before clicking dropdownlist should not be visible, the button should be */
+    await expect(dropdownButton).toBeVisible();
+    await expect(dropdownListElementOne).toBeVisible();
+
+    /* checks diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
