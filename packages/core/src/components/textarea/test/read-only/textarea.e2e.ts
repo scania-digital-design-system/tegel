7cc8e85e1ed554373d2f06dab62080cd5ce77dc0
Alfred Skedebäck <alfred.skedeback@umain.com>
2023-12-01 16:55:29 +0100

    test(textarea): added unit tests (#436)
    * test(textarea): adding unit tests
    
    * test(textarea): remove hydrated class check from tests
    
    * test(textarea): update tests with some comments on all the expect
    
    * test(textarea): update check on component to be truthy
    
    * test(textarea): update after pr comments
    
    * test(textarea): remove test for filling or clicking readonly textarea since it does not work
    
    * test(textarea): updates after reading article
    
    * test(textarea): added basic test and also update test to use getByRole
    
    * test(textarea): added screenshot for when adding text to textarea
    
    * test(textarea): add tests for label and update some screenshot names

---
+++ packages/core/src/components/textarea/test/read-only/textarea.e2e.ts
@@ -0,0 +1,33 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/textarea/test/read-only/index.html';
+
+test.describe.only('tds-textarea-read-only', () => {
+  test('renders read-only textarea correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const tdsTextarea = page.getByTestId('tds-textarea-testid');
+    await expect(tdsTextarea).toHaveCount(1);
+
+    /* Expect the tds-textarea to have the read-only attribute */
+    await expect(tdsTextarea).toHaveAttribute('read-only');
+
+    /* Expect no diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('read-only textarea - native textarea should have readonly attribute', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textarea = page.getByRole('textbox');
+
+    /* Expect the textarea within tds-textarea to have the readonly attribute */
+    await expect(textarea).toHaveAttribute('readonly');
+  });
+
+  test('be able to find label if "outside" is set', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const textareaLabel = page.getByText('Label');
+    await expect(textareaLabel).toHaveCount(1);
+    await expect(textareaLabel).toBeVisible();
+  });
+});
