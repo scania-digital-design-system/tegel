95603415083cb944da45a2dfe1eb35e0d444ac83
Alfred Skedebäck <alfred.skedeback@umain.com>
2023-12-05 09:52:03 +0100

    test(table): added unit tests (#438)
    * test(table): added unit tests
    
    * test(table): added a bunch more unit tests
    
    * test(table): change toBeTruthy to toHaveCount instead
    
    * test(table): update all tests to follow less implementation details
    
    * test(table): update tests to reflect hidden
    
    * chore: typo correction just to trigger SonarCloud
    
    * test(table): rename basic to default
    
    * test(table): update name of test in multiselect
    
    ---------
    
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

---
+++ packages/core/src/components/table/table/test/default/table.e2e.ts
@@ -0,0 +1,57 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/table/table/test/default/index.html';
+
+test.describe('tds-table-default', () => {
+  test('renders default table correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const tableComponent = page.getByRole('table');
+    await expect(tableComponent).toHaveCount(1);
+
+    /** Check screenshot diff */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('table has four columns', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const tableHeaderCells = page.locator('tds-header-cell');
+    await expect(tableHeaderCells).toHaveCount(4);
+  });
+
+  test('columns are: Truck type, Driver name, Country, Mileage', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Expect each header to be visible */
+    await expect(page.getByText('Truck type')).toBeVisible();
+    await expect(page.getByText('Driver name')).toBeVisible();
+    await expect(page.getByText('Country')).toBeVisible();
+    await expect(page.getByText('Mileage')).toBeVisible();
+  });
+
+  test('Row should contain the correct number of rows with', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Expect the number of rows to be correct amount */
+    const tableBodyRows = page.locator('tds-table-body-row');
+    await expect(tableBodyRows).toHaveCount(6);
+  });
+
+  test('table has the correct text inside each cell', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Checks all rows to see that they have the correct amount of tds-body-cells with values provided */
+    const promises = [];
+    for (let i = 1; i <= 8; i++) {
+      const tableBodyCellHasText = page
+        .locator('tds-body-cell')
+        .filter({ hasText: `Test value ${i}` });
+      promises.push(expect(tableBodyCellHasText).toHaveCount(3));
+      promises.push(expect(tableBodyCellHasText.first()).toBeVisible());
+      promises.push(expect(tableBodyCellHasText.nth(1)).toBeVisible());
+      promises.push(expect(tableBodyCellHasText.nth(2)).toBeVisible());
+    }
+
+    await Promise.all(promises);
+  });
+});
---
+++ packages/core/src/components/table/table/test/sorting/table.e2e.ts
@@ -0,0 +1,43 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/table/table/test/sorting/index.html';
+
+test.describe('tds-table-sorting', () => {
+  test('renders sorting table correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const tableComponent = page.getByRole('table');
+    await expect(tableComponent).toHaveCount(1);
+
+    /* Check for diffs in screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('table has header "Sorting"', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Search for header by text and see if it exists */
+    const tdsTableToolbarCaption = page.getByText('Sorting');
+    await expect(tdsTableToolbarCaption).toHaveCount(1);
+    await expect(tdsTableToolbarCaption).toBeVisible();
+  });
+
+  test('column headers are clickable', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const myEventSpy = await page.spyOnEvent('tdsSort');
+    const truckTypeHeader = page.getByText('Truck type');
+    await truckTypeHeader.click();
+    expect(myEventSpy).toHaveReceivedEventTimes(1);
+    const driverNameHeader = page.getByText('Driver name');
+    await driverNameHeader.click();
+    expect(myEventSpy).toHaveReceivedEventTimes(2);
+    const countryHeader = page.getByText('Country');
+    await countryHeader.click();
+    expect(myEventSpy).toHaveReceivedEventTimes(3);
+    const mileageHeader = page.getByText('Mileage');
+    await mileageHeader.click();
+    expect(myEventSpy).toHaveReceivedEventTimes(4);
+
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
