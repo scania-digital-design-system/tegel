f19203ad0f5148c2054808985809abd9532e4d26
alfredskeden <alfred.skedeback91@gmail.com>
2023-12-11 14:46:10 +0100

    test(footer): remove only for test describe

--- packages/core/src/components/footer/test/default/footer.e2e.ts
+++ packages/core/src/components/footer/test/default/footer.e2e.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test';
 
 const componentTestPath = 'src/components/footer/test/default/index.html';
 
-test.describe.only('tds-footer-default', () => {
+test.describe('tds-footer-default', () => {
   test('renders default footer correctly', async ({ page }) => {
     await page.goto(componentTestPath);
     const footerComponent = page.locator('footer');
