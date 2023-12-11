364e7f1f0a4037da00e1c29f9f7ccca796ee8fc0
alfredskeden <alfred.skedeback91@gmail.com>
2023-12-11 16:31:55 +0100

    test(card): initial tests for card component

---
+++ packages/core/src/components/card/test/clickable/card.e2e.ts
@@ -0,0 +1,26 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/card/test/clickable/index.html';
+
+test.describe('tds-card-clickable', () => {
+  test('renders clickable card correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const cardButton = page.getByRole('button');
+    await cardButton.hover();
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('card should contain a button that is clickable', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const cardButton = page.getByRole('button');
+    await expect(cardButton).toHaveCount(1);
+    await expect(cardButton).toBeVisible();
+
+    const myEventSpy = await page.spyOnEvent('click');
+    await cardButton.click();
+    expect(myEventSpy).toHaveReceivedEvent();
+  });
+});
