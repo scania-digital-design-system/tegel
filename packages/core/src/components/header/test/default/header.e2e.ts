d664ae8c0dfe15a4540d15f0b2757126594f56b7
alfredskeden <alfred.skedeback91@gmail.com>
2023-12-18 12:07:13 +0100

    test(header): update some locators to better ones and tests cases

--- packages/core/src/components/header/test/default/header.e2e.ts
+++ packages/core/src/components/header/test/default/header.e2e.ts
@@ -17,21 +17,37 @@ test.describe('tds-header-default', () => {
   test('title exists and is "Example: default"', async ({ page }) => {
     await page.goto(componentTestPath);
     const headerComponentHeaderText = page.getByText('Example: default');
-    await expect(headerComponentHeaderText).toBeVisible();
     await expect(headerComponentHeaderText).toHaveCount(1);
+    await expect(headerComponentHeaderText).toBeVisible();
   });
 
   test('luncher button icon exists', async ({ page }) => {
     await page.goto(componentTestPath);
-    const headerComponentLuncherIcon = page.getByRole('button');
-    await expect(headerComponentLuncherIcon).toBeVisible();
-    await expect(headerComponentLuncherIcon).toHaveCount(1);
+    const headerComponentLuncherButton = page.getByRole('button');
+    await expect(headerComponentLuncherButton).toHaveCount(1);
+    await expect(headerComponentLuncherButton).toBeVisible();
   });
 
   test('brand label with link exists', async ({ page }) => {
     await page.goto(componentTestPath);
-    const headerComponentBrandLink = page.getByRole('link');
-    await expect(headerComponentBrandLink).toBeVisible();
+    const headerComponentBrandLink = page.getByLabel('Scania - red gryphon on blue shield');
     await expect(headerComponentBrandLink).toHaveCount(1);
+    await expect(headerComponentBrandLink).toBeVisible();
+  });
+
+  test('launcher should open on click', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const headerComponentLuncherButton = page.getByRole('button');
+    await headerComponentLuncherButton.click();
+
+    const headerLuncherList = page
+      .getByRole('listitem')
+      .filter({ has: page.getByRole('heading') })
+      .getByRole('list');
+    await expect(headerLuncherList).toHaveCount(1);
+    await expect(headerLuncherList).toBeVisible();
+
+    /** Check screenshot diff */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 });
