2d0bc348527c6ace24db0439ac34cbdf3c0d206b
alfredskeden <alfred.skedeback91@gmail.com>
2023-12-18 11:13:56 +0100

    test(header): fix some tests locators

--- packages/core/src/components/header/test/default/header.e2e.ts
+++ packages/core/src/components/header/test/default/header.e2e.ts
@@ -8,25 +8,27 @@ test.describe('tds-header-default', () => {
     await page.goto(componentTestPath);
     const headerComponent = page.getByRole('navigation');
     await expect(headerComponent).toHaveCount(1);
+    await expect(headerComponent).toBeVisible();
 
     /** Check screenshot diff */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
-  test('Header title exists and is "Example: default"', async ({ page }) => {
+  test('title exists and is "Example: default"', async ({ page }) => {
     await page.goto(componentTestPath);
     const headerComponentHeaderText = page.getByText('Example: default');
     await expect(headerComponentHeaderText).toBeVisible();
+    await expect(headerComponentHeaderText).toHaveCount(1);
   });
 
-  test('Header luncher icon exists', async ({ page }) => {
+  test('luncher button icon exists', async ({ page }) => {
     await page.goto(componentTestPath);
-    const headerComponentLuncherIcon = page.getByRole('img');
+    const headerComponentLuncherIcon = page.getByRole('button');
     await expect(headerComponentLuncherIcon).toBeVisible();
     await expect(headerComponentLuncherIcon).toHaveCount(1);
   });
 
-  test('Header brand label with link exists', async ({ page }) => {
+  test('brand label with link exists', async ({ page }) => {
     await page.goto(componentTestPath);
     const headerComponentBrandLink = page.getByRole('link');
     await expect(headerComponentBrandLink).toBeVisible();
