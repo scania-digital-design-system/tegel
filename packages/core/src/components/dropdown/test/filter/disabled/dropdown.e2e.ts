d40d54d44835ff41128509abc86c3eee3d461df9
mJarsater <62651103+mJarsater@users.noreply.github.com>
2024-01-24 13:58:30 +0100

    fix(dropdown-filter): disabled can be opened (#487)
    * Toggle the open state on icon press only is disabled !== true
    
    * Added tests for click on icon when dropdown is disabled
    
    * Corrected border bottom on disabled filter dropdown
    
    * Seperated and updated tests
    
    * Cleaned up tests to only produce one snapshot per test
    
    * removed local mac script
    
    * fix(dropdown): gramma correction and fixing failing test
    
    * fix: test images update
    
    ---------
    
    Co-authored-by: Tim <tim.romberg@scania.com>
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

---
+++ packages/core/src/components/dropdown/test/filter/disabled/dropdown.e2e.ts
@@ -0,0 +1,46 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/dropdown/test/filter/disabled/index.html';
+
+test.describe('tds-dropdown-filter-disabled', () => {
+  test('clicking the dropdown icon does not open the dropdown-list', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const dropdownButtonIcon = page.locator('tds-icon');
+
+    const dropdownListElementOne = page
+      .locator('tds-dropdown-option')
+      .filter({ hasText: 'Option 1' });
+
+    await expect(dropdownListElementOne).toBeHidden();
+    /* force a click on the icon. Since the icon is inside of a disabled button playwright will error if it is not forced */
+    await dropdownButtonIcon.click({
+      force: true,
+    });
+
+    /* no child elements should be visible. */
+    await expect(dropdownListElementOne).toBeHidden();
+
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+  test('clicking the dropdown button does not open the dropdown-list', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const dropdownButton = page.getByRole('textbox').first();
+
+    /* check that the icon is inside of a disabled element */
+    await expect(dropdownButton).toBeDisabled();
+
+    const dropdownListElementOne = page
+      .locator('tds-dropdown-option')
+      .filter({ hasText: 'Option 1' });
+
+    await expect(dropdownListElementOne).toBeHidden();
+    /* force a click on the icon. Since the icon is inside of a disabled button playwright will error if it is not forced */
+    await dropdownButton.click({
+      force: true,
+    });
+
+    /* no child elements should be visible. */
+    await expect(dropdownListElementOne).toBeHidden();
+  });
+});
