b5785bde7a2497ee82010e39d0a05dcc2292991a
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-01-30 09:37:04 +0100

    test(slider): added unit tests (#489)
    * test(slider): added default test case
    
    * test(slider): added default test case
    
    * test(slider): read-only unit test
    
    * test(slider): disabled unit test
    
    * fix: test images update
    
    * fix: min, max, default value, thumb size improvement
    
    * fix: disabled and read-only updates
    
    * fix: screenshots files
    
    * test(slider-disabled): update screenshot and disabled check

---
+++ packages/core/src/components/slider/test/read-only/slider.e2e.ts
@@ -0,0 +1,29 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/slider/test/read-only/index.html';
+test.describe('tds-slider-read-only', () => {
+  test('read-only is true', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const slider = page.locator('tds-slider');
+    await expect(slider).toHaveAttribute('read-only');
+  });
+
+  test('is not disabled', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const sliderThumb = page.locator('.tds-slider__thumb-inner');
+    const sliderCursorStyle = await sliderThumb.evaluate((style) => getComputedStyle(style).cursor);
+    expect(sliderCursorStyle).toBe('pointer');
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('slider can not be clicked on', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const sliderThumb = page.locator('.tds-slider__thumb-inner');
+    const sliderThumbStyle = await sliderThumb.evaluate(
+      (style) => getComputedStyle(style).pointerEvents,
+    );
+    expect(sliderThumbStyle).toBe('none');
+  });
+});
