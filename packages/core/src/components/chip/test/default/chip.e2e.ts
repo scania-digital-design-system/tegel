115577eadacfe72c76b8b9e841ffd6933d4e9371
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-02-27 16:58:10 +0100

    feat(chip): initial default unit test

---
+++ packages/core/src/components/chip/test/default/chip.e2e.ts
@@ -0,0 +1,30 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/chip/test/default/index.html';
+
+test.describe('tds-chip-default', () => {
+  test('renders default chip correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const chip = page.locator('tds-chip');
+    await expect(chip).toHaveCount(1);
+
+    const labelElement = page.locator('tds-chip label'); // Target label underneath chip
+    expect(labelElement).toHaveText('Label');
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('expect chip to be of the role button', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const chipRole = page.getByRole('button');
+    await expect(chipRole).toHaveCount(1);
+  });
+
+  //   test('Check so that height is correct to lg/default measurements', async ({ page }) => {
+  //     const chip = page.getByText('Label', { exact: true });
+  //     const chipHeight = await chip.evaluate((style) => getComputedStyle(style).height);
+  //     expect(chipHeight).toBe('32px');
+  //   });
+});
