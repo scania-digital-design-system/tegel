b688861db84f1b1313813e27c617d5e17cda678d
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-02-28 11:45:06 +0100

    test(chip): add initial test for chip type radio

---
+++ packages/core/src/components/chip/test/radio/chip.e2e.ts
@@ -0,0 +1,21 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/chip/test/radio/index.html';
+
+test.describe('tds-chip-default', () => {
+  test('renders default chip correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const chip1 = page.getByText('Label 1', { exact: true });
+    await expect(chip1).toHaveCount(1);
+
+    const chip2 = page.getByText('Label 2', { exact: true });
+    await expect(chip2).toHaveCount(1);
+
+    const chip3 = page.getByText('Label 3', { exact: true });
+    await expect(chip3).toHaveCount(1);
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
