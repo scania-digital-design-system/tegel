d2c6df5e05c50ab2984931ef93401828021e8fc0
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-03-06 10:33:27 +0100

    test(chip): add tests for type radio

--- packages/core/src/components/chip/test/radio/chip.e2e.ts
+++ packages/core/src/components/chip/test/radio/chip.e2e.ts
@@ -24,4 +24,19 @@ test.describe('tds-chip-default', () => {
     const chipRole = page.getByRole('radio');
     await expect(chipRole).toHaveCount(3);
   });
+
+  test('Check so that height is correct to lg/default measurements', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const chip1 = page.locator('tds-chip label').filter({ hasText: `Label 1` });
+    const chipHeight1 = await chip1.evaluate((style) => getComputedStyle(style).height);
+    expect(chipHeight1).toBe('32px');
+
+    const chip2 = page.locator('tds-chip label').filter({ hasText: `Label 2` });
+    const chipHeight2 = await chip2.evaluate((style) => getComputedStyle(style).height);
+    expect(chipHeight2).toBe('32px');
+
+    const chip3 = page.locator('tds-chip label').filter({ hasText: `Label 3` });
+    const chipHeight3 = await chip3.evaluate((style) => getComputedStyle(style).height);
+    expect(chipHeight3).toBe('32px');
+  });
 });
