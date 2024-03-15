777f558f47c34eef211f2b239d2c929b943c65ec
jst1ws <jernej.strazisar@scania.com>
2024-03-15 08:37:01 +0100

    feat: export the value accessor generated by stencil

--- packages/angular/projects/components/src/lib/components.module.ts
+++ packages/angular/projects/components/src/lib/components.module.ts
@@ -1,10 +1,26 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { DIRECTIVES } from './stencil-generated';
 import { defineCustomElements } from '@scania/tegel/loader';
+import { TextValueAccessor } from './stencil-generated/text-value-accessor';
+import { BooleanValueAccessor } from './stencil-generated/boolean-value-accessor';
+import { NumericValueAccessor } from './stencil-generated/number-value-accessor';
+import { RadioValueAccessor } from './stencil-generated/radio-value-accessor';
 
 @NgModule({
-  declarations: [...DIRECTIVES],
-  exports: [...DIRECTIVES],
+  declarations: [
+    ...DIRECTIVES,
+    BooleanValueAccessor,
+    NumericValueAccessor,
+    RadioValueAccessor,
+    TextValueAccessor,
+  ],
+  exports: [
+    ...DIRECTIVES,
+    BooleanValueAccessor,
+    NumericValueAccessor,
+    RadioValueAccessor,
+    TextValueAccessor,
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
--- packages/angular/projects/components/src/public-api.ts
+++ packages/angular/projects/components/src/public-api.ts
@@ -4,4 +4,8 @@
 
 export * from './lib/components.module';
 export { DIRECTIVES } from './lib/stencil-generated';
+export { TextValueAccessor } from './lib/stencil-generated/text-value-accessor';
+export { BooleanValueAccessor } from './lib/stencil-generated/boolean-value-accessor';
+export { NumericValueAccessor } from './lib/stencil-generated/number-value-accessor';
+export { RadioValueAccessor } from './lib/stencil-generated/radio-value-accessor';
 export * from './lib/stencil-generated/components';
