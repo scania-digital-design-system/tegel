535d86a4d44e9eb9e674f0828fcce172771b6fcb
jst1ws <jernej.strazisar@scania.com>
2024-03-15 14:04:23 +0100

    feat: enable the angular forms integration for tds-dropdown

--- packages/angular/projects/components/src/public-api.ts
+++ packages/angular/projects/components/src/public-api.ts
@@ -8,4 +8,5 @@ export { TextValueAccessor } from './lib/stencil-generated/text-value-accessor';
 export { BooleanValueAccessor } from './lib/stencil-generated/boolean-value-accessor';
 export { NumericValueAccessor } from './lib/stencil-generated/number-value-accessor';
 export { RadioValueAccessor } from './lib/stencil-generated/radio-value-accessor';
+export { TdsDropdownValueAccessor } from './lib/directives/tds-dropdown-value-accessor';
 export * from './lib/stencil-generated/components';
