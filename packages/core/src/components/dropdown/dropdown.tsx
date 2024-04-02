4f8128f02eb371d32b2c55e8e9938524ba6cdd55
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-04-02 16:35:05 +0200

    fix(dropdown): position

--- packages/core/src/components/dropdown/dropdown.tsx
+++ packages/core/src/components/dropdown/dropdown.tsx
@@ -594,16 +594,8 @@ export class TdsDropdown {
                   {this.value?.length ? this.getValue() : this.placeholder}
                 </div>
                 <tds-icon
-                  tabIndex={0}
-                  role="button"
                   aria-label="Open/Close dropdown"
                   svgTitle="Open/Close dropdown"
-                  onClick={this.handleToggleOpen}
-                  onKeyDown={(event) => {
-                    if (event.key === 'Enter') {
-                      this.handleToggleOpen();
-                    }
-                  }}
                   class={`menu-icon ${this.open ? 'open' : 'closed'}`}
                   name="chevron_down"
                   size="16px"
