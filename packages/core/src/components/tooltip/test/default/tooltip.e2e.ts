4a5ecf3144326ff9f2fa4932faef61a11acb9c6d
Tim <tim.romberg@scania.com>
2024-04-04 08:45:49 +0200

    test(tooltip): test cases for tooltip added

---
+++ packages/core/src/components/tooltip/test/default/tooltip.e2e.ts
@@ -0,0 +1,55 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/tooltip/test/default/index.html';
+
+test.describe('tds-tooltip', () => {
+  test('renders the tooltip correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('tooltip appears on button hover', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    // Select the button that triggers the tooltip on hover
+    const button = page.locator('tds-button#button-1');
+
+    // Use Playwright's hover method to simulate moving the mouse over the button
+    await button.hover();
+
+    const tooltip = page.locator('text=Text inside Tooltip');
+
+    // Assert that the tooltip is visible after hovering over the button
+    await expect(tooltip).toBeVisible();
+
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+
+  test('tooltip contains correct HTML content on hover', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    // Hover over the button to trigger the tooltip
+    const button = page.locator('tds-button#button-1');
+    await button.hover();
+
+    const tooltipParagraph = page.locator('.tooltip-paragraph');
+
+    // Verify the paragraph is visible as part of the tooltip
+    await expect(tooltipParagraph).toBeVisible();
+
+    // Fetch the inner HTML of the tooltip paragraph
+    let innerHtml = await tooltipParagraph.innerHTML();
+
+    // Normalize whitespace in the inner HTML for comparison
+    innerHtml = innerHtml.replace(/\s+/g, ' ').trim();
+
+    // Define the expected HTML content, ensuring to trim any potential whitespace for a precise match
+    const expectedHtmlContent =
+      'Paragraph tag inside of Tooltip with <b>bold</b> and <i>italic</i> tags too.';
+
+    // Perform the comparison to verify the tooltip's content
+    expect(innerHtml).toEqual(expectedHtmlContent);
+  });
+});
