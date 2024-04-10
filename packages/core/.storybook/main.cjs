53359a4016e2b083e4f2e473c548564d6e6ba153
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-04-10 09:04:54 +0200

    fix: add mdx extensions stories back

--- packages/core/.storybook/main.cjs
+++ packages/core/.storybook/main.cjs
@@ -15,12 +15,12 @@ if (process.env.STORYBOOK_ENV === 'dev') {
 
 function loadStories() {
   // Gather all story files synchronously
-  const storyFiles = glob.sync(path.resolve(__dirname, '../src/**/*.stories.@(js|jsx|ts|tsx)'));
+  const storyFiles = glob.sync(path.resolve(__dirname, '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'));
 
   // If in development environment, return all story files
   // Otherwise, exclude stories from the _beta folder
-  return process.env.STORYBOOK_ENV === 'dev' 
-    ? storyFiles 
+  return process.env.STORYBOOK_ENV === 'dev'
+    ? storyFiles
     : storyFiles.filter(file => !file.includes('/_beta/'));
 }
 
