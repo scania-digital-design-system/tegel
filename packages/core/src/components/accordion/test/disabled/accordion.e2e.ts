c4ca9a73ecb5df91cc91cf0ed87aabf910518b99
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-04-22 09:25:41 +0200

    test(accordion): update test for disabled state

--- packages/core/src/components/accordion/test/disabled/accordion.e2e.ts
+++ packages/core/src/components/accordion/test/disabled/accordion.e2e.ts
@@ -30,8 +30,40 @@ test.describe.parallel('tds-accordion', () => {
     await page.goto(componentTestPath);
     const accordionSecondItem = page.getByTestId('second-item');
 
+    // Define selector for second accordion item button since
+    // the disabled attribute does not propagate the same when using 'header' slot
+    const accordionSecondItemButton = accordionSecondItem.getByRole('button');
+
     // Expect second accordion item to be disabled
-    await expect(accordionSecondItem).toHaveAttribute('disabled', '');
+    await expect(accordionSecondItemButton).toBeDisabled();
+  });
+
+  test('cursor should be not-allowed on disabled accordion items', async ({ page }) => {
+    // Define selector for first accordion item
+    await page.goto(componentTestPath);
+    const accordionFirstItem = page.getByTestId('first-item');
+    const accordionFirstItemButton = accordionFirstItem.getByRole('button');
+
+    // Hover first accordion item
+    const accordionCursorFirstItem = await accordionFirstItemButton.evaluate(
+      (style) => getComputedStyle(style).cursor,
+    );
+
+    // Expect cursor to be pointer
+    await expect(accordionCursorFirstItem).toBe('not-allowed');
+
+    // Define selector for second accordion item
+    await page.goto(componentTestPath);
+    const accordionSecondItem = page.getByTestId('second-item');
+    const accordionSecondItemButton = accordionSecondItem.getByRole('button');
+
+    // Hover second accordion item
+    const accordionCursorSecondItem = await accordionSecondItemButton.evaluate(
+      (style) => getComputedStyle(style).cursor,
+    );
+
+    // Expect cursor to be pointer
+    await expect(accordionCursorSecondItem).toBe('not-allowed');
   });
 
   test('does not fire tdsToggle event on click on disabled accordion', async ({ page }) => {
