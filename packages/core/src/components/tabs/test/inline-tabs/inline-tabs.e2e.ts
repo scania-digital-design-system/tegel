336c9b49339e588cce464479a34e23dd33502efd
Tim <tim.romberg@scania.com>
2024-04-29 17:19:21 +0200

    test(tabs): reveiw feedback and refactoring

--- packages/core/src/components/tabs/test/inline-tabs/inline-tabs.e2e.ts
+++ packages/core/src/components/tabs/test/inline-tabs/inline-tabs.e2e.ts
@@ -5,105 +5,76 @@ import { expect } from '@playwright/test';
 const componentTestPath = 'src/components/tabs/test/inline-tabs/index.html';
 
 test.describe.parallel('tds-inline-tabs', () => {
+  let inlineTabs;
+  let firstTab;
+  let firstTabDiv;
+  let secondTab;
+  let secondTabDiv;
+  let thirdTab;
+  let thirdTabDiv;
+  let fourthTab;
+
   test.beforeEach(async ({ page }) => {
     // Navigate to the component test page before each test
     await page.goto(componentTestPath);
+    inlineTabs = page.locator('tds-inline-tabs');
+    firstTab = page.locator('button', { hasText: 'First tab' });
+    secondTab = page.locator('button', { hasText: 'Second tab is much longer' });
+    thirdTab = page.locator('button', { hasText: 'Third Tab' });
+    fourthTab = page.locator('button', { hasText: 'Fourth Tab' });
+    // Divs inside tabs specifically for click interactions
+    firstTabDiv = page.locator('tds-inline-tab:has-text("First tab") >> div');
+    secondTabDiv = page.locator('tds-inline-tab:has-text("Second tab is much longer") >> div');
+    thirdTabDiv = page.locator('tds-inline-tab:has-text("Third Tab") >> div');
   });
 
-  test('renders inline-tabs correctly', async ({ page }) => {
-    const inlineTabs = page.locator('tds-inline-tabs');
+  test('renders inline-tabs correctly', async () => {
     await expect(inlineTabs).toHaveCount(1);
-
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    await expect(inlineTabs.page()).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
-  test('selected tab index should be 0', async ({ page }) => {
-    const selectedIndex = await page.locator('tds-inline-tabs').getAttribute('selected-index');
+  test('selected tab index should be 0', async () => {
+    const selectedIndex = await inlineTabs.getAttribute('selected-index');
     expect(selectedIndex).toBe('0');
   });
 
-  test('renders the correct tabs with expected labels', async ({ page }) => {
-    // Check for the first tab
-    const firstTab = page.locator('button', { hasText: 'First tab' });
-    const secondTab = page.locator('button', { hasText: 'Second tab is much longer' });
-    const thirdTab = page.locator('button', { hasText: 'Third Tab' });
-    const fourthTab = page.locator('button', { hasText: 'Fourth tab' });
-
+  test('renders the correct tabs with expected labels', async () => {
     await expect(firstTab).toBeVisible();
     await expect(secondTab).toBeVisible();
     await expect(thirdTab).toBeVisible();
     await expect(fourthTab).toBeVisible();
   });
 
-  test('First tab is selected by default', async ({ page }) => {
-    const inlineTabs = page.locator('tds-inline-tabs');
-    // Check if the selected-index attribute is set to 0
+  test('First tab is selected by default', async () => {
     await expect(inlineTabs).toHaveAttribute('selected-index', '0');
   });
 
-  test('Second tab and Third tab are not selected', async ({ page }) => {
-    const inlineTabs = page.locator('tds-inline-tabs');
-    const secondTab = page.locator('button', { hasText: 'Second tab is much longer' });
-    // Check if the selected-index attribute is not set to 1 or 2
+  test('Second tab and Third tab are not selected', async () => {
     await expect(secondTab).toBeVisible();
     await expect(inlineTabs).not.toHaveAttribute('selected-index', '2');
   });
 
-  test('Hover over Second tab changes cursor to pointer', async ({ page }) => {
-    const secondTab = page.locator('button', { hasText: 'Second tab is much longer' });
+  test('Hover over tabs changes cursor', async () => {
     await secondTab.hover();
     await expect(secondTab).toHaveCSS('cursor', 'pointer');
-  });
-
-  test('Hover over Third tab changes cursor to pointer', async ({ page }) => {
-    const thirdTab = page.locator('button', { hasText: 'Third Tab' });
     await thirdTab.hover();
     await expect(thirdTab).toHaveCSS('cursor', 'pointer');
-  });
-
-  test('Hover over Fourth tab changes cursor to not-allowed', async ({ page }) => {
-    const fourthTab = page.locator('button', { hasText: 'Fourth Tab' });
     await fourthTab.hover();
     await expect(fourthTab).toHaveCSS('cursor', 'not-allowed');
   });
 
-  test('Click on Second tab selects it and updates selected-index', async ({ page }) => {
-    const inlineTabs = page.locator('tds-inline-tabs');
-
-    // Locate the second tab and the div inside it
-    const firstTab = page.locator('tds-inline-tab:has-text("First tab") >> div');
-    const secondTab = page.locator('tds-inline-tab:has-text("Second tab is much longer") >> div');
-    // Click on the second tab
-    await secondTab.click({ force: true });
-
-    // Assert that the 'selected-index' is now '1'
+  test('Click on Second tab selects it and updates selected-index', async () => {
+    await secondTabDiv.click({ force: true });
     await expect(inlineTabs).toHaveAttribute('selected-index', '1', { timeout: 5000 });
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-
-    // Additional checks for tab selection state
-    // const firstTab = page.locator('tds-inline-tab div');
-    // Check if the first tab no longer has the 'selected' class
-    await expect(firstTab).not.toHaveClass(/selected/);
-    // Check if the second tab now has the 'selected' class
-    await expect(secondTab).toHaveClass(/selected/);
+    await expect(inlineTabs.page()).toHaveScreenshot({ maxDiffPixels: 0 });
+    await expect(firstTabDiv).not.toHaveClass(/selected/);
+    await expect(secondTabDiv).toHaveClass(/selected/);
   });
 
-  test('Click on Third tab selects it and updates selected-index', async ({ page }) => {
-    const inlineTabs = page.locator('tds-inline-tabs');
-
-    // Locate the third tab and the div inside it
-    const secondTab = page.locator('tds-inline-tab:has-text("Second tab is much longer") >> div');
-    const thirdTab = page.locator('tds-inline-tab:has-text("Third tab") >> div');
-    // Click on the third tab
-    await thirdTab.click({ force: true });
-
-    // Assert that the 'selected-index' is now '2'
+  test('Click on Third tab selects it and updates selected-index', async () => {
+    await thirdTabDiv.click({ force: true });
     await expect(inlineTabs).toHaveAttribute('selected-index', '2', { timeout: 5000 });
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-
-    // Check if the third tab now has the 'selected' class
-    await expect(secondTab).not.toHaveClass(/selected/);
-    await expect(thirdTab).toHaveClass(/selected/);
+    await expect(secondTabDiv).not.toHaveClass(/selected/);
+    await expect(thirdTabDiv).toHaveClass(/selected/);
   });
 });
