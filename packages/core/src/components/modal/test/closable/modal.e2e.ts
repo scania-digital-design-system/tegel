9d0e7f3863284cc8f02b6b3bae3693f5e63e5d4f
mistermalm <45939401+mistermalm@users.noreply.github.com>
2024-05-29 16:54:18 +0200

    feat(modal): Expose a closable property to be able to hide x icon (#619)
    * feat(modal): control visibility of the close button
    
    * fix(modal): closable storybook default value
    
    * fix(table): better description for closable
    
    * fix(modal): closable default true
    
    * fix(modal): readme update
    
    * fix(table): revert
    
    * test(modal): closable -> true/false/undefined
    
    * fix(modal): true if undefined
    
    ---------
    
    Co-authored-by: Jonatan Malm <jonatan.malm@rhyme.se>

---
+++ packages/core/src/components/modal/test/closable/modal.e2e.ts
@@ -0,0 +1,29 @@
+import { E2EPage, test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+test.describe.parallel('tds-modal-closable', () => {
+  const isClosable = async (page: E2EPage, id: string) => {
+    // get modal by id
+    const modalComponent = page.getByTestId(id);
+    expect(modalComponent).toHaveCount(1);
+    // wait for it to show
+    await modalComponent.waitFor({ state: 'visible' });
+    // get close button within that modal
+    const tdsModalCloseButton = modalComponent.locator('.tds-modal-close');
+    // expect close button is visible, unless false
+    await expect(tdsModalCloseButton).toHaveCount(1);
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  };
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('src/components/modal/test/closable/index.html');
+  });
+
+  test('x button is visible', async ({ page }) => {
+    await isClosable(page, 'tds-modal-testid-0');
+  });
+
+  test('x button defaults to true', async ({ page }) => {
+    await isClosable(page, 'tds-modal-testid-1');
+  });
+});
