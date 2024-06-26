0a3b3ba9b5448ce88e2eb1879b672f1cd20c7441
Jonatan Malm <jonatan.malm@rhyme.se>
2024-06-26 13:03:00 +0200

    fix(grid): splitted fixed and fluid stories

--- packages/core/src/stories/foundations/grid/grid.stories.tsx
+++ packages/core/src/stories/foundations/grid/grid.stories.tsx
@@ -5,18 +5,6 @@ export default {
   parameters: {
     layout: 'fullscreen',
   },
-  args: {
-    fixedGrid: false,
-  },
-  argTypes: {
-    fixedGrid: {
-      name: 'Fixed grid',
-      description: 'Set grid to fixed',
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
 };
 
 // Styling for grid templates
@@ -37,12 +25,37 @@ const style = formatHtmlPreview(`
   </style>
 `);
 
-const GridFluidTemplate = ({ fixedGrid }) =>
+const GridFixedTemplate = () =>
   formatHtmlPreview(`
-    <h4>Grid New</h4>
+    <h4>Fixed</h4>
     
     <div class="tds-grid-container">
-      <div class="${fixedGrid ? 'tds-grid-fixed' : 'tds-grid-fluid'}">
+      <div class="tds-grid-fixed">
+        <div class="tds-grid-item">1</div>
+        <div class="tds-grid-item">2</div>
+        <div class="tds-grid-item">3</div>
+        <div class="tds-grid-item">4</div>
+        <div class="tds-grid-item">5</div>
+        <div class="tds-grid-item">6</div>
+        <div class="tds-grid-item">7</div>
+        <div class="tds-grid-item">8</div>
+        <div class="tds-grid-item">9</div>
+        <div class="tds-grid-item">10</div>
+        <div class="tds-grid-item">11</div>
+        <div class="tds-grid-item">12</div>
+      </div>  
+    </div>
+    ${style}
+  `);
+
+export const Fixed = GridFixedTemplate.bind({});
+
+const GridFluidTemplate = () =>
+  formatHtmlPreview(`
+    <h4>Fluid</h4>
+    
+    <div class="tds-grid-container">
+      <div class="tds-grid-fluid">
         <div class="tds-grid-item">1</div>
         <div class="tds-grid-item">2</div>
         <div class="tds-grid-item">3</div>
