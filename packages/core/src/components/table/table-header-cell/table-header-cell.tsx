5cff089cb32f12e08c0060230e0d3e9347a465bc
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-06-27 15:05:18 +0200

    fix: use icon component instead of hardcoding it

--- packages/core/src/components/table/table-header-cell/table-header-cell.tsx
+++ packages/core/src/components/table/table-header-cell/table-header-cell.tsx
@@ -208,44 +208,16 @@ export class TdsTableHeaderCell {
           </span>
 
           {this.sortingDirection === undefined && (
-            <svg
-              class="tds-table__header-button-icon"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 12 15"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8.45 13.67V4.62a.5.5 0 0 1 1 0v9.05h-1Z"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M6.07 10.28a.5.5 0 0 1 .7.08l2.1 2.66a.1.1 0 0 0 .15 0l2.09-2.66a.5.5 0 1 1 .78.62l-2.08 2.66a1.1 1.1 0 0 1-1.73 0l-2.1-2.66a.5.5 0 0 1 .1-.7ZM3.55.4v9.04a.5.5 0 1 1-1 0V.39h1Z"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M5.93 3.78a.5.5 0 0 1-.7-.08l-2.1-2.66a.1.1 0 0 0-.15 0L.89 3.7a.5.5 0 0 1-.78-.62L2.19.42a1.1 1.1 0 0 1 1.73 0l2.1 2.66a.5.5 0 0 1-.1.7Z"
-              />
-            </svg>
+            <tds-icon class="tds-table__header-button-icon" name="sorting" size="16px"></tds-icon>
           )}
           {this.sortingDirection && ['asc', 'desc'].includes(this.sortingDirection) && (
-            <svg
+            <tds-icon
               class={`tds-table__header-button-icon ${
                 this.sortingDirection === 'asc' ? 'tds-table__header-button-icon--rotate' : ''
               }`}
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M17 2.974a1 1 0 0 0-2 0v24.3l-9.312-9.312a1 1 0 0 0-1.414 1.414l9.887 9.888a2.6 2.6 0 0 0 3.677 0l9.888-9.888a1 1 0 0 0-1.414-1.414L17 27.274v-24.3Z"
-              />
-            </svg>
+              name="arrow_down"
+              size="16px"
+            ></tds-icon>
           )}
         </button>
       );
