92a99a452e18878b37c696d0c526638921afd63c
Jernej Stražišar <strazisar.jernej@gmail.com>
2024-08-01 14:11:15 +0200

    feat(tegel-angular): reactive forms integration (#598)
    * feat: export the value accessor generated by stencil
    
    * feat: add the angular value accessor config for tds-datetime
    
    * feat: fix the build errors for the angular library
    
    * feat: enable the angular forms integration for tds-dropdown
    
    * feat: update angular form control on input
    
    * fix: update the input value before the tdsInput event is emitted
    
    * feat: add angular forms integration for chips and checkboxes
    
    * feat: add angular forms integration for tds-chip in radio mode and tds-radio-button
    
    * feat: add angular forms integration for tds-toggle
    
    * feat(angular-17): copying reactive form integration
    
    ---------
    
    Co-authored-by: jst1ws <jernej.strazisar@scania.com>
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

--- packages/angular-17/projects/components/src/lib/components.module.ts
+++ packages/angular-17/projects/components/src/lib/components.module.ts
@@ -1,10 +1,29 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { DIRECTIVES } from './stencil-generated';
 import { defineCustomElements } from '@scania/tegel/loader';
+import { DIRECTIVES } from './stencil-generated';
+import { TextValueAccessor } from './stencil-generated/text-value-accessor';
+import { BooleanValueAccessor } from './stencil-generated/boolean-value-accessor';
+import { NumericValueAccessor } from './stencil-generated/number-value-accessor';
+import { TdsRadioValueAccessor } from './directives/tds-radio-value-accessor';
+import { TdsDropdownValueAccessor } from './directives/tds-dropdown-value-accessor';
 
 @NgModule({
-  declarations: [...DIRECTIVES],
-  exports: [...DIRECTIVES],
+  declarations: [
+    ...DIRECTIVES,
+    BooleanValueAccessor,
+    NumericValueAccessor,
+    TextValueAccessor,
+    TdsRadioValueAccessor,
+    TdsDropdownValueAccessor,
+  ],
+  exports: [
+    ...DIRECTIVES,
+    BooleanValueAccessor,
+    NumericValueAccessor,
+    TextValueAccessor,
+    TdsRadioValueAccessor,
+    TdsDropdownValueAccessor,
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
