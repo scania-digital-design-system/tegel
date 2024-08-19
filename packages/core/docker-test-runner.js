7d60e49130dec3ff59a5af427d4905f8cb1159e9
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-08-19 17:04:23 +0200

    fix(storybook): locking dependencies  (#723)
    * fix: mdx2 support
    
    * test: playwright version update
    
    * fix: locking all versions

--- packages/core/docker-test-runner.js
+++ packages/core/docker-test-runner.js
@@ -28,7 +28,7 @@ const args = [
   `${pwd}:/work/`,
   '-w',
   '/work/',
-  'mcr.microsoft.com/playwright:v1.45.1-jammy',
+  'mcr.microsoft.com/playwright:v1.46.1-jammy',
   '/bin/bash',
   '-c',
   'npx playwright test && exit',
