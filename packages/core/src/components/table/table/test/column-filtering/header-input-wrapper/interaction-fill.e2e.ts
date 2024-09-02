8dccc445e52fed3e0345b52bbf271629566b59d2
Johan Lundgren <johan.lundgren@umain.com>
2024-09-02 07:51:38 +0200

    test(table): testing input wrapper interactions (#729)
    * fix: removed unused colors
    
    * test: added test for table input wrapper
    
    * fix: removed comments
    
    * fix: changed to using data-testid
    
    * fix: renamed and separated files
    
    * fix: renamed tests to keep naming convention

---
+++ packages/core/src/components/table/table/test/column-filtering/header-input-wrapper/interaction-fill.e2e.ts
@@ -0,0 +1,23 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath =
+  'src/components/table/table/test/column-filtering/header-input-wrapper/index.html';
+
+test.describe.parallel('tds-table-column-filtering fill', () => {
+  test('expect slotted inputs to persist inputed value', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    const inputfield = page.getByTestId('firstHeaderInput');
+    await inputfield.fill('Hello World!');
+
+    await inputfield.blur();
+
+    let value = await inputfield.inputValue();
+
+    expect(value).toBe('Hello World!');
+
+    /* Check diff of screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0.01 });
+  });
+});
