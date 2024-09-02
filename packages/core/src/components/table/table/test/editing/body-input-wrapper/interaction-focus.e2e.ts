25c262d55c85bf17b07cd6cff205128010908b07
Johan Lundgren <johan.lundgren@umain.com>
2024-09-02 07:51:38 +0200

    test(table): testing input wrapper interactions (#729)
    * fix: removed unused colors
    
    * test: added test for table input wrapper
    
    * fix: removed comments
    
    * fix: changed to using data-testid
    
    * fix: renamed and separated files
    
    * fix: renamed tests to keep naming convention

---
+++ packages/core/src/components/table/table/test/editing/body-input-wrapper/interaction-focus.e2e.ts
@@ -0,0 +1,23 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/table/table/test/editing/body-input-wrapper/index.html';
+
+test.describe.parallel('tds-table-editable-cells focus', () => {
+  test('expect wrapper to effect slotted inputs style on focus', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    const inputfield = page.getByTestId('firstInput');
+
+    await inputfield.focus();
+
+    const color = await inputfield.evaluate((el) => {
+      return window.getComputedStyle(el).getPropertyValue('background-color');
+    });
+
+    expect(color).toBe('rgb(255, 255, 255)'); // white
+
+    /* Check diff of screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0.01 });
+  });
+});
