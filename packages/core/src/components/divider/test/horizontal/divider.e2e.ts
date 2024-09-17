c25549bd4d53de763082bfff5a9b50d9b5547788
Johan Lundgren <johan.lundgren@umain.com>
2024-09-17 11:30:19 +0200

    test(divider): added tests (#743)
    * test(divider): added tests
    
    * fix: added test for darkmode
    
    * fix: typos and titles

---
+++ packages/core/src/components/divider/test/horizontal/divider.e2e.ts
@@ -0,0 +1,18 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/divider/test/horizontal/index.html';
+
+test.describe.parallel('tds-divider', () => {
+  test('expect to render a horizontal divider', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    // expect width to be greater than height
+    const divider = page.getByTestId('divider').locator('div.divider').first();
+    const box = await divider.boundingBox();
+    expect(box.width).toBeGreaterThan(box.height);
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
