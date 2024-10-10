15edb634fee09bf8be4c3ee7e983460f4eb75d5f
Charles Krook <charles.krook@gmail.com>
2024-10-10 16:08:19 +0200

    feat: adding test to the multiselect filter dropdown

--- packages/core/src/components/dropdown/test/multiselect-filter/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/multiselect-filter/dropdown.e2e.ts
@@ -1,27 +1,31 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/dropdown/test/multiselect/index.html';
+const componentTestPath = 'src/components/dropdown/test/multiselect-filter/index.html';
 
 test.describe.parallel('tds-dropdown-multiselect-filter', () => {
   test('When focusing on the input it should clear itself', async ({ page }) => {
     await page.goto(componentTestPath);
 
     // Click the dropdown button
-    const dropdownButton = page.getByRole('button', { name: 'Placeholder' });
+    const dropdownButton = page.locator('tds-icon[aria-label="Open/Close dropdown"]');
     await dropdownButton.click();
 
+    // Get the input element
     const dropdownListElementOneButton = page
       .getByText(/Option 1/)
       .filter({ has: page.getByRole('checkbox') });
 
+    // Check if the first option is present
     await expect(dropdownListElementOneButton).toHaveCount(1);
 
+    // Click the first option
     await dropdownListElementOneButton.click();
-    await expect(dropdownListElementOneButton).toHaveAttribute('aria-selected');
 
-    const inputText = page.getByRole('button', { name: /Option 1/ });
-    await expect(inputText).toHaveCount(1);
+    // Closing dropdown options
+    await dropdownButton.click();
+    // Opening dropdown options to autofocus the input
+    await dropdownButton.click();
 
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
