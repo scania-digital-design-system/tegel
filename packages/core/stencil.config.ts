90863456fecb97ccd513d55c3fa79e17ecc96431
Tim <tim.romberg@scania.com>
2024-10-24 15:42:28 +0200

    fix: updated script to make it executable and also path

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -64,7 +64,7 @@ export const config: Config = {
       copy: [
         {
           src: '../../../assets',
-          dest: 'assets',
+          dest: 'assets/',
           warn: true,
         },
       ],
