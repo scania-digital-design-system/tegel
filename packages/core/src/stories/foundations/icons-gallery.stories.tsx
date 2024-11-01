6ab3cdc24e6739afe8595bb72ec771a377bbb190
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-11-01 11:49:39 +0100

    feat: icon gallery

---
+++ packages/core/src/stories/foundations/icons-gallery.stories.tsx
@@ -0,0 +1,33 @@
+import formatHtmlPreview from '../formatHtmlPreview';
+import { iconsNames } from '../../components/icon/iconsArray';
+
+export default {
+  title: 'Foundations/Icons',
+  parameters: {
+    layout: 'fullscreen',
+    options: {
+      showPanel: false,
+      showToolbar: false,
+    },
+  },
+};
+
+const icons = iconsNames.map(
+  (icon) => `
+        <tds-block>  
+            <tds-icon name="${icon}" size="48"></tds-icon>  
+            <p class="tds-detail-05">${icon}</p>  
+        </tds-block>
+ `,
+);
+
+const Template = () =>
+  formatHtmlPreview(
+    `
+    <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(150px, 1fr)); gap: 16px; padding: 16px;">
+        ${icons.join('')} 
+    </div>
+  `,
+  );
+
+export const Gallery = Template.bind({});
