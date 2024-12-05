bbd5b29571486e8dee7f32fa64f4b72f2b0a9268
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-05 16:10:22 +0100

    test: add end-to-end tests for banner component in dark mode

---
+++ packages/core/src/components/banner/test/darkmode/banner.e2e.ts
@@ -0,0 +1,13 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/banner/test/darkmode/index.html';
+
+test.describe.parallel('tds-banner-darkmode', () => {
+  test('renders default banner with darkmode correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
