2905822c50110d1fd8b8e53a7f59907c12c94a88
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-11 10:46:47 +0100

    refactor: enhance screenshotTest to support dark mode and customizable background color

--- packages/core/src/components/_reusable-tests/screenshotTest.ts
+++ packages/core/src/components/_reusable-tests/screenshotTest.ts
@@ -1,28 +1,23 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-export const screenshotTest = (componentTestPath: string) => {
-  test(`renders component with lightmode`, async ({ page }) => {
+export const screenshotTest = ({ componentTestPath, darkmode, backgroundColor }) => {
+  const testDescription = `renders component with ${
+    darkmode ? 'darkmode' : 'lightmode'
+  } and ${backgroundColor} background`;
+
+  test(testDescription, async ({ page }) => {
     await page.goto(componentTestPath);
 
-    await page.evaluate(() => {
-      document.body.setAttribute('style', `background-color: white; padding: 20px`);
-    });
+    if (darkmode) {
+      await page.evaluate(() => {
+        document.body.classList.add('tds-mode-dark');
+      });
+    }
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
-
-  test(`renders component with darkmode`, async ({ page }) => {
-    await page.goto(componentTestPath);
-
-    await page.evaluate(() => {
-      document.body.classList.add('tds-mode-dark');
-    });
-
-    await page.evaluate(() => {
-      document.body.setAttribute('style', `background-color: grey-958; padding: 20px`);
-    });
+    await page.evaluate((color) => {
+      document.body.setAttribute('style', `background-color: ${color}; padding: 20px`);
+    }, backgroundColor);
 
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
