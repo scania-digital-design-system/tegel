7639dd0110c5a5e0a1cda2f0d4a812e6bb72000c
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-11 17:44:07 +0100

    test(block): add tests for primary and secondary with light and dark modes

--- packages/core/src/components/block/test/3-level-dark-mode/primary/block.e2e.ts
+++ packages/core/src/components/block/test/3-level-dark-mode/primary/block.e2e.ts
@@ -1,10 +1,10 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/block/test/2-level-light-mode/index.html';
+const componentTestPath = 'src/components/block/test/3-level-dark-mode/primary/index.html';
 
-test.describe.parallel('tds-block-2-level-light-mode', () => {
-  test('renders 2 level block correctly', async ({ page }) => {
+test.describe.parallel('tds-block-3-level-dark-mode-primary', () => {
+  test('renders 3 level block correctly', async ({ page }) => {
     await page.goto(componentTestPath);
 
     /* Check diff on screenshot */
---
+++ packages/core/src/components/block/test/3-level-dark-mode/secondary/block.e2e.ts
@@ -0,0 +1,13 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/block/test/3-level-dark-mode/secondary/index.html';
+
+test.describe.parallel('tds-block-3-level-dark-mode-secondary', () => {
+  test('renders 3 level block correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
