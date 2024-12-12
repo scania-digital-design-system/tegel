db2c2af3484a45a2f1f5a183d01b35a4740d29a5
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-12 14:59:47 +0100

    test(block): refactor tests to use beforeEach for page navigation

--- packages/core/src/components/block/test/3-level-dark-mode/primary/block.e2e.ts
+++ packages/core/src/components/block/test/3-level-dark-mode/primary/block.e2e.ts
@@ -4,9 +4,11 @@ import { expect } from '@playwright/test';
 const componentTestPath = 'src/components/block/test/3-level-dark-mode/primary/index.html';
 
 test.describe.parallel('tds-block-3-level-dark-mode-primary', () => {
-  test('renders 3 level block correctly', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
 
+  test('renders 3 level block correctly', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
