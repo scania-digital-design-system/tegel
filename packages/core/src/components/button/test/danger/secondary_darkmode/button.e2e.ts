6daf0397c5f8a31ce2ed44eb1bcc5d2a56dc36d0
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-12 16:52:22 +0100

    test(button): add primary and secondary mode variant tests for danger variant

---
+++ packages/core/src/components/button/test/danger/secondary_darkmode/button.e2e.ts
@@ -0,0 +1,15 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/button/test/danger/secondary_darkmode/index.html';
+
+test.describe.parallel('tds-button-danger-secondary-darkmode', () => {
+  test('renders danger button correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const button = page.getByTestId('tds-button-testid');
+    await expect(button).toHaveCount(1);
+
+    /* Check diff on screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
