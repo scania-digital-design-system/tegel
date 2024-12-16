56ae4193bd6ef47796cc75d8abaed43c17f5cf4f
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-16 13:45:44 +0100

    test(footer): add lightmode and darkmode screenshot tests for mode variants

---
+++ packages/core/src/components/footer/test/default/primary/lightmode/footer.e2e.ts
@@ -0,0 +1,15 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/footer/test/default/primary/lightmode/index.html';
+
+test.describe.parallel('tds-footer-default-primary-lightmode', () => {
+  test('renders default footer correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const footerComponent = page.locator('footer');
+    await expect(footerComponent).toHaveCount(1);
+
+    /** Check screenshot diff */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
---
+++ packages/core/src/components/footer/test/default/secondary/darkmode/footer.e2e.ts
@@ -0,0 +1,15 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath = 'src/components/footer/test/default/secondary/darkmode/index.html';
+
+test.describe.parallel('tds-footer-default-secondary-darkmode', () => {
+  test('renders default footer correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const footerComponent = page.locator('footer');
+    await expect(footerComponent).toHaveCount(1);
+
+    /** Check screenshot diff */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+  });
+});
