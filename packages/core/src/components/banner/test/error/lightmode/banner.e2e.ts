7783cb9b872dc3944cdf5ac5fd7d9d4bd6c7f827
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-16 13:58:14 +0100

    test(banner): add darkmode screenshot test and refactor folder structure (#880)
    * test: add screenshot tests for banner in light and dark modes
    
    * refactor: reduce complexity for banner screenshot tests
    
    * test: add reusable screenshot tests for all components
    
    * test: create lightmode and darkmode tests for variants of banner
    
    * test(banner): remove inline styles from lightmode banner test html files
    
    * test(banner): refactor tests to use beforeEach for page navigation
    
    * test(banner): fix import of css variables

--- packages/core/src/components/banner/test/error/lightmode/banner.e2e.ts
+++ packages/core/src/components/banner/test/error/lightmode/banner.e2e.ts
@@ -1,19 +1,19 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/banner/test/information/index.html';
+const componentTestPath = 'src/components/banner/test/error/lightmode/index.html';
 
-test.describe.parallel('tds-banner-information', () => {
-  test('renders information banner correctly', async ({ page }) => {
+test.describe.parallel('tds-banner-error-lightmode', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
 
+  test('renders error banner correctly', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
   test('icons exists', async ({ page }) => {
-    await page.goto(componentTestPath);
-
     const images = page.getByRole('img');
     await expect(images).toHaveCount(2);
   });
--- packages/core/src/components/banner/test/information/lightmode/banner.e2e.ts
+++ packages/core/src/components/banner/test/information/lightmode/banner.e2e.ts
@@ -1,19 +1,19 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/banner/test/error/index.html';
+const componentTestPath = 'src/components/banner/test/information/lightmode/index.html';
 
-test.describe.parallel('tds-banner-error', () => {
-  test('renders error banner correctly', async ({ page }) => {
+test.describe.parallel('tds-banner-information-lightmode', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
 
+  test('renders information banner correctly', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
   test('icons exists', async ({ page }) => {
-    await page.goto(componentTestPath);
-
     const images = page.getByRole('img');
     await expect(images).toHaveCount(2);
   });
