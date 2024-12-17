ca417e087568f51873f091bf769701e97a33aed2
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-17 09:55:41 +0100

    test(spinner): refactor tests to use beforeEach for page navigation

--- packages/core/src/components/spinner/test/inverted/lightmode/spinner.e2e.ts
+++ packages/core/src/components/spinner/test/inverted/lightmode/spinner.e2e.ts
@@ -4,9 +4,11 @@ import { expect } from '@playwright/test';
 const componentTestPath = 'src/components/spinner/test/inverted/lightmode/index.html';
 
 test.describe.parallel('tds-spinner-inverted-lightmode', () => {
-  test('renders inverted spinner correctly', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
 
+  test('renders inverted spinner correctly', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
--- packages/core/src/components/spinner/test/standard/darkmode/spinner.e2e.ts
+++ packages/core/src/components/spinner/test/standard/darkmode/spinner.e2e.ts
@@ -4,9 +4,11 @@ import { expect } from '@playwright/test';
 const componentTestPath = 'src/components/spinner/test/standard/darkmode/index.html';
 
 test.describe.parallel('tds-spinner-standard-darkmode', () => {
-  test('renders basic spinner correctly', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
 
+  test('renders basic spinner correctly', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
