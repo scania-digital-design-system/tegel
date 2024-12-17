6f3565c91c5ee05fbfd6e9bea33d21d5e61857fc
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-17 15:34:01 +0100

    test(table): add lightmode and darkmode screenshot tests with expanded row in table

---
+++ packages/core/src/components/table/table/test/expandable-row-expanded/primary/darkmode/expandable-row-expanded.e2e.ts
@@ -0,0 +1,23 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath =
+  'src/components/table/table/test/expandable-row-expanded/primary/darkmode/index.html';
+
+test.describe.parallel('tds-table-expandable-row-expanded-primary-darkmode', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentTestPath);
+    const tableComponent = page.getByRole('table');
+    await expect(tableComponent).toHaveCount(1);
+    await tableComponent.waitFor({ state: 'visible' });
+  });
+
+  test('first row is expanded', async ({ page }) => {
+    const tableBodyRowFirstInput = page.getByRole('cell').nth(1);
+    const tableBodyExpandableRowSlot = page.getByText(/Hello world 1/);
+    await expect(tableBodyRowFirstInput).toHaveCount(1);
+    await expect(tableBodyExpandableRowSlot).toHaveCount(1);
+    await expect(tableBodyExpandableRowSlot).toBeVisible();
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0.05 });
+  });
+});
