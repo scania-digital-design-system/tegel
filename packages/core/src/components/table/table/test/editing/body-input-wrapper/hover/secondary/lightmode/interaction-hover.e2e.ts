1d66430a35e2420cd7b3d03f0bda764eef0fe4c2
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-17 14:57:46 +0100

    test(table): add editable table hover lightmode and darkmode screenshot tests

---
+++ packages/core/src/components/table/table/test/editing/body-input-wrapper/hover/secondary/lightmode/interaction-hover.e2e.ts
@@ -0,0 +1,18 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+
+const componentTestPath =
+  'src/components/table/table/test/editing/body-input-wrapper/hover/secondary/lightmode/index.html';
+
+test.describe.parallel('tds-table-editable-cells-hover-secondary-lightmode', () => {
+  test('expect wrapper to effect slotted inputs style on hover', async ({ page }) => {
+    await page.goto(componentTestPath);
+
+    const inputfield = page.getByTestId('firstInput');
+
+    await inputfield.hover();
+
+    /* Check diff of screenshot */
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0.01 });
+  });
+});
