bb8e5767477b4030235d3c2225f6fa9eef3bc943
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-18 17:18:18 +0100

    test(banner): use reusable code for lightmode and darkmode screenshot tests

--- packages/core/src/components/banner/test/error/banner.e2e.ts
+++ packages/core/src/components/banner/test/error/banner.e2e.ts
@@ -1,20 +1,29 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  getTestDescribeText,
+  setupPage,
+  testConfigurations,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/banner/test/error/index.html';
+const componentName = 'tds-banner';
+const testDescription = 'tds-banner-error';
 
-test.describe.parallel('tds-banner-error', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(componentTestPath);
-  });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-  test('renders error banner correctly', async ({ page }) => {
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders error banner correctly', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('icons exists', async ({ page }) => {
-    const images = page.getByRole('img');
-    await expect(images).toHaveCount(2);
+    test('icons exists', async ({ page }) => {
+      const images = page.getByRole('img');
+      await expect(images).toHaveCount(2);
+    });
   });
 });
