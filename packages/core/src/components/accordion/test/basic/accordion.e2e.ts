f700d9f652e83c23031e329072db0ea464529b59
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-18 16:18:37 +0100

    test(accordion): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/accordion/test/basic/accordion.e2e.ts
+++ packages/core/src/components/accordion/test/basic/accordion.e2e.ts
@@ -1,77 +1,84 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  getTestDescribeText,
+  setupPage,
+  testConfigurations,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/accordion/test/basic/index.html';
 const accordionSelector = 'tds-accordion';
+const componentName = 'tds-accordion';
 
-test.describe.parallel('tds-accordion', () => {
-  test('renders basic accordion correctly', async ({ page }) => {
-    // Define selector for accordion
-    await page.goto(componentTestPath);
-    const accordion = page.locator(accordionSelector);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    // Check if accordion contains the correct text
-    await expect(accordion).toContainText('First item');
-    await expect(accordion).toContainText('Second item');
+    test('renders basic accordion correctly', async ({ page }) => {
+      // Define selector for accordion
+      const accordion = page.locator(accordionSelector);
 
-    // Check screenshot diff to make sure the accordion is rendered correctly
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      // Check if accordion contains the correct text
+      await expect(accordion).toContainText('First item');
+      await expect(accordion).toContainText('Second item');
 
-  test('should handle hover on accordion items', async ({ page }) => {
-    // Define selector for first accordion item
-    await page.goto(componentTestPath);
-    const accordionFirstItem = page.getByText('First item');
+      // Check screenshot diff to make sure the accordion is rendered correctly
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    // Hover first accordion item
-    await accordionFirstItem.hover();
+    test('should handle hover on accordion items', async ({ page }) => {
+      // Define selector for first accordion item
+      const accordionFirstItem = page.getByText('First item');
 
-    // Expect cursor to be pointer
-    await expect(accordionFirstItem).toHaveCSS('cursor', 'pointer');
+      // Hover first accordion item
+      await accordionFirstItem.hover();
 
-    // Define selector for second accordion item
-    await page.goto(componentTestPath);
-    const accordionSecondItem = page.getByText('Second item');
+      // Expect cursor to be pointer
+      await expect(accordionFirstItem).toHaveCSS('cursor', 'pointer');
 
-    // Hover second accordion item
-    await accordionSecondItem.hover();
+      // Define selector for second accordion item
+      await page.goto(componentTestPath);
+      const accordionSecondItem = page.getByText('Second item');
 
-    // Expect cursor to be pointer
-    await expect(accordionSecondItem).toHaveCSS('cursor', 'pointer');
-  });
+      // Hover second accordion item
+      await accordionSecondItem.hover();
 
-  test('second accordion item opens on click', async ({ page }) => {
-    // Define selector for second accordion item
-    await page.goto(componentTestPath);
-    const accordionSecondItem = page.getByText('Second item');
+      // Expect cursor to be pointer
+      await expect(accordionSecondItem).toHaveCSS('cursor', 'pointer');
+    });
 
-    // Hover second accordion item
-    await accordionSecondItem.click();
+    test('second accordion item opens on click', async ({ page }) => {
+      // Define selector for second accordion item
+      const accordionSecondItem = page.getByText('Second item');
 
-    // Check screenshot diff to make sure the second accordion item is open
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      // Hover second accordion item
+      await accordionSecondItem.click();
 
-  test('fires tdsToggle event on click', async ({ page }) => {
-    // Define selector for first accordion item
-    await page.goto(componentTestPath);
-    const accordionFirstItem = page.getByText('First item');
+      // Check screenshot diff to make sure the second accordion item is open
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    // Define selector for second accordion item
-    await page.goto(componentTestPath);
-    const accordionSecondItem = page.getByText('Second item');
+    test('fires tdsToggle event on click', async ({ page }) => {
+      // Define selector for first accordion item
+      const accordionFirstItem = page.getByText('First item');
 
-    // Click first accordion item
-    const myEventSpy = await page.spyOnEvent('tdsToggle');
-    await accordionFirstItem.click();
+      // Define selector for second accordion item
+      const accordionSecondItem = page.getByText('Second item');
 
-    // Expect event to be fired
-    expect(myEventSpy).toHaveReceivedEvent();
+      // Click first accordion item
+      const myEventSpy = await page.spyOnEvent('tdsToggle');
+      await accordionFirstItem.click();
 
-    // Click second accordion item
-    await accordionSecondItem.click();
+      // Expect event to be fired
+      expect(myEventSpy).toHaveReceivedEvent();
 
-    // Expect event to be fired
-    expect(myEventSpy).toHaveReceivedEvent();
+      // Click second accordion item
+      await accordionSecondItem.click();
+
+      // Expect event to be fired
+      expect(myEventSpy).toHaveReceivedEvent();
+    });
   });
 });
--- packages/core/src/components/accordion/test/disabled/accordion.e2e.ts
+++ packages/core/src/components/accordion/test/disabled/accordion.e2e.ts
@@ -1,81 +1,87 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  getTestDescribeText,
+  setupPage,
+  testConfigurations,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/accordion/test/disabled/index.html';
 const accordionSelector = 'tds-accordion';
+const componentName = 'tds-accordion';
 
-test.describe.parallel('tds-accordion', () => {
-  test('renders disabled accordion correctly', async ({ page }) => {
-    // Define selector for accordion
-    await page.goto(componentTestPath);
-    const accordion = page.locator(accordionSelector);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    // Check if accordion contains the correct text
-    await expect(accordion).toContainText('First item');
-    await expect(accordion).toContainText('Second item');
+    test('renders disabled accordion correctly', async ({ page }) => {
+      // Define selector for accordion
+      const accordion = page.locator(accordionSelector);
 
-    // Check screenshot diff to make sure the accordion is rendered correctly
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      // Check if accordion contains the correct text
+      await expect(accordion).toContainText('First item');
+      await expect(accordion).toContainText('Second item');
 
-  test('disabled accordion items should be displayed', async ({ page }) => {
-    // Define selector for first accordion item
-    await page.goto(componentTestPath);
-    const accordionFirstItem = page.locator(accordionSelector + '>> text=First item');
+      // Check screenshot diff to make sure the accordion is rendered correctly
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    // Expect first accordion item to be disabled
-    await expect(accordionFirstItem).toBeDisabled();
+    test('disabled accordion items should be displayed', async ({ page }) => {
+      // Define selector for first accordion item
+      const accordionFirstItem = page.locator(accordionSelector + '>> text=First item');
 
-    // Define selector for second accordion item
-    await page.goto(componentTestPath);
-    const accordionSecondItem = page.getByTestId('second-item');
+      // Expect first accordion item to be disabled
+      await expect(accordionFirstItem).toBeDisabled();
 
-    // Define selector for second accordion item button since
-    // the disabled attribute does not propagate the same when using 'header' slot
-    const accordionSecondItemButton = accordionSecondItem.getByRole('button');
+      // Define selector for second accordion item
+      const accordionSecondItem = page.getByTestId('second-item');
 
-    // Expect second accordion item to be disabled
-    await expect(accordionSecondItemButton).toBeDisabled();
-  });
+      // Define selector for second accordion item button since
+      // the disabled attribute does not propagate the same when using 'header' slot
+      const accordionSecondItemButton = accordionSecondItem.getByRole('button');
 
-  test('cursor should be not-allowed on disabled accordion items', async ({ page }) => {
-    // Define selector for first accordion item
-    await page.goto(componentTestPath);
-    const accordionFirstItem = page.getByTestId('first-item');
-    const accordionFirstItemButton = accordionFirstItem.getByRole('button');
+      // Expect second accordion item to be disabled
+      await expect(accordionSecondItemButton).toBeDisabled();
+    });
 
-    // Hover first accordion item
-    const accordionCursorFirstItem = await accordionFirstItemButton.evaluate(
-      (style) => getComputedStyle(style).cursor,
-    );
+    test('cursor should be not-allowed on disabled accordion items', async ({ page }) => {
+      // Define selector for first accordion item
+      const accordionFirstItem = page.getByTestId('first-item');
+      const accordionFirstItemButton = accordionFirstItem.getByRole('button');
 
-    // Check if selector has "not-allowed" cursor
-    await expect(accordionCursorFirstItem).toBe('not-allowed');
+      // Hover first accordion item
+      const accordionCursorFirstItem = await accordionFirstItemButton.evaluate(
+        (style) => getComputedStyle(style).cursor,
+      );
 
-    // Define selector for second accordion item
-    await page.goto(componentTestPath);
-    const accordionSecondItem = page.getByTestId('second-item');
-    const accordionSecondItemButton = accordionSecondItem.getByRole('button');
+      // Check if selector has "not-allowed" cursor
+      await expect(accordionCursorFirstItem).toBe('not-allowed');
 
-    // Hover second accordion item
-    const accordionCursorSecondItem = await accordionSecondItemButton.evaluate(
-      (style) => getComputedStyle(style).cursor,
-    );
+      // Define selector for second accordion item
+      const accordionSecondItem = page.getByTestId('second-item');
+      const accordionSecondItemButton = accordionSecondItem.getByRole('button');
 
-    // Check if selector has "not-allowed" cursor
-    await expect(accordionCursorSecondItem).toBe('not-allowed');
-  });
+      // Hover second accordion item
+      const accordionCursorSecondItem = await accordionSecondItemButton.evaluate(
+        (style) => getComputedStyle(style).cursor,
+      );
 
-  test('does not fire tdsToggle event on click on disabled accordion', async ({ page }) => {
-    // Define selector for first accordion item
-    await page.goto(componentTestPath);
-    const accordionFirstItem = page.getByText('First item');
+      // Check if selector has "not-allowed" cursor
+      await expect(accordionCursorSecondItem).toBe('not-allowed');
+    });
 
-    // Click first accordion item
-    const myEventSpy = await page.spyOnEvent('tdsToggle');
-    await accordionFirstItem.click({ force: true });
+    test('does not fire tdsToggle event on click on disabled accordion', async ({ page }) => {
+      // Define selector for first accordion item
+      const accordionFirstItem = page.getByText('First item');
 
-    // Expect event not to be fired
-    expect(myEventSpy).not.toHaveReceivedEvent();
+      // Click first accordion item
+      const myEventSpy = await page.spyOnEvent('tdsToggle');
+      await accordionFirstItem.click({ force: true });
+
+      // Expect event not to be fired
+      expect(myEventSpy).not.toHaveReceivedEvent();
+    });
   });
 });
