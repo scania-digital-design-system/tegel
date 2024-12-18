c20306da8fc058fe8758e45dd9c349a562c049d0
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-18 17:11:30 +0100

    test(banner): restore to original structure of screenshot tests

--- packages/core/src/components/banner/test/default/banner.e2e.ts
+++ packages/core/src/components/banner/test/default/banner.e2e.ts
@@ -1,9 +1,9 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/banner/test/default/lightmode/index.html';
+const componentTestPath = 'src/components/banner/test/default/index.html';
 
-test.describe.parallel('tds-banner-default-lightmode', () => {
+test.describe.parallel('tds-banner-default', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
   });
