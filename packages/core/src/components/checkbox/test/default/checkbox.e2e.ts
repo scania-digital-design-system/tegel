b1c94e8a35d9a20c0282c398b99c9022cc76f80b
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 10:20:18 +0100

    test(checkbox): restore to original structure for default tests

--- packages/core/src/components/checkbox/test/default/checkbox.e2e.ts
+++ packages/core/src/components/checkbox/test/default/checkbox.e2e.ts
@@ -1,9 +1,9 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/checkbox/test/default/lightmode/index.html';
+const componentTestPath = 'src/components/checkbox/test/default/index.html';
 
-test.describe.parallel('tds-checkbox-lightmode', () => {
+test.describe.parallel('tds-checkbox', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
   });
