79fd9e756e5b452015506eb86599039016205c00
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 09:16:50 +0100

    test(button): use reusable test code for lightmode and darkmode screenshot tests for all variants

--- packages/core/src/components/button/test/disabled/button.e2e.ts
+++ packages/core/src/components/button/test/disabled/button.e2e.ts
@@ -1,30 +1,39 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/button/test/disabled/index.html';
+const componentName = 'tds-button';
+const testDescription = 'tds-button-disabled';
 
-test.describe.parallel('tds-button-disabled', () => {
-  test('renders disabled button correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const button = page.getByTestId('tds-button-testid');
-    await expect(button).toHaveCount(1);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders disabled button correctly', async ({ page }) => {
+      const button = page.getByTestId('tds-button-testid');
+      await expect(button).toHaveCount(1);
 
-  test('disabled button should be disabled', async ({ page }) => {
-    await page.goto(componentTestPath);
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    /* Check if disabled */
-    const button = page.getByRole('button');
-    await expect(button).toBeDisabled();
-  });
+    test('disabled button should be disabled', async ({ page }) => {
+      /* Check if disabled */
+      const button = page.getByRole('button');
+      await expect(button).toBeDisabled();
+    });
 
-  test('the cursor should be not-allowed', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const button = page.getByTestId('tds-button-testid').getByRole('button');
-    const buttonCursorState = await button.evaluate((style) => getComputedStyle(style).cursor);
-    expect(buttonCursorState).toBe('not-allowed');
+    test('the cursor should be not-allowed', async ({ page }) => {
+      const button = page.getByTestId('tds-button-testid').getByRole('button');
+      const buttonCursorState = await button.evaluate((style) => getComputedStyle(style).cursor);
+      expect(buttonCursorState).toBe('not-allowed');
+    });
   });
 });
