47d52d9131da3a63468885f52be3b0aac943abe1
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 17:15:30 +0100

    test(popover-canvas): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/popover-canvas/test/show-true/popover-canvas.e2e.ts
+++ packages/core/src/components/popover-canvas/test/show-true/popover-canvas.e2e.ts
@@ -1,44 +1,55 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/popover-canvas/test/show-true/index.html';
+const componentName = 'tds-popover-canvas';
+const testDescription = 'tds-popover-canvas-show-true';
 
-test.describe.parallel('tds-popover-canvas-show-true', () => {
-  test('renders show=true popover-canvas correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button');
-    await triggerButton.click();
-
-    // get popover element
-    const popover = page.locator('tds-popover-core');
-
-    // wait for it to be visible
-    await popover.waitFor({ state: 'visible' });
-
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
-  });
-
-  test('make sure the popover canvas content is displayed both before and after the trigger button is pressed', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button');
-    const popoverCanvasHeader = page.getByRole('heading');
-    const popoverCanvasBody = page.getByText('Where you can put anything you want!', {
-      exact: true,
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
     });
-    const popoverCanvasLink = page.getByRole('link');
-    await expect(triggerButton).toBeVisible();
-    await expect(popoverCanvasHeader).toBeVisible();
-    await expect(popoverCanvasBody).toBeVisible();
-    await expect(popoverCanvasLink).toBeVisible();
 
-    await triggerButton.click();
+    test('renders show=true popover-canvas correctly', async ({ page }) => {
+      const triggerButton = page.getByRole('button');
+      await triggerButton.click();
 
-    await expect(triggerButton).toBeVisible();
-    await expect(popoverCanvasHeader).toBeVisible();
-    await expect(popoverCanvasBody).toBeVisible();
-    await expect(popoverCanvasLink).toBeVisible();
+      // get popover element
+      const popover = page.locator('tds-popover-core');
+
+      // wait for it to be visible
+      await popover.waitFor({ state: 'visible' });
+
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 });
+    });
+
+    test('make sure the popover canvas content is displayed both before and after the trigger button is pressed', async ({
+      page,
+    }) => {
+      const triggerButton = page.getByRole('button');
+      const popoverCanvasHeader = page.getByRole('heading');
+      const popoverCanvasBody = page.getByText('Where you can put anything you want!', {
+        exact: true,
+      });
+      const popoverCanvasLink = page.getByRole('link');
+      await expect(triggerButton).toBeVisible();
+      await expect(popoverCanvasHeader).toBeVisible();
+      await expect(popoverCanvasBody).toBeVisible();
+      await expect(popoverCanvasLink).toBeVisible();
+
+      await triggerButton.click();
+
+      await expect(triggerButton).toBeVisible();
+      await expect(popoverCanvasHeader).toBeVisible();
+      await expect(popoverCanvasBody).toBeVisible();
+      await expect(popoverCanvasLink).toBeVisible();
+    });
   });
 });
