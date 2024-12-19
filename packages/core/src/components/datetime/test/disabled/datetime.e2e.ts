8dfce09382d5f75e77bd1b62a31734f840b86df7
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 14:27:52 +0100

    test(datetime): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/datetime/test/disabled/datetime.e2e.ts
+++ packages/core/src/components/datetime/test/disabled/datetime.e2e.ts
@@ -1,34 +1,43 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/datetime/test/disabled/index.html';
+const componentName = 'tds-datetime';
+const testDescription = 'tds-datetime-disabled';
 
-test.describe('tds-datetime-disabled', () => {
-  test('renders disabled datetime component correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders disabled datetime component correctly', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('when in disabled state all but helper text should have pointer events none', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
+    test('when in disabled state all but helper text should have pointer events none', async ({
+      page,
+    }) => {
+      // Check for disabled state of the datetime input
+      const datetimeInput = page.locator('input[type="datetime-local"]');
+      const label = page.locator('label');
+      const helperText = page.locator('.tds-datetime-helper .tds-helper');
 
-    // Check for disabled state of the datetime input
-    const datetimeInput = page.locator('input[type="datetime-local"]');
-    const label = page.locator('label');
-    const helperText = page.locator('.tds-datetime-helper .tds-helper');
+      await expect(datetimeInput).toBeDisabled();
 
-    await expect(datetimeInput).toBeDisabled();
+      await datetimeInput.hover();
+      await expect(datetimeInput).toHaveCSS('cursor', 'not-allowed');
 
-    await datetimeInput.hover();
-    await expect(datetimeInput).toHaveCSS('cursor', 'not-allowed');
+      await label.hover();
+      await expect(label).toHaveCSS('cursor', 'not-allowed');
 
-    await label.hover();
-    await expect(label).toHaveCSS('cursor', 'not-allowed');
-
-    await expect(helperText).not.toHaveCSS('pointer-events', 'none');
+      await expect(helperText).not.toHaveCSS('pointer-events', 'none');
+    });
   });
 });
