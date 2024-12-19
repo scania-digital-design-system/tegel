e1b22a9e983132b8861e20239d556c8f8574c98e
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 16:18:10 +0100

    test(chip): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/chip/test/default/chip.e2e.ts
+++ packages/core/src/components/chip/test/default/chip.e2e.ts
@@ -1,43 +1,52 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/chip/test/default/index.html';
+const componentName = 'tds-chip';
+const testDescription = 'tds-chip-default';
 
-test.describe.parallel('tds-chip-default', () => {
-  test('renders default chip correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const chip = page.locator('tds-chip');
-    await expect(chip).toHaveCount(1);
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    const labelElement = page.locator('tds-chip label'); // Target label underneath chip
-    expect(labelElement).toHaveText('Label');
+    test('renders default chip correctly', async ({ page }) => {
+      const chip = page.locator('tds-chip');
+      await expect(chip).toHaveCount(1);
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      const labelElement = page.locator('tds-chip label'); // Target label underneath chip
+      expect(labelElement).toHaveText('Label');
 
-  test('expect chip to be of the role button', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const chipRole = page.getByRole('button');
-    await expect(chipRole).toHaveCount(1);
-  });
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('Check so that height is correct to lg/default measurements', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const chip = page.locator('tds-chip label');
-    const chipHeight = await chip.evaluate((style) => getComputedStyle(style).height);
-    expect(chipHeight).toBe('32px');
-  });
+    test('expect chip to be of the role button', async ({ page }) => {
+      const chipRole = page.getByRole('button');
+      await expect(chipRole).toHaveCount(1);
+    });
 
-  test('Chip is clickable', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const chip = page.locator('tds-chip label');
-    chip.hover();
-    const chipCursorStyle = await chip.evaluate((style) => getComputedStyle(style).cursor);
-    expect(chipCursorStyle).toBe('pointer');
+    test('Check so that height is correct to lg/default measurements', async ({ page }) => {
+      const chip = page.locator('tds-chip label');
+      const chipHeight = await chip.evaluate((style) => getComputedStyle(style).height);
+      expect(chipHeight).toBe('32px');
+    });
 
-    const myEventSpy = await page.spyOnEvent('click');
-    await chip.click();
-    expect(myEventSpy).toHaveReceivedEvent();
+    test('Chip is clickable', async ({ page }) => {
+      const chip = page.locator('tds-chip label');
+      chip.hover();
+      const chipCursorStyle = await chip.evaluate((style) => getComputedStyle(style).cursor);
+      expect(chipCursorStyle).toBe('pointer');
+
+      const myEventSpy = await page.spyOnEvent('click');
+      await chip.click();
+      expect(myEventSpy).toHaveReceivedEvent();
+    });
   });
 });
