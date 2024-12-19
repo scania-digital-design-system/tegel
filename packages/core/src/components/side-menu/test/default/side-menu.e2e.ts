e3ed5c5278fbc51f490aef77a7c51cf17038c147
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 18:07:29 +0100

    test(side-menu): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/side-menu/test/default/side-menu.e2e.ts
+++ packages/core/src/components/side-menu/test/default/side-menu.e2e.ts
@@ -1,49 +1,58 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/side-menu/test/default/index.html';
+const componentName = 'tds-side-menu';
+const testDescription = 'tds-side-menu-default';
 
-test.describe.parallel('tds-side-menu-default', () => {
-  test('renders default side-menu correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const sideMenuNavigation = page.getByRole('navigation');
-    await expect(sideMenuNavigation).toHaveCount(1);
-    await expect(sideMenuNavigation).toBeVisible();
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders default side-menu correctly', async ({ page }) => {
+      const sideMenuNavigation = page.getByRole('navigation');
+      await expect(sideMenuNavigation).toHaveCount(1);
+      await expect(sideMenuNavigation).toBeVisible();
 
-  test('all side menu buttons to be visible', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const sideMenuButtons = page.getByRole('button');
-    await expect(sideMenuButtons).toHaveCount(4);
-    const promises = [];
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    for (let i = 0; i < 4; i++) {
-      promises.push(expect(sideMenuButtons.nth(i)).toBeVisible());
-    }
+    test('all side menu buttons to be visible', async ({ page }) => {
+      const sideMenuButtons = page.getByRole('button');
+      await expect(sideMenuButtons).toHaveCount(4);
+      const promises = [];
 
-    await Promise.all(promises);
-  });
+      for (let i = 0; i < 4; i++) {
+        promises.push(expect(sideMenuButtons.nth(i)).toBeVisible());
+      }
 
-  test('wheel type list is open by default and under Wheel types there are two sublink', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
+      await Promise.all(promises);
+    });
 
-    /* Make sure first list item is there and visible */
-    const sideMenuWheelTypeListItemOne = page
-      .getByRole('listitem')
-      .getByRole('link', { name: /Hub-centric wheel/ });
-    await expect(sideMenuWheelTypeListItemOne).toHaveCount(1);
-    await expect(sideMenuWheelTypeListItemOne).toBeVisible();
+    test('wheel type list is open by default and under Wheel types there are two sublink', async ({
+      page,
+    }) => {
+      /* Make sure first list item is there and visible */
+      const sideMenuWheelTypeListItemOne = page
+        .getByRole('listitem')
+        .getByRole('link', { name: /Hub-centric wheel/ });
+      await expect(sideMenuWheelTypeListItemOne).toHaveCount(1);
+      await expect(sideMenuWheelTypeListItemOne).toBeVisible();
 
-    /* Make sure the second list item is there and visible */
-    const sideMenuWheelTypeListItemTwo = page
-      .getByRole('listitem')
-      .getByRole('link', { name: /Rim wheel/ });
-    await expect(sideMenuWheelTypeListItemTwo).toHaveCount(1);
-    await expect(sideMenuWheelTypeListItemTwo).toBeVisible();
+      /* Make sure the second list item is there and visible */
+      const sideMenuWheelTypeListItemTwo = page
+        .getByRole('listitem')
+        .getByRole('link', { name: /Rim wheel/ });
+      await expect(sideMenuWheelTypeListItemTwo).toHaveCount(1);
+      await expect(sideMenuWheelTypeListItemTwo).toBeVisible();
+    });
   });
 });
