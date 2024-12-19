108bb7e56cd622d1ef818cc5e312a2c792f11e6c
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 16:31:57 +0100

    test(message): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/message/test/basic/message.e2e.ts
+++ packages/core/src/components/message/test/basic/message.e2e.ts
@@ -1,20 +1,30 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/message/test/basic/index.html';
+const componentName = 'tds-message';
+const testDescription = 'tds-message-basic';
 
-test.describe.parallel('tds-message-basic', () => {
-  test('is basic message rendered correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('is basic message rendered correctly', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('has icon', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const messageIcon = page.getByRole('img');
-    await expect(messageIcon).toHaveCount(1);
-    await expect(messageIcon).toBeVisible();
+    test('has icon', async ({ page }) => {
+      const messageIcon = page.getByRole('img');
+      await expect(messageIcon).toHaveCount(1);
+      await expect(messageIcon).toBeVisible();
+    });
   });
 });
