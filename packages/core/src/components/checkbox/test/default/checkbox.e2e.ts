8551fd0544748ab0a1140f1af320eecafc770466
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 13:18:53 +0100

    test(checkbox): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/checkbox/test/default/checkbox.e2e.ts
+++ packages/core/src/components/checkbox/test/default/checkbox.e2e.ts
@@ -1,31 +1,39 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/checkbox/test/default/index.html';
+const componentName = 'tds-checkbox';
 
-test.describe.parallel('tds-checkbox', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(componentTestPath);
-  });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-  test('renders basic checkbox correctly', async ({ page }) => {
-    const labelElement = page.locator('tds-checkbox label'); // Target label underneath checkbox
+    test('renders basic checkbox correctly', async ({ page }) => {
+      const labelElement = page.locator('tds-checkbox label'); // Target label underneath checkbox
 
-    expect(labelElement).toHaveText('Label'); // Check label text
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      expect(labelElement).toHaveText('Label'); // Check label text
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('Hover and click on checkbox -> should become checked', async ({ page }) => {
-    const checkbox = page.locator('tds-checkbox');
-    // Hover over the checkbox
-    await checkbox.hover();
+    test('Hover and click on checkbox -> should become checked', async ({ page }) => {
+      const checkbox = page.locator('tds-checkbox');
+      // Hover over the checkbox
+      await checkbox.hover();
 
-    // Click the checkbox
-    await checkbox.click();
+      // Click the checkbox
+      await checkbox.click();
 
-    // Check if the checkbox is now checked
-    const isChecked = await checkbox.evaluate((element: HTMLInputElement) => element.checked);
-    expect(isChecked).toBe(true);
+      // Check if the checkbox is now checked
+      const isChecked = await checkbox.evaluate((element: HTMLInputElement) => element.checked);
+      expect(isChecked).toBe(true);
+    });
   });
 });
