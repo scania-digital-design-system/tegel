4b1f15c9c788c623768e5a494bc008ab2adc54e0
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 10:13:41 +0100

    test(card): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/card/test/basic/card.e2e.ts
+++ packages/core/src/components/card/test/basic/card.e2e.ts
@@ -1,13 +1,24 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/card/test/basic/index.html';
+const componentName = 'tds-card';
+const testDescription = 'tds-card-basic';
 
-test.describe('tds-card-basic', () => {
-  test('renders basic card correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    test('renders basic card correctly', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
   });
 });
