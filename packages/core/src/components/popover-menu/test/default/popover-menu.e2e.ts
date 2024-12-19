e3f894a4aa62bc6c25d44bc29767f8947dd7d838
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-19 17:41:21 +0100

    test(popover-menu): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/popover-menu/test/default/popover-menu.e2e.ts
+++ packages/core/src/components/popover-menu/test/default/popover-menu.e2e.ts
@@ -1,93 +1,100 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/popover-menu/test/default/index.html';
+const componentName = 'tds-popover-menu';
+const testDescription = 'tds-popover-menu-default';
 
-test.describe.parallel('tds-popover-menu-default', () => {
-  test('renders default popover-menu correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    await triggerButton.click();
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders default popover-menu correctly', async ({ page }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      await triggerButton.click();
 
-  test('clicking the trigger button should open the popover menu dialog', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    const dropDownList = page.getByRole('list');
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    await expect(triggerButton).toBeVisible();
-    await expect(dropDownList).toBeHidden();
+    test('clicking the trigger button should open the popover menu dialog', async ({ page }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      const dropDownList = page.getByRole('list');
 
-    await triggerButton.click();
+      await expect(triggerButton).toBeVisible();
+      await expect(dropDownList).toBeHidden();
 
-    await expect(triggerButton).toBeVisible();
-    await expect(dropDownList).toBeVisible();
+      await triggerButton.click();
 
-    const tdsMenuItemListItems = dropDownList.getByRole('listitem');
-    await expect(tdsMenuItemListItems).toHaveCount(9);
+      await expect(triggerButton).toBeVisible();
+      await expect(dropDownList).toBeVisible();
 
-    const tdsMenuItemListItemLinks = tdsMenuItemListItems.getByRole('link');
-    const tdsMenuItemListItemButtons = tdsMenuItemListItems.getByRole('button');
+      const tdsMenuItemListItems = dropDownList.getByRole('listitem');
+      await expect(tdsMenuItemListItems).toHaveCount(9);
 
-    await expect(tdsMenuItemListItemLinks).toHaveCount(7);
-    await expect(tdsMenuItemListItemButtons).toHaveCount(2);
-  });
+      const tdsMenuItemListItemLinks = tdsMenuItemListItems.getByRole('link');
+      const tdsMenuItemListItemButtons = tdsMenuItemListItems.getByRole('button');
 
-  test('hover active menu item -> active item should be clickable', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    const dropDownList = page.getByRole('list');
-    await triggerButton.click();
+      await expect(tdsMenuItemListItemLinks).toHaveCount(7);
+      await expect(tdsMenuItemListItemButtons).toHaveCount(2);
+    });
 
-    const tdsMenuItemListItemLinks = page
-      .getByRole('listitem')
-      .filter({ has: page.getByRole('link') });
+    test('hover active menu item -> active item should be clickable', async ({ page }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      const dropDownList = page.getByRole('list');
+      await triggerButton.click();
 
-    const myEventSpy = await page.spyOnEvent('click');
-    await tdsMenuItemListItemLinks.first().click();
-    expect(myEventSpy).toHaveReceivedEvent();
+      const tdsMenuItemListItemLinks = page
+        .getByRole('listitem')
+        .filter({ has: page.getByRole('link') });
 
-    /* Expect dropdown not to close after clicking the menu item */
-    await expect(dropDownList).toBeVisible();
-  });
+      const myEventSpy = await page.spyOnEvent('click');
+      await tdsMenuItemListItemLinks.first().click();
+      expect(myEventSpy).toHaveReceivedEvent();
 
-  test('hover inactive menu item -> inactive menu item should not be clickable', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    await triggerButton.click();
+      /* Expect dropdown not to close after clicking the menu item */
+      await expect(dropDownList).toBeVisible();
+    });
 
-    const tdsMenuItemListItemButtons = page
-      .getByRole('listitem')
-      .filter({ has: page.getByRole('button') });
+    test('hover inactive menu item -> inactive menu item should not be clickable', async ({
+      page,
+    }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      await triggerButton.click();
 
-    await expect(tdsMenuItemListItemButtons).toHaveCount(2);
+      const tdsMenuItemListItemButtons = page
+        .getByRole('listitem')
+        .filter({ has: page.getByRole('button') });
 
-    await expect(tdsMenuItemListItemButtons.first()).toHaveAttribute('disabled');
-    await expect(tdsMenuItemListItemButtons.last()).not.toHaveAttribute('disabled');
-  });
+      await expect(tdsMenuItemListItemButtons).toHaveCount(2);
 
-  test('icons are not existing for menu items', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const tdsMenuItemListItemIcons = page
-      .getByRole('listitem')
-      .filter({ has: page.getByRole('img') });
-    await expect(tdsMenuItemListItemIcons).toHaveCount(0);
-  });
+      await expect(tdsMenuItemListItemButtons.first()).toHaveAttribute('disabled');
+      await expect(tdsMenuItemListItemButtons.last()).not.toHaveAttribute('disabled');
+    });
 
-  test('activating close method should close the dialog', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    await triggerButton.click();
+    test('icons are not existing for menu items', async ({ page }) => {
+      const tdsMenuItemListItemIcons = page
+        .getByRole('listitem')
+        .filter({ has: page.getByRole('img') });
+      await expect(tdsMenuItemListItemIcons).toHaveCount(0);
+    });
 
-    const closeButton = page.getByTestId('menu-close-button');
-    await expect(closeButton).toBeVisible();
-    await closeButton.click();
+    test('activating close method should close the dialog', async ({ page }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      await triggerButton.click();
 
-    await expect(closeButton).not.toBeVisible();
+      const closeButton = page.getByTestId('menu-close-button');
+      await expect(closeButton).toBeVisible();
+      await closeButton.click();
+
+      await expect(closeButton).not.toBeVisible();
+    });
   });
 });
--- packages/core/src/components/popover-menu/test/show-false/popover-menu.e2e.ts
+++ packages/core/src/components/popover-menu/test/show-false/popover-menu.e2e.ts
@@ -1,29 +1,38 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/popover-menu/test/show-false/index.html';
+const componentName = 'tds-popover-menu';
+const testDescription = 'tds-popover-menu-show-false';
 
-test.describe.parallel('tds-popover-menu-show-false', () => {
-  test('renders show=false popover-menu correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
+    test('renders show=false popover-menu correctly', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('clicking the trigger button should not open the popover menu dialog when show is false', async ({
+      page,
+    }) => {
+      const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
+      const dropDownList = page.getByRole('list');
 
-  test('clicking the trigger button should not open the popover menu dialog when show is false', async ({
-    page,
-  }) => {
-    await page.goto(componentTestPath);
-    const triggerButton = page.getByRole('button').filter({ has: page.getByRole('img') });
-    const dropDownList = page.getByRole('list');
+      await expect(triggerButton).toBeVisible();
+      await expect(dropDownList).toBeHidden();
 
-    await expect(triggerButton).toBeVisible();
-    await expect(dropDownList).toBeHidden();
+      await triggerButton.click();
 
-    await triggerButton.click();
-
-    await expect(triggerButton).toBeVisible();
-    await expect(dropDownList).toBeHidden();
+      await expect(triggerButton).toBeVisible();
+      await expect(dropDownList).toBeHidden();
+    });
   });
 });
