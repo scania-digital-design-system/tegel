0fff29be233ac097a3e4fd7cdd29771e92324faa
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2024-12-20 15:22:35 +0100

    test(toast): use reusable test code for lightmode and darkmode screenshot tests

--- packages/core/src/components/toast/test/warning-not-closable/toast.e2e.ts
+++ packages/core/src/components/toast/test/warning-not-closable/toast.e2e.ts
@@ -1,27 +1,37 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/toast/test/warning-not-closable/index.html';
+const componentName = 'tds-toast';
+const testDescription = 'Toast Component Tests';
 
-test.describe('Toast Component Tests', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(componentTestPath);
-    const tdsToast = page.getByTestId('tds-toast-testid');
-    expect(tdsToast).not.toBeNull();
-  });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
 
-  // Test if component renders and take screenshot
-  test('should render toast component and take screenshot', async ({ page }) => {
-    const tdsToast = page.getByTestId('tds-toast-testid');
-    await expect(tdsToast).toHaveCount(1);
-    /* Expect no difference in screenshot  */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+      const tdsToast = page.getByTestId('tds-toast-testid');
+      expect(tdsToast).not.toBeNull();
+    });
 
-  // Test if component does not have close button
-  test('should not have close button', async ({ page }) => {
-    const tdsToast = page.getByTestId('tds-toast-testid');
-    const closeButton = tdsToast.locator('button.close');
-    await expect(closeButton).toHaveCount(0);
+    // Test if component renders and take screenshot
+    test('should render toast component and take screenshot', async ({ page }) => {
+      const tdsToast = page.getByTestId('tds-toast-testid');
+      await expect(tdsToast).toHaveCount(1);
+      /* Expect no difference in screenshot  */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
+
+    // Test if component does not have close button
+    test('should not have close button', async ({ page }) => {
+      const tdsToast = page.getByTestId('tds-toast-testid');
+      const closeButton = tdsToast.locator('button.close');
+      await expect(closeButton).toHaveCount(0);
+    });
   });
 });
