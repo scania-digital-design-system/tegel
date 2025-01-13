c99c10aec59c00c867ff0d1e956450d7a6689ced
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-13 14:54:12 +0100

    test(breadcrumbs): move non-screenshot tests to not run for all style configurations

--- packages/core/src/components/breadcrumbs/test/default/breadcrumbs.e2e.ts
+++ packages/core/src/components/breadcrumbs/test/default/breadcrumbs.e2e.ts
@@ -21,25 +21,6 @@ testConfigurations.basic.forEach((config) => {
       await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
     });
 
-    test('items Page 1, Page 2, Page 3 exist on the page', async ({ page }) => {
-      const navigation = page.getByRole('navigation');
-      await expect(navigation).toHaveCount(1);
-      const listItems = page.getByRole('listitem');
-      await expect(listItems).toHaveCount(3);
-      const pageOne = page.getByRole('link').filter({ hasText: 'Page 1' });
-      const pageTwo = page.getByRole('link').filter({ hasText: 'Page 2' });
-      const pageThree = page.getByRole('link').filter({ hasText: 'Page 3' });
-
-      await expect(pageOne).toHaveCount(1);
-      await expect(pageOne).toBeVisible();
-
-      await expect(pageTwo).toHaveCount(1);
-      await expect(pageTwo).toBeVisible();
-
-      await expect(pageThree).toHaveCount(1);
-      await expect(pageThree).toBeVisible();
-    });
-
     test('page 3 item should be able to become focused', async ({ page }) => {
       const pageThree = page.getByText(/Page 3/);
       await pageThree.focus();
@@ -47,5 +28,30 @@ testConfigurations.basic.forEach((config) => {
       /* Check diff on screenshot */
       await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
     });
+  });
+});
+
+test.describe.parallel(componentName, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentTestPath);
+  });
+
+  test('items Page 1, Page 2, Page 3 exist on the page', async ({ page }) => {
+    const navigation = page.getByRole('navigation');
+    await expect(navigation).toHaveCount(1);
+    const listItems = page.getByRole('listitem');
+    await expect(listItems).toHaveCount(3);
+    const pageOne = page.getByRole('link').filter({ hasText: 'Page 1' });
+    const pageTwo = page.getByRole('link').filter({ hasText: 'Page 2' });
+    const pageThree = page.getByRole('link').filter({ hasText: 'Page 3' });
+
+    await expect(pageOne).toHaveCount(1);
+    await expect(pageOne).toBeVisible();
+
+    await expect(pageTwo).toHaveCount(1);
+    await expect(pageTwo).toBeVisible();
+
+    await expect(pageThree).toHaveCount(1);
+    await expect(pageThree).toBeVisible();
   });
 });
