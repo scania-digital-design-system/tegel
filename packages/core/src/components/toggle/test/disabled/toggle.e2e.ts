e2c936de891fa032e09005529d1dcb487ac2aa48
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-14 11:33:22 +0100

    test(toggle): move non-screenshot tests to not run for all style configurations

--- packages/core/src/components/toggle/test/disabled/toggle.e2e.ts
+++ packages/core/src/components/toggle/test/disabled/toggle.e2e.ts
@@ -24,28 +24,32 @@ testConfigurations.basic.forEach((config) => {
       /* Check diff on screenshot */
       await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
     });
+  });
+});
 
-    test('Hover over toggle -> should have inactive cursor', async ({ page }) => {
-      const label = page.locator('tds-toggle label');
-      const headline = page.locator('tds-toggle .toggle-headline');
-      const input = page.locator('tds-toggle input');
+test.describe.parallel(componentName, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(componentTestPath);
+  });
 
-      // Label should have a not-allowed cursor
-      await label.hover();
-      const labelCursorStyle = await label.evaluate((style) => getComputedStyle(style).cursor);
-      expect(labelCursorStyle).toBe('not-allowed');
+  test('Hover over toggle -> should have inactive cursor', async ({ page }) => {
+    const label = page.locator('tds-toggle label');
+    const headline = page.locator('tds-toggle .toggle-headline');
+    const input = page.locator('tds-toggle input');
 
-      // Headline should have a not-allowed cursor
-      await headline.hover();
-      const headlineCursorStyle = await headline.evaluate(
-        (style) => getComputedStyle(style).cursor,
-      );
-      expect(headlineCursorStyle).toBe('not-allowed');
+    // Label should have a not-allowed cursor
+    await label.hover();
+    const labelCursorStyle = await label.evaluate((style) => getComputedStyle(style).cursor);
+    expect(labelCursorStyle).toBe('not-allowed');
 
-      // Input should have a not-allowed cursor
-      await input.hover();
-      const inputCursorStyle = await input.evaluate((style) => getComputedStyle(style).cursor);
-      expect(inputCursorStyle).toBe('not-allowed');
-    });
+    // Headline should have a not-allowed cursor
+    await headline.hover();
+    const headlineCursorStyle = await headline.evaluate((style) => getComputedStyle(style).cursor);
+    expect(headlineCursorStyle).toBe('not-allowed');
+
+    // Input should have a not-allowed cursor
+    await input.hover();
+    const inputCursorStyle = await input.evaluate((style) => getComputedStyle(style).cursor);
+    expect(inputCursorStyle).toBe('not-allowed');
   });
 });
