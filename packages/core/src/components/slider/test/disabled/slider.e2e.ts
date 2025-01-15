f9b0a26597db9339a95715bb2562f89f4d5b26aa
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-15 14:27:20 +0100

    test(slider): add screenshot tests using reusable test code (#953)

--- packages/core/src/components/slider/test/disabled/slider.e2e.ts
+++ packages/core/src/components/slider/test/disabled/slider.e2e.ts
@@ -1,27 +1,46 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/slider/test/disabled/index.html';
-test.describe.parallel('tds-slider-disabled', () => {
-  test('read-only is false', async ({ page }) => {
+const componentName = 'tds-slider';
+const testDescription = 'tds-slider-disabled';
+
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
+
+    test('slider is disabled', async ({ page }) => {
+      const slider = page.locator('tds-slider input');
+      const sliderThumb = page.locator('.tds-slider__thumb');
+      const sliderCursorStyle = await sliderThumb.evaluate(
+        (style) => getComputedStyle(style).cursor,
+      );
+      expect(sliderCursorStyle).toBe('not-allowed');
+      /* Check diff on screenshot as disabled state changes colors of a component */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+      await expect(slider).toBeDisabled();
+    });
+  });
+});
+
+test.describe.parallel(componentName, () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
+
+  test('read-only is false', async ({ page }) => {
     const slider = page.locator('tds-slider');
     await expect(slider).not.toHaveAttribute('read-only');
   });
 
-  test('slider is disabled', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const slider = page.locator('tds-slider input');
-    const sliderThumb = page.locator('.tds-slider__thumb');
-    const sliderCursorStyle = await sliderThumb.evaluate((style) => getComputedStyle(style).cursor);
-    expect(sliderCursorStyle).toBe('not-allowed');
-    /* Check diff on screenshot as disabled state changes colors of a component */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-    await expect(slider).toBeDisabled();
-  });
-
   test('slider can not be clicked on', async ({ page }) => {
-    await page.goto(componentTestPath);
     const sliderThumb = page.locator('.tds-slider__thumb-inner');
     const sliderPointerEvents = await sliderThumb.evaluate(
       (style) => getComputedStyle(style).pointerEvents,
--- packages/core/src/components/slider/test/read-only/slider.e2e.ts
+++ packages/core/src/components/slider/test/read-only/slider.e2e.ts
@@ -1,25 +1,44 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/slider/test/read-only/index.html';
-test.describe.parallel('tds-slider-read-only', () => {
-  test('read-only is true', async ({ page }) => {
+const componentName = 'tds-slider';
+const testDescription = 'tds-slider-read-only';
+
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
+
+    test('is not disabled', async ({ page }) => {
+      const sliderThumb = page.locator('.tds-slider__thumb-inner');
+      const sliderCursorStyle = await sliderThumb.evaluate(
+        (style) => getComputedStyle(style).cursor,
+      );
+      expect(sliderCursorStyle).toBe('pointer');
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
+  });
+});
+
+test.describe.parallel(componentName, () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
+  });
+
+  test('read-only is true', async ({ page }) => {
     const slider = page.locator('tds-slider');
     await expect(slider).toHaveAttribute('read-only');
   });
 
-  test('is not disabled', async ({ page }) => {
-    await page.goto(componentTestPath);
-    const sliderThumb = page.locator('.tds-slider__thumb-inner');
-    const sliderCursorStyle = await sliderThumb.evaluate((style) => getComputedStyle(style).cursor);
-    expect(sliderCursorStyle).toBe('pointer');
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
-
   test('slider can not be clicked on', async ({ page }) => {
-    await page.goto(componentTestPath);
     const sliderThumb = page.locator('.tds-slider__thumb-inner');
     const sliderThumbStyle = await sliderThumb.evaluate(
       (style) => getComputedStyle(style).pointerEvents,
