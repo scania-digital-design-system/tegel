ee076ff0883927fb5f70f1c3d9cb24c1f77c86e5
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-15 14:56:46 +0100

    test(card): use reusable test code for lightmode and darkmode screenshot tests (#940)

--- packages/core/src/components/card/test/clickable/card.e2e.ts
+++ packages/core/src/components/card/test/clickable/card.e2e.ts
@@ -1,20 +1,37 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/card/test/clickable/index.html';
+const componentName = 'tds-card';
+const testDescription = 'tds-card-clickable';
 
-test.describe.parallel('tds-card-clickable', () => {
-  test('renders clickable card correctly', async ({ page }) => {
+testConfigurations.withModeVariants.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
+
+    test('renders clickable card correctly', async ({ page }) => {
+      const cardButton = page.getByRole('button');
+      await cardButton.hover();
+
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
+  });
+});
+
+test.describe.parallel(componentName, () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
-    const cardButton = page.getByRole('button');
-    await cardButton.hover();
-
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 
   test('card should contain a button that is clickable', async ({ page }) => {
-    await page.goto(componentTestPath);
     const cardButton = page.getByRole('button');
     await expect(cardButton).toHaveCount(1);
     await expect(cardButton).toBeVisible();
