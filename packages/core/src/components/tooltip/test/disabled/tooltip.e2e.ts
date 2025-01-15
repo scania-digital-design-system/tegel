ad0d938412de2d627d72c52175fcdf1247c66182
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-15 14:24:14 +0100

    test(tooltip): add screenshot tests using reusable test code (#960)
    * test(tooltip): use reusable test code for lightmode and darkmode screenshot tests
    
    * test(tooltip): move non-screenshot tests to not run for all style configurations

--- packages/core/src/components/tooltip/test/disabled/tooltip.e2e.ts
+++ packages/core/src/components/tooltip/test/disabled/tooltip.e2e.ts
@@ -1,37 +1,48 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/tooltip/test/disabled/index.html';
+const componentName = 'tds-tooltip';
 
-test.describe('tds-tooltip', () => {
-  test('renders the tooltip correctly', async ({ page }) => {
-    await page.goto(componentTestPath);
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-  });
+    test('renders the tooltip correctly', async ({ page }) => {
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
 
-  test('tooltip closes when cursor is removed from the button', async ({ page }) => {
-    await page.goto(componentTestPath);
+    test('tooltip closes when cursor is removed from the button', async ({ page }) => {
+      // Assuming your button and tooltip setup is already appropriate for this test
+      const button = page.locator('tds-button#button-2');
 
-    // Assuming your button and tooltip setup is already appropriate for this test
-    const button = page.locator('tds-button#button-2');
+      // Hover over the button to show the tooltip
+      await button.hover();
+      // Assuming you can uniquely identify the tooltip that should be visible on hover
+      const tooltip = page.locator('text=Text inside Tooltip');
 
-    // Hover over the button to show the tooltip
-    await button.hover();
-    // Assuming you can uniquely identify the tooltip that should be visible on hover
-    const tooltip = page.locator('text=Text inside Tooltip');
+      await expect(tooltip).toBeVisible();
+      // Make sure tooltip is visible first
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
 
-    await expect(tooltip).toBeVisible();
-    // Make sure tooltip is visible first
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+      const buttonBox = await button.boundingBox();
+      // Move the mouse to a position slightly outside the button's bounding box
+      await page.mouse.move(
+        buttonBox.x + buttonBox.width + 10,
+        buttonBox.y + buttonBox.height + 10,
+      );
 
-    const buttonBox = await button.boundingBox();
-    // Move the mouse to a position slightly outside the button's bounding box
-    await page.mouse.move(buttonBox.x + buttonBox.width + 10, buttonBox.y + buttonBox.height + 10);
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
 
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-
-    // Verify the tooltip is no longer visible
-    await expect(tooltip).toBeHidden();
+      // Verify the tooltip is no longer visible
+      await expect(tooltip).toBeHidden();
+    });
   });
 });
