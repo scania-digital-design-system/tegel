35426a9aafad1e23a9f6f2eb7012fd654b21be25
Charles Krook <charles.krook@gmail.com>
2025-01-16 11:55:59 +0100

    fix: update Figma design links in motion stories

--- packages/core/src/stories/motions/transitions/transitions-zoom.stories.tsx
+++ packages/core/src/stories/motions/transitions/transitions-zoom.stories.tsx
@@ -3,12 +3,11 @@ import formatHtmlPreview from '../../formatHtmlPreview';
 export default {
   title: 'Motions/Transitions/Zoom',
   parameters: {
-    layout: 'fullscreen',
     design: [
       {
         name: 'Figma',
         type: 'figma',
-        url: 'https://www.figma.com/file/d8bTgEx7h694MSesi2CTLF/Tegel-UI-Library?node-id=11142%3A42941&t=Ne6myqwca5m00de7-1',
+        url: 'https://www.figma.com/design/3vglvmuE8d0wXQRhFM0Op6/Tegel-Motions?node-id=9-167&p=f&t=J2E8IQiExfDl7AHR-0',
       },
     ],
   },
