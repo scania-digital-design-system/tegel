4d71e5c6a1c75797ff51a162edde06b4cdb7b5cc
Charles Krook <charles.krook@gmail.com>
2025-01-17 21:23:58 +0100

    feature(motion): Storybook variable documentation (#966)
    * feat(motion): add slide in/out animations and update motion parameters
    
    * test: saving screenshots of failing tests
    
    * test: corrected output directory
    
    * test: fix output directory path
    
    * test: using artifacts upload v4
    
    * test: reverting to older ubuntu version
    
    * test: lowering artifacts retention days
    
    * feat(motion): enhance animation classes and improve visibility handling in keyframes
    
    * feat(motion): update animation styles and add new transition stories
    
    * fix: update Figma design links in motion stories
    
    * feat(motion): add slide in/out animations and update motion parameters
    
    * feat(motion): enhance animation classes and improve visibility handling in keyframes
    
    * feat(motion): update animation styles and add new transition stories
    
    * fix: update Figma design links in motion stories
    
    * feat(motion): add usage instructions for animation properties in transition stories
    
    * feat(motion): add usage instructions for animation variables in animation classes stories
    
    * feat(motion): refine animation stories and update motion variables in transitions
    
    * feat(motion): update animation variables and improve storybook examples
    
    * chore(docs): restore docs files
    
    * chore(test): remove darwin snapshots
    
    * feat(motion): refactor collapse animations to use transform scaling instead of height
    
    ---------
    
    Co-authored-by: Johan <johan.lundgren@umain.com>

---
+++ packages/core/src/stories/motions/transitions/transitions-collapse.stories.tsx
@@ -0,0 +1,97 @@
+import formatHtmlPreview from '../../formatHtmlPreview';
+
+export default {
+  title: 'Motions/Transitions/Collapse',
+  parameters: {
+    layout: 'fullscreen',
+    design: [
+      {
+        name: 'Figma',
+        type: 'figma',
+        url: 'https://www.figma.com/design/3vglvmuE8d0wXQRhFM0Op6/Tegel-Motions?node-id=9-167&p=f&t=J2E8IQiExfDl7AHR-0',
+      },
+    ],
+  },
+};
+
+const Template = () =>
+  formatHtmlPreview(
+    `
+    <main>
+      <h2>Collapse</h2>
+      
+      <p>
+        Use these variables in the <code>animation</code> property of your elements to 
+        achieve smooth and uniform transitions, as demonstrated below.
+      </p>
+
+      <div class="tds-u-flex-start tds-u-gap4">
+        <div>
+          <div class="grey-box" style="height: 200px;">
+            <div class="content">
+              <div class="collapse-in-box"></div>
+            </div>
+          </div>
+          <p>--tds-motion-collapse-in</p>
+        </div>
+
+        <div>
+          <div class="grey-box" style="height: 200px;">
+            <div class="content">
+              <div class="collapse-out-box"></div>
+            </div>
+          </div>
+          <p>--tds-motion-collapse-out</p>
+        </div>
+      </div>
+    </main>
+
+    <style>
+      main {
+        padding: 20px;
+      }
+
+      .grey-box {
+        width: 200px;
+        height: 200px;
+        background-color: #EDEFF3;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 8px;
+        overflow: hidden;
+      }
+
+      .content {
+        width: 100px;
+        height: 100px;
+      }
+
+      .collapse-in-box,
+      .collapse-out-box {
+        border-radius: 4px;
+        width: 100px;
+        height: 100px;
+        background-color: #041E42;
+      }
+
+      .collapse-in-box {
+        transform: scaleY(0);
+      }
+
+      .collapse-out-box {
+        transform: scaleY(0);
+      }
+
+      .grey-box:hover .collapse-in-box {
+        animation: var(--tds-motion-collapse-in);
+      }
+
+      .grey-box:hover .collapse-out-box {
+        animation: var(--tds-motion-collapse-out);
+      }
+    </style>
+    `,
+  );
+
+export const Collapse = Template.bind({});
