1d800dff520afd62f605262ba63829dfbd52827a
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-20 13:41:11 +0100

    test(divider): add screenshot tests using reusable test code

--- packages/core/src/components/divider/test/horizontal/divider.e2e.ts
+++ packages/core/src/components/divider/test/horizontal/divider.e2e.ts
@@ -1,15 +1,23 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/divider/test/horizontal/index.html';
+const componentName = 'tds-divider';
 
-test.describe.parallel('tds-divider', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(componentTestPath);
-  });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-  test('expect to render a divider', async ({ page }) => {
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    test('expect to render a divider', async ({ page }) => {
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
   });
 });
--- packages/core/src/components/divider/test/vertical/divider.e2e.ts
+++ packages/core/src/components/divider/test/vertical/divider.e2e.ts
@@ -1,20 +1,28 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/divider/test/vertical/index.html';
+const componentName = 'tds-divider';
 
-test.describe.parallel('tds-divider', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(componentTestPath);
-  });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
 
-  test('expect to render a vertical divider', async ({ page }) => {
-    // expect height to be greater than width
-    const divider = page.getByTestId('divider').locator('div.divider').first();
-    const box = await divider.boundingBox();
-    expect(box.height).toBeGreaterThan(box.width);
+    test('expect to render a vertical divider', async ({ page }) => {
+      // expect height to be greater than width
+      const divider = page.getByTestId('divider').locator('div.divider').first();
+      const box = await divider.boundingBox();
+      expect(box.height).toBeGreaterThan(box.width);
 
-    /* Check diff on screenshot */
-    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+      /* Check diff on screenshot */
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
   });
 });
