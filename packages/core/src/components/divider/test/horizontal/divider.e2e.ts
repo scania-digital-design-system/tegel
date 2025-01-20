a6816361deb373db996ddc462ea320b20d3d47d3
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-20 13:33:42 +0100

    test(divider): change to folder structure without lightmode or darkmode

--- packages/core/src/components/divider/test/horizontal/divider.e2e.ts
+++ packages/core/src/components/divider/test/horizontal/divider.e2e.ts
@@ -1,14 +1,14 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 
-const componentTestPath = 'src/components/divider/test/horizontal/darkmode/index.html';
+const componentTestPath = 'src/components/divider/test/horizontal/index.html';
 
-test.describe.parallel('tds-divider-darkmode', () => {
+test.describe.parallel('tds-divider', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(componentTestPath);
   });
 
-  test('expect to render a divider in darkmode', async ({ page }) => {
+  test('expect to render a divider', async ({ page }) => {
     /* Check diff on screenshot */
     await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
