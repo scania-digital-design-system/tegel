43523dfb0fe6af1b654033fd17e188b83a2b6e19
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-01-20 14:18:38 +0100

    test(stepper): add screenshot tests using reusable test code

--- packages/core/src/components/stepper/test/small-vertical-text-aside/stepper.e2e.ts
+++ packages/core/src/components/stepper/test/small-vertical-text-aside/stepper.e2e.ts
@@ -1,12 +1,25 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
+import {
+  testConfigurations,
+  getTestDescribeText,
+  setupPage,
+} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/stepper/test/small-vertical-text-aside/index.html';
+const componentName = 'tds-stepper';
 
-//Test if component renders and take a screenshot of the component
-test('Stepper - Small vertical text aside', async ({ page }) => {
-  await page.goto(componentTestPath);
-  const stepper = page.locator('[data-testid="tds-stepper-testid"]');
-  await stepper.waitFor({ state: 'visible' });
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+testConfigurations.basic.forEach((config) => {
+  test.describe.parallel(getTestDescribeText(config, componentName), () => {
+    test.beforeEach(async ({ page }) => {
+      await setupPage(page, config, componentTestPath, componentName);
+    });
+
+    // Test if component renders and take a screenshot of the component
+    test('Stepper - Small vertical text aside', async ({ page }) => {
+      const stepper = page.locator('[data-testid="tds-stepper-testid"]');
+      await stepper.waitFor({ state: 'visible' });
+      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
+    });
+  });
 });
