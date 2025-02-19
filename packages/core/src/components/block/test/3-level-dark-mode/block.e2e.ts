043b677f8ed0b6f1f78462e9f7f567acb676cb44
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2025-02-19 10:07:38 +0100

    fix(block): nesting bg color issue (#1010)
    * fix(block): nesting coloring issue
    
    * fix: docs and test improvement

--- packages/core/src/components/block/test/3-level-dark-mode/block.e2e.ts
+++ packages/core/src/components/block/test/3-level-dark-mode/block.e2e.ts
@@ -1,24 +1,11 @@
 import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
-import {
-  testConfigurations,
-  getTestDescribeText,
-  setupPage,
-} from '../../../../utils/testConfiguration';
 
 const componentTestPath = 'src/components/block/test/3-level-dark-mode/index.html';
-const componentName = 'tds-block';
-const testDescription = 'tds-block-3-level-dark-mode';
 
-testConfigurations.withModeVariants.forEach((config) => {
-  test.describe.parallel(getTestDescribeText(config, testDescription), () => {
-    test.beforeEach(async ({ page }) => {
-      await setupPage(page, config, componentTestPath, componentName);
-    });
-
-    test('renders 2 level block correctly', async ({ page }) => {
-      /* Check diff on screenshot */
-      await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
-    });
+test.describe('3-level block screenshot test', () => {
+  test('renders 3 level block correctly', async ({ page }) => {
+    await page.goto(componentTestPath);
+    await expect(page).toHaveScreenshot({ maxDiffPixels: 0 });
   });
 });
