7fc36749eda5265e302716d723b4981e09af2330
Charles Krook <charles.krook@gmail.com>
2025-02-24 11:46:04 +0100

    test(accordion): improve accessibility test by ensuring no violations are detected

--- packages/core/src/components/accordion/test/basic/accordion.axe.ts
+++ packages/core/src/components/accordion/test/basic/accordion.axe.ts
@@ -2,17 +2,13 @@ import { test } from 'stencil-playwright';
 import { expect } from '@playwright/test';
 import { tegelAnalyze } from '../../../../utils/axeHelpers';
 
-
 const componentTestPath = 'src/components/accordion/test/basic/index.html';
 
 test.describe.parallel('Accordion accessibility test', () => {
   test('Should render without detected accessibility issues', async ({ page }) => {
     await page.goto(componentTestPath);
-    const { violations } = await tegelAnalyze(page)
+    const { violations } = await tegelAnalyze(page);
 
     expect(violations).toEqual([]);
   });
-  // tab sequence
-  // Esc should close
-  // Other keyboard interaction
 });
