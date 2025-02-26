8e310e847ae4052d31133dcdb4eaf6846729d035
Charles Krook <charles.krook@gmail.com>
2025-02-26 08:45:50 +0100

    feat(core): add hydrate support in package.json and update .gitignore

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -69,6 +69,9 @@ export const config: Config = {
         },
       ],
     },
+    {
+      type: 'dist-hydrate-script',
+    },
     angularOutputTarget({
       componentCorePackage: '@scania/tegel',
       outputType: 'component',
