c44bf1615a8e313dec802ee398c85c57de1ed59f
mistermalm <45939401+mistermalm@users.noreply.github.com>
2025-02-28 11:29:21 +0100

    chore(textfield): removed linter warning (#1037)
    * chore(textfield): removed lint warning
    
    * chore(textfield): lint error fix

--- packages/core/src/components/text-field/text-field.tsx
+++ packages/core/src/components/text-field/text-field.tsx
@@ -1,5 +1,5 @@
-import hasSlot from '../../utils/hasSlot';
 import { Component, Element, Event, EventEmitter, h, Method, Prop, State } from '@stencil/core';
+import hasSlot from '../../utils/hasSlot';
 
 /**
  * @slot prefix - Slot for the prefix in the component.
@@ -181,7 +181,9 @@ export class TdsTextField {
 
           <div class="text-field-input-container">
             <input
-              ref={(inputEl) => (this.textInput = inputEl as HTMLInputElement)}
+              ref={(inputEl) => {
+                this.textInput = inputEl as HTMLInputElement;
+              }}
               class={{
                 'text-field-input': true,
                 'text-field-input-sm': this.size === 'sm',
