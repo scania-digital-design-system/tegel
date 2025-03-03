7730d714b6464d3e8b6356874450155fe0517262
mistermalm <45939401+mistermalm@users.noreply.github.com>
2025-03-03 12:08:53 +0100

    chore(helper): remove linter warning (#1030)

--- packages/core/src/utils/axeHelpers.ts
+++ packages/core/src/utils/axeHelpers.ts
@@ -1,9 +1,7 @@
-
 import AxeBuilder from '@axe-core/playwright';
 import type { Page } from '@playwright/test';
 
-const disabledRules = ["page-has-heading-one", "landmark-one-main"]
+const disabledRules = ['page-has-heading-one', 'landmark-one-main'];
 
-export const tegelAnalyze = async (page: Page) => {
-  return new AxeBuilder({ page }).disableRules(disabledRules).analyze();
-}
+export const tegelAnalyze = async (page: Page) =>
+  new AxeBuilder({ page }).disableRules(disabledRules).analyze();
