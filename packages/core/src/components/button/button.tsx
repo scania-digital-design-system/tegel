f474a2ba32f30fbbe31ac8266c968c533f12a5f9
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-03-05 11:07:42 +0100

    fix(button): improve accessibility

--- packages/core/src/components/button/button.tsx
+++ packages/core/src/components/button/button.tsx
@@ -71,6 +71,7 @@ export class TdsButton {
             'only-icon': this.onlyIcon,
             [`animation-${this.animation}`]: this.animation !== 'none',
           }}
+          {...(this.onlyIcon && { 'aria-label': 'button' })}
         >
           {this.text}
           {hasLabelSlot && !this.onlyIcon && <slot name="label" />}
