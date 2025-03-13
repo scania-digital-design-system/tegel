fe186fe0d356865758638eb0ab13986d800de7cb
Charles Krook <charles.krook@gmail.com>
2025-03-13 12:56:50 +0100

    chore(table): enhance a11y by adding aria-controls and updating aria-expanded for expandable rows

--- packages/core/src/components/table/table-body-row-expandable/table-body-row-expandable.tsx
+++ packages/core/src/components/table/table-body-row-expandable/table-body-row-expandable.tsx
@@ -192,12 +192,12 @@ export class TdsTableBodyRowExpandable {
         }}
       >
         <tr
+          id={`expandable-content-${this.rowId}`}
           class={{
             'tds-table__row': true,
             'tds-table__row--expanded': this.isExpanded,
           }}
           part="row"
-          aria-expanded={this.isExpanded}
         >
           <td
             class={{
@@ -210,6 +210,8 @@ export class TdsTableBodyRowExpandable {
                 type="checkbox"
                 onChange={(event) => this.onChangeHandler(event)}
                 checked={this.isExpanded}
+                aria-expanded={this.isExpanded ? 'true' : 'false'}
+                aria-controls={`expandable-content-${this.rowId}`}
               />
               <span class="tds-expendable-row-icon">
                 <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
