148a76d53526b8ad26b1040d29f9fc0b4e8acbc4
Artur Höppner <Artur.Hoppner@hotmail.com>
2025-03-25 11:39:24 +0100

    Feat/tegel light setup accordion update (#1091)
    * Updated version of ts-accordion

--- packages/core/src/tegel-light/components/tl-accordion/tl-accordion.stories.tsx
+++ packages/core/src/tegel-light/components/tl-accordion/tl-accordion.stories.tsx
@@ -1,6 +1,4 @@
 import formatHtmlPreview from '../../../stories/formatHtmlPreview';
-// import readme from './readme.md';
-// import readmeItem from './accordion-item/readme.md';
 
 export default {
   title: `Tegel light (CSS)/Accordion`,
@@ -14,7 +12,7 @@ export default {
       },
       options: ['Default', 'Primary', 'Secondary'],
       table: {
-        defaultValue: { summary: 'No class (Default)' },
+        defaultValue: { summary: 'Default (No class)' },
       },
     },
     iconPosition: {
@@ -59,23 +57,8 @@ export default {
       },
     },
   },
-  parameters: {
-    // notes: { 'Accordion': readme, 'Accordion Item': readmeItem },
-    design: [
-      {
-        name: 'Figma',
-        type: 'figma',
-        url: 'https://www.figma.com/file/d8bTgEx7h694MSesi2CTLF/Tegel-UI-Library?node-id=2762%3A84&t=rVXuTOgTmXPauyHd-1',
-      },
-      {
-        name: 'Link',
-        type: 'link',
-        url: 'https://www.figma.com/file/d8bTgEx7h694MSesi2CTLF/Tegel-UI-Library?node-id=2762%3A84&t=rVXuTOgTmXPauyHd-1',
-      },
-    ],
-  },
   args: {
-    modeVariant: '',
+    modeVariant: 'Default',
     iconPosition: false,
     paddingReset: false,
     disabled: false,
@@ -84,25 +67,30 @@ export default {
 };
 
 const Template = ({ disabled, iconPosition, paddingReset, modeVariant, hideLastBorder }) => {
-  const disabledClass = disabled ? 'tl-accordion__button--disabled' : '';
+  const disabledClass = disabled ? 'tl-accordion--disabled' : '';
   const iconPositionClass = iconPosition ? 'tl-accordion__button--icon-start' : '';
   const paddingResetClass = paddingReset ? 'tl-accordion__body--less-padding-right' : '';
   const hideLastBorderClass = hideLastBorder ? '' : 'tl-accordion__item--bottom-border';
   const modeVariantClass =
-    modeVariant === 'Primary'
+    modeVariant === 'tds-mode-variant-primary'
       ? 'tl-accordion--primary'
       : modeVariant === 'Secondary'
-      ? 'tl-accordion--secondary'
+      ? 'tds-mode-variant-secondary'
       : '';
 
   return formatHtmlPreview(`
-    <div class="tl-accordion ${modeVariantClass}">
+
+    <script>
+      import "@scania/tegel-light/tl-accordion.css"
+    </script>
+
+    <div class="tl-accordion ${modeVariantClass} ${disabledClass}">
       <div class="tl-accordion__item ${hideLastBorderClass}">
-        <button class="tl-accordion__button ${iconPositionClass} ${disabledClass}">
+        <button class="tl-accordion__button ${iconPositionClass}">
           <span>
              First item
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-labelledby="title-chevron_down" role="img" height="16px" width="16px" style="font-size: 16px;"><title id="title-chevron_down">Chevron Down</title><path fill="currentColor" d="M4.273 9.783a1 1 0 0 1 1.415 0l9.888 9.888a.6.6 0 0 0 .848 0l9.888-9.888a1 1 0 1 1 1.415 1.414l-9.889 9.889a2.6 2.6 0 0 1-3.677 0l-9.888-9.889a1 1 0 0 1 0-1.414Z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height="16px" width="16px" style="font-size: 16px;"><path fill="currentColor" d="M4.273 9.783a1 1 0 0 1 1.415 0l9.888 9.888a.6.6 0 0 0 .848 0l9.888-9.888a1 1 0 1 1 1.415 1.414l-9.889 9.889a2.6 2.6 0 0 1-3.677 0l-9.888-9.889a1 1 0 0 1 0-1.414Z"></path></svg>
         </button>
         <div class="tl-accordion__body ${paddingResetClass}">
           <span>
@@ -116,9 +104,9 @@ const Template = ({ disabled, iconPosition, paddingReset, modeVariant, hideLastB
           <span>
             Second item
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-labelledby="title-chevron_down" role="img" height="16px" width="16px" style="font-size: 16px;"><title id="title-chevron_down">Chevron Down</title><path fill="currentColor" d="M4.273 9.783a1 1 0 0 1 1.415 0l9.888 9.888a.6.6 0 0 0 .848 0l9.888-9.888a1 1 0 1 1 1.415 1.414l-9.889 9.889a2.6 2.6 0 0 1-3.677 0l-9.888-9.889a1 1 0 0 1 0-1.414Z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" role="img" height="16px" width="16px" style="font-size: 16px;"><path fill="currentColor" d="M4.273 9.783a1 1 0 0 1 1.415 0l9.888 9.888a.6.6 0 0 0 .848 0l9.888-9.888a1 1 0 1 1 1.415 1.414l-9.889 9.889a2.6 2.6 0 0 1-3.677 0l-9.888-9.889a1 1 0 0 1 0-1.414Z"></path></svg>
         </button>
-        <div class="tl-accordion__body ${paddingResetClass}">
+        <div class="tl-accordion__body tl-accordion__body--open ${paddingResetClass}">
           <span>
             This is the panel, which contains associated information with the header. Usually it contains text, set in the same size as the header. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet vestibulum fermentum.
           </span>
