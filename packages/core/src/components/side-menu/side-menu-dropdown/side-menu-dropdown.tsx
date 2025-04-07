8ad720f960c11dfad1c59ec02977f90b7bf3f023
Charles Krook <charles.krook@gmail.com>
2025-04-07 14:22:53 +0200

    feat(side-menu-dropdown): add keyboard accessibility and aria-expanded attribute

--- packages/core/src/components/side-menu/side-menu-dropdown/side-menu-dropdown.tsx
+++ packages/core/src/components/side-menu/side-menu-dropdown/side-menu-dropdown.tsx
@@ -58,6 +58,14 @@ export class TdsSideMenuDropdown {
     this.setHoverStateClosed();
   }
 
+  @Listen('keydown')
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.open = !this.open;
+    }
+  }
+
   setHoverStateOpen() {
     this.hoverState = { isHovered: true, updatedAt: Date.now() };
   }
@@ -99,6 +107,7 @@ export class TdsSideMenuDropdown {
             onClick={() => {
               this.open = !this.open;
             }}
+            aria-expanded={this.getIsOpenState() ? 'true' : 'false'}
           >
             <button>
               <slot name="icon"></slot>
@@ -106,7 +115,12 @@ export class TdsSideMenuDropdown {
                 <Fragment>
                   {this.buttonLabel}
                   <slot name="label"></slot>
-                  <tds-icon class="dropdown-icon" name="chevron_down" size="16px"></tds-icon>
+                  <tds-icon
+                    class="dropdown-icon"
+                    name="chevron_down"
+                    size="16px"
+                    svgTitle="Chevron Down"
+                  ></tds-icon>
                 </Fragment>
               )}
             </button>
