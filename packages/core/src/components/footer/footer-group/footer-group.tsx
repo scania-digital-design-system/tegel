47296b2475fa8416714b036985a7977d63ffd94a
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-04-10 16:16:05 +0200

    feat(footer): accessibility improvements (#1105)
    * fix(footer): accessibility improvements
    
    * fix(footer): add title slot, adjust scss
    
    * fix(footer): remove h4 element when using titleText prop

--- packages/core/src/components/footer/footer-group/footer-group.tsx
+++ packages/core/src/components/footer/footer-group/footer-group.tsx
@@ -1,7 +1,9 @@
-import { Component, Host, h, Prop, Element, State } from '@stencil/core';
+import { Component, Host, h, Prop, Element, State, Listen } from '@stencil/core';
+import hasSlot from '../../../utils/hasSlot';
 
 /**
  * @slot <default> - <b>Unnamed slot.</b> For footer items.
+ * @slot title - Slot for the title of the group. Should contain a heading element adjusted to the rest of the website in terms of heading nesting, for accessibility.
  */
 @Component({
   tag: 'tds-footer-group',
@@ -14,8 +16,14 @@ export class TdsFooterGroup {
   /** Title text for the link group, only valid on top part of Footer. */
   @Prop() titleText: string;
 
+  /** Value to be used for the aria-label attribute for the nav element wrapping the list. Should be unique for accessibility. */
+  @Prop() tdsListAriaLabel: string;
+
   /** In mobile, this indicates when the group (if it's in the top part) is opened/closed. */
   @State() open: boolean = false;
+
+  /** Indicates if the component is in mobile view. */
+  @State() isMobile: boolean;
 
   /** If the group is placed in the main part of the Footer,
    * it can have either start or end as a slot position otherwise undefined. */
@@ -24,38 +32,74 @@ export class TdsFooterGroup {
   /** Indicates if a group is part of the top part of the Footer. */
   private topPartGroup: boolean = false;
 
+  private updateIsMobile() {
+    this.isMobile = window.innerWidth <= 992;
+  }
+
   connectedCallback() {
+    this.updateIsMobile();
+
     this.topPartGroup = this.host.parentElement.slot === 'top';
     if (!this.topPartGroup) {
       this.slotPosition = this.host.parentElement.slot === 'end' ? 'end' : 'start';
     }
+
+    if (!this.tdsListAriaLabel) {
+      console.warn('Tegel Footer Group component: missing tdsListAriaLabel prop');
+    }
+
+    if (this.titleText) {
+      console.warn(
+        'Tegel Footer Group component: to ensure accessibility, please use the title slot instead of the titleText prop. titleText will be deprecated in the future.',
+      );
+    }
+
+    const hasTitleSlot = hasSlot('title', this.host);
+    if (hasTitleSlot && this.titleText) {
+      console.warn(
+        'Tegel Footer Group component: titleText prop is set but title slot is used. The titleText prop will be ignored.',
+      );
+    }
+  }
+
+  @Listen('resize', { target: 'window' })
+  handleResize() {
+    this.updateIsMobile();
   }
 
   render() {
-    return (
-      <Host>
-        {this.titleText && this.topPartGroup && (
-          <div class="footer-top-title">{this.titleText}</div>
-        )}
+    const hasTitleSlot = hasSlot('title', this.host);
 
-        {this.titleText && this.topPartGroup && (
-          <button
-            onClick={() => {
-              this.open = !this.open;
-            }}
-            class={`footer-top-title-button  ${this.open ? 'expanded' : 'closed'}`}
-          >
-            {this.titleText}
-            <tds-icon name="chevron_down" size="20px"></tds-icon>
-          </button>
-        )}
-        <div
-          role="list"
-          class={`${this.slotPosition ? this.slotPosition : ''}
+    return (
+      <Host aria-expanded={this.open ? 'true' : 'false'}>
+        <div class={this.isMobile ? 'mobile-view' : ''}>
+          {this.topPartGroup &&
+            (this.isMobile ? (
+              <button
+                onClick={() => {
+                  this.open = !this.open;
+                }}
+                class={`footer-top-title-button ${this.open ? 'expanded' : 'closed'}`}
+              >
+                {hasTitleSlot ? <slot name="title" /> : this.titleText}
+                <tds-icon name="chevron_down" size="20px" aria-hidden="true"></tds-icon>
+              </button>
+            ) : (
+              <div class="footer-top-title">
+                {hasTitleSlot ? <slot name="title" /> : this.titleText}
+              </div>
+            ))}
+
+          <nav aria-label={this.tdsListAriaLabel}>
+            <div
+              role="list"
+              class={`${this.slotPosition ? this.slotPosition : ''}
             ${this.topPartGroup ? 'top-part-child' : ''}
             ${this.open ? 'expanded' : 'closed'}`}
-        >
-          <slot></slot>
+            >
+              <slot></slot>
+            </div>
+          </nav>
         </div>
       </Host>
     );
