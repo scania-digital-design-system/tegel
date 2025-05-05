6e9d0a2ae76055fe40aa2ebed44356eab12691de
mistermalm <github@jonatanmalm.com>
2025-05-05 15:14:02 +0200

    build(storybook): hide beta stories

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,8 +1,21 @@
 import { join } from 'path';
+import glob from 'glob';
+import path from 'path';
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 
+function loadStories() {
+  // Gather all story files synchronously
+  const storyFiles = glob.sync(path.resolve(__dirname, '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'));
+
+  // If in development environment, return all story files
+  // Otherwise, exclude stories from the _beta folder
+  return process.env.STORYBOOK_ENV === 'dev'
+    ? storyFiles
+    : storyFiles.filter((file) => !file.includes('/_beta/'));
+}
+
 const config = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: loadStories(),
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/html-vite',
@@ -17,4 +30,5 @@ const config = {
   },
   managerEntries: [join(__dirname, './manager.jsx')],
 };
+
 export default config;
