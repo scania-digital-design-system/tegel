a5860d6969f0fc2a18404fe304625aa72c5278b9
mistermalm <github@jonatanmalm.com>
2025-05-07 15:28:20 +0200

    chore(storybook): changed back structure due to issues

--- packages/core/.storybook/preview.ts
+++ packages/core/.storybook/preview.ts
@@ -1,11 +1,43 @@
 import { defineCustomElements } from '../loader';
 import '../dist/tegel/tegel.css';
-import { docs, options } from './parameters';
 
 const preview = {
   parameters: {
-    docs,
-    options,
+    docs: {
+      source: {
+        type: 'dynamic',
+      },
+      canvas: {
+        sourceState: 'shown',
+      },
+      controls: {
+        expanded: true,
+      },
+    },
+    options: {
+      showPanel: true,
+      showToolbar: true,
+      enableShortcuts: false,
+      storySort: {
+        method: 'alphabetical',
+        includeNames: true,
+        order: [
+          'Intro',
+          [
+            'Tegel Design System',
+            'Installation',
+            'Migrating from components v4',
+            'Events',
+            'Announcements',
+          ],
+          'Motions',
+          'Foundations',
+          'Utilities',
+          'Patterns',
+          'Components',
+        ],
+      },
+    },
     tags: ['autodocs'],
   },
 };
