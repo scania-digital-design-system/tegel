87e36d098c839d48b5bfb9866f9fccc13e2a250a
Johan Lundgren <johan.lundgren@umain.com>
2025-05-08 13:48:31 +0200

    test(card): added additional accessibility tests (#1181)

---
+++ packages/core/src/components/card/test/clickable/card.axe.ts
@@ -0,0 +1,14 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+import { tegelAnalyze } from '../../../../utils/axeHelpers';
+
+const componentTestPath = 'src/components/card/test/clickable/index.html';
+
+test.describe.parallel('Card clickable accessibility test', () => {
+  test('Should render clickable behavior without detected accessibility issues', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const { violations } = await tegelAnalyze(page);
+
+    expect(violations).toEqual([]);
+  });
+});
