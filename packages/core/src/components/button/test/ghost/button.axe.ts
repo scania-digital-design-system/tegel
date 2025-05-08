bfcbffbdaed98086c1c27c98cece543cec5205d5
Johan Lundgren <johan.lundgren@umain.com>
2025-05-08 13:28:37 +0200

    test(button): added more accessibility tests (#1179)

---
+++ packages/core/src/components/button/test/ghost/button.axe.ts
@@ -0,0 +1,14 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+import { tegelAnalyze } from '../../../../utils/axeHelpers';
+
+const componentTestPath = 'src/components/button/test/ghost/index.html';
+
+test.describe.parallel('Button ghost accessibility test', () => {
+  test('Should render ghost behavior without detected accessibility issues', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const { violations } = await tegelAnalyze(page);
+
+    expect(violations).toEqual([]);
+  });
+});
