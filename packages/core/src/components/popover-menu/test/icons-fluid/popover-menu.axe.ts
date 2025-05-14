17845f23ff38746b91476a72bde57405950feb58
Johan <johan.lundgren@umain.com>
2025-05-14 07:31:14 +0200

    test(popover-menu): added additional accessibility tests

---
+++ packages/core/src/components/popover-menu/test/icons-fluid/popover-menu.axe.ts
@@ -0,0 +1,14 @@
+import { test } from 'stencil-playwright';
+import { expect } from '@playwright/test';
+import { tegelAnalyze } from '../../../../utils/axeHelpers';
+
+const componentTestPath = 'src/components/popover-menu/test/icons-fluid/index.html';
+
+test.describe.parallel('Popover Menu Ion accessibility test', () => {
+  test('Should render Icon behavior without detected accessibility issues', async ({ page }) => {
+    await page.goto(componentTestPath);
+    const { violations } = await tegelAnalyze(page);
+
+    expect(violations).toEqual([]);
+  });
+});
