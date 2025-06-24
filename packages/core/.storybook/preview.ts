d311e085dbab485cadcfb98f425b98fafa371163
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-06-24 13:27:53 +0200

    fix: add back setting of darkmode in the url

--- packages/core/.storybook/preview.ts
+++ packages/core/.storybook/preview.ts
@@ -7,7 +7,51 @@ import '../dist/tegel/tegel.css';
 import './preview.css';
 import { ScaniaDark, ScaniaLight } from './ScaniaLogotype';
 
+import { UPDATE_GLOBALS } from '@storybook/core-events';
+
 const channel = addons.getChannel();
+
+declare global {
+  interface Window {
+    TDS_DID_SUBSCRIBE_DARK_BG?: boolean;
+    TDS_DARK_BG?: boolean;
+  }
+}
+
+// Below is some hacky code that changes selected background when the theme changes
+// Inspiration links:
+// https://github.com/storybookjs/storybook/issues/12982#issuecomment-865304427
+// https://github.com/storybookjs/storybook/blob/9eeb6ddfdd09a64c554843380187d27a41a8a235/addons/backgrounds/src/containers/BackgroundSelector.tsx#L100
+// https://github.com/hipstersmoothie/storybook-dark-mode/blob/bdb64ee8302bb95c23ebc2709ed3fe88431072f3/src/index.tsx
+if (!window.TDS_DID_SUBSCRIBE_DARK_BG) {
+  window.TDS_DID_SUBSCRIBE_DARK_BG = true;
+
+  // const darkModeBgColor = parameters.backgrounds.values.find(
+  //   ({ name }) => name === 'grey-958',
+  // ).value;
+  // const lightModeBgColor = parameters.backgrounds.values.find(
+  //   ({ name }) => name === 'grey-50',
+  // ).value;
+  const darkModeBgColor = '#0d0f13';
+  const lightModeBgColor = '#F9FAFB';
+
+  const channel = addons.getChannel();
+  channel.emit(UPDATE_GLOBALS, {
+    globals: { backgrounds: { value: lightModeBgColor } },
+  });
+  channel.on('DARK_MODE', (isDarkMode) => {
+    if ((isDarkMode && !window.TDS_DARK_BG) || (!isDarkMode && window.TDS_DARK_BG)) {
+      channel.emit(UPDATE_GLOBALS, {
+        globals: {
+          backgrounds: {
+            value: isDarkMode ? darkModeBgColor : lightModeBgColor,
+          },
+        },
+      });
+      window.TDS_DARK_BG = isDarkMode;
+    }
+  });
+}
 
 channel.on('DARK_MODE', (isDarkMode) => {
   document.body.classList.remove('tds-mode-light', 'tds-mode-dark');
@@ -123,6 +167,27 @@ const preview: Preview = {
       darkClass: 'tds-mode-dark',
       light: ScaniaLight,
       lightClass: 'tds-mode-light',
+    },
+    backgrounds: {
+      default: 'white',
+      values: [
+        {
+          name: 'grey-50',
+          value: '#F9FAFB',
+        },
+        {
+          name: 'white',
+          value: '#FFFFFF',
+        },
+        {
+          name: 'grey-900',
+          value: '#1d2229',
+        },
+        {
+          name: 'grey-958',
+          value: '#0d0f13',
+        },
+      ],
     },
     options: {
       showPanel: true,
