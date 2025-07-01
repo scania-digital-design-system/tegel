4b04c5b621b1bd2c39e11291019ba1777fc3d208
mistermalm <45939401+mistermalm@users.noreply.github.com>
2025-07-01 09:10:13 +0200

    fix(dropdown) implementation of new variable tokens (#1266)
    * refactor(dropdown): fonts
    
    * fix(dropdown): updated tokens in vars files
    
    * fix(dropdown): updated tokens
    
    * fix(dropdown): error token and disable state for icons
    
    * fix(dropdown): renaming tokens
    
    * fix(dropdown): cleaned up tokens
    
    * fix(dropdown): option background
    
    * fix(dropdown): removed unnecessary :root selector
    
    * fix(dropdown): box-shadow
    
    * fix(dropdown): clean up
    
    * fix(dropdown): formatting
    
    * fix(dropdown): adde more space for focused border
    
    * fix(dropdown): updated filter
    
    * fix(dropdown): formatting
    
    * fix(dropdown): formatting
    
    * fix(dropdown): hardcoded color
    
    * fix(dropdown): remove hardcoded value
    
    * fix(dropdown): added active state colors
    
    * fix(dropdown): error message + border thickness
    
    * fix(dropdown): error border thickness traton
    
    * fix(dropdown): background color table footer dd
    
    * fix(dropdown): correct background color dropdown hover
    
    * fix(dropdown): correct focus color
    
    * fix(dropdown): error message stories control heading
    
    * Update packages/core/src/components/dropdown/dropdown-vars.scss
    
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>
    
    * fix(dropdown): update font
    
    * fix(dropdown): remove accidently added md files
    
    * fix(dropdown): options import updated
    
    * fix(dropdown): undo changed icon readme
    
    * fix(dropdown): undo changes popover readme
    
    * fix(dropdown): undo changes text-field readme
    
    * fix(dropdown): undo space text-field readme
    
    ---------
    
    Co-authored-by: mistermalm <jonatan.malm@live.se>
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

--- packages/core/src/components/dropdown/dropdown.tsx
+++ packages/core/src/components/dropdown/dropdown.tsx
@@ -720,10 +720,11 @@ export class TdsDropdown {
                 class={{
                   'clear-icon': true,
                   'hide': !(this.open && this.inputElement.value !== ''),
+                  'disabled': this.disabled
                 }}
                 name="cross"
                 size="16px"
-              ></tds-icon>
+              />
               <tds-icon
                 tdsAriaHidden
                 role="button"
@@ -735,10 +736,10 @@ export class TdsDropdown {
                     this.handleToggleOpen();
                   }
                 }}
-                class={`menu-icon ${this.open ? 'open' : 'closed'}`}
+                class={`menu-icon ${this.open ? 'open' : 'closed'} ${this.disabled ? 'disabled' : ''}`}
                 name="chevron_down"
                 size="16px"
-              ></tds-icon>
+              />
             </div>
           ) : (
             <button
@@ -783,10 +784,10 @@ export class TdsDropdown {
                 <tds-icon
                   aria-label="Open/Close dropdown"
                   svgTitle="Open/Close dropdown"
-                  class={`menu-icon ${this.open ? 'open' : 'closed'}`}
+                  class={`menu-icon ${this.open ? 'open' : 'closed'} ${this.disabled ? 'disabled' : ''}`}
                   name="chevron_down"
                   size="16px"
-                ></tds-icon>
+                />
               </div>
             </button>
           )}
@@ -828,7 +829,7 @@ export class TdsDropdown {
               disabled: this.disabled,
             }}
           >
-            {this.error && <tds-icon name="error" size="16px"></tds-icon>}
+            {this.error && <tds-icon name="error" size="16px"/>}
             {this.helper}
           </div>
         )}
