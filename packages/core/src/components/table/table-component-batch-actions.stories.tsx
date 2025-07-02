4db88d0acbaa01dbd4de2eb0007739b5567279e1
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2025-07-02 15:08:58 +0200

    fix: self closing tags

--- packages/core/src/components/table/table-component-batch-actions.stories.tsx
+++ packages/core/src/components/table/table-component-batch-actions.stories.tsx
@@ -115,8 +115,8 @@ export default {
          </span>
       </div>
       <div slot="end" class="tds-u-flex tds-u-align-items-center tds-u-h-100 tds-u-gap1"><tds-button type="ghost" size="sm">
-      <tds-icon slot="icon" class="tds-btn-icon" size="16px" name="plus"></tds-icon>
-    </tds-button><tds-button  type="primary" size="sm" text="Download"></tds-button></div>`,
+      <tds-icon slot="icon" class="tds-btn-icon" size="16px" name="plus" />
+    </tds-button><tds-button  type="primary" size="sm" text="Download" /></div>`,
     ),
     verticalDivider: false,
     noMinWidth: false,
