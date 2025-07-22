47987585e154e169959146117ab6f745e2c6cf8f
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2025-07-22 11:44:50 +0200

    feat(divider): a11y color update and variant prop

--- packages/core/src/components/divider/divider.tsx
+++ packages/core/src/components/divider/divider.tsx
@@ -9,13 +9,27 @@ export class Divider {
   /** Orientation of the Divider, horizontal if not specified. */
   @Prop() orientation: 'horizontal' | 'vertical' = 'horizontal';
 
+  /** Variant of the Divider, subtle if not specified. */
+  @Prop() variant: 'discrete' | 'subtle' | 'soft' | 'defined' | 'dark-blue' = 'subtle';
+
   render() {
     return (
       <Host
         role="separator"
         aria-orientation={this.orientation === 'vertical' ? 'vertical' : undefined}
       >
-        <div class={`divider ${this.orientation}`} />
+        <div
+          class={{
+            'divider': true,
+            'vertical': this.orientation === 'vertical',
+            'horizontal': this.orientation === 'horizontal',
+            'discrete': this.variant === 'discrete',
+            'subtle': this.variant === 'subtle',
+            'soft': this.variant === 'soft',
+            'defined': this.variant === 'defined',
+            'dark-blue': this.variant === 'dark-blue',
+          }}
+        />
       </Host>
     );
   }
