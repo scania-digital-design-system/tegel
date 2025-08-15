b1dceeb4cd66bd06c7dd756a09dc633d499c8321
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-08-15 18:38:38 +0200

    feat(side-menu): add multibrand tokens

--- packages/core/src/components/icon/icon.tsx
+++ packages/core/src/components/icon/icon.tsx
@@ -26,7 +26,7 @@ export class Icon {
   @Prop({ reflect: true }) name: IconNames = 'truck';
 
   /** Pass a size of icon as a string, for example, 32px, 1rem, 4em... */
-  @Prop({ reflect: true }) size: string = '16px';
+  @Prop({ reflect: true }) size: string;
 
   /** Override the default title for the svg. Also used by aria-labelledby. */
   @Prop() svgTitle?: string;
@@ -42,10 +42,27 @@ export class Icon {
   @State() arrayOfIcons = [];
 
   componentWillLoad() {
-    this.detectAndSetBrand();
+    const brand = this.detectBrand();
+    this.setBrand(brand);
+
+    if (!this.size) {
+      const insideSideMenu = this.host.closest('tds-side-menu');
+
+      if (insideSideMenu) {
+        if (brand === 'scania') {
+          this.size = '24px';
+        } else if (brand === 'traton') {
+          this.size = '16px';
+        }
+      }
+
+      if (!this.size) {
+        this.size = '16px';
+      }
+    }
   }
 
-  private detectAndSetBrand() {
+  private detectBrand() {
     const brandClasses = Object.keys(brandIconMap);
     // First try to find brand class using closest
     let matchingBrand = brandClasses.find((brand) => this.host.closest(`.${brand}`));
@@ -57,8 +74,12 @@ export class Icon {
       );
     }
 
-    // Set the icons_object based on the found brand or default to scania
-    this.icons_object = matchingBrand ? brandIconMap[matchingBrand] : scaniaIcons;
+    return matchingBrand || 'scania'; // default to Scania if not found
+  }
+
+  private setBrand(brand) {
+    // Set the icons_object based on the brand
+    this.icons_object = brandIconMap[brand];
     this.arrayDataWatcher(this.icons_object);
   }
 
