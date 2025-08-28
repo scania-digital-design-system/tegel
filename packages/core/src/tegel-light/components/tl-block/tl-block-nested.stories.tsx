e0f5515b189fbbc49a825276dfe44d771241e134
NordbergAnna <nordberganna9@gmail.com>
2025-08-28 09:25:50 +0200

    feat(tegel-light): added tl-block component

---
+++ packages/core/src/tegel-light/components/tl-block/tl-block-nested.stories.tsx
@@ -0,0 +1,60 @@
+import formatHtmlPreview from '../../../stories/formatHtmlPreview';
+
+export default {
+  title: 'Tegel Light (CSS)/Block/Nested',
+  parameters: {
+    layout: 'padded',
+  },
+
+  argTypes: {
+    outerModeVariant: {
+      name: 'Mode variant',
+      description: 'Mode variant that should be set on the parent/app level.',
+      control: { type: 'radio' },
+      options: ['Primary', 'Secondary'],
+    },
+    componentTag: {
+      name: 'Component Tag',
+      description: 'Specifies the HTML tag utilized for the component wrapper.',
+      control: { type: 'radio' },
+      options: ['section', 'div', 'article', 'aside', 'header', 'footer', 'nav', 'main'],
+    },
+  },
+
+  args: {
+    outerModeVariant: 'Primary',
+    componentTag: 'div',
+  },
+};
+
+const Template = ({ outerModeVariant, componentTag }) =>
+  formatHtmlPreview(`
+    <!-- Required stylesheets:
+      "@scania/tegel-light/global.css"
+      "@scania/tegel-light/tl-block.css"
+    -->
+    <div class="tl-block${
+      outerModeVariant !== 'Primary' ? ` tl-block--${outerModeVariant.toLowerCase()}` : ''
+    }">
+      <div class="tl-block tl-block--even">
+        <${componentTag}>
+          <h2 class="tl-block__headline-02">Outer Block (${componentTag})</h2>
+          <p class="tl-block__body-01">This block is now structured using a <code>&lt;${componentTag}&gt;</code>.</p>
+          <div class="tl-block tl-block--odd">
+            <aside>
+              <h3 class="tl-block__headline-04">Middle Block (Aside)</h3>
+              <p class="tl-block__detail-03">Nested content inside an <code>&lt;aside&gt;</code> element.</p>
+              <div class="tl-block tl-block--even">
+                <${componentTag}>
+                  <h4 class="tl-block__headline-06">Inner Block (Section)</h4>
+                  <p class="tl-block__detail-03">Ensuring meaningful content structure with semantic HTML.</p>
+                </${componentTag}>
+              </div>
+            </aside>
+          </div>
+        </${componentTag}>
+      </div>
+    </div>
+  `);
+
+export const Nested = Template.bind({});
