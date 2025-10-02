c55cc895ce852e74295f446f46b085994e626753
Augustine Al-Zebary <augustine.x.al-zebary@scania.com>
2025-10-02 15:53:54 +0200

    feat(tegel-lite): refactored tl-header (#1453)
    * chore(tl-header): restructured files
    
    * chore(tl-header): updated tegel light components
    
    * chore(tl-header): added vars file for tl-header
    
    * chore(tl-header): updated brand symbol file
    
    * chore(tl-header): added header dropdown scss file
    
    * feat(tl-header): added header item scss file
    
    * feat(tl-header): added header title scss file
    
    * feat(tl-header): updated tl-header.scss
    
    * feat(tl-header): updated tl-header stories file
    
    * feat(tl-header): updated header.scss in tokens folder
    
    * chore(tl-header): removed old files
    
    * chore(tl-header-stories): removed questionmark from the story options
    
    * refactor(tl-header): align class names with BEM conventions
    
    * refactor(tl-header): reorganize SCSS structure into block-level partials
    
    * refactor(tl-header): aligned component to stricter BEM conventions
    
    * docs(tl-header): updated stories file
    
    * refactor(tl-header): move dropdown state handling to element-level modifiers
    
    * chore(tl-header): improved semantic html structure
    
    * fix(tl-header): adjust focus outline to render inside elements
    
    * chore(tl-header): restructure required stylesheet comments
    
    * chore(tl-header): rename list component class
    
    ---------
    
    Co-authored-by: Nathalie Lindqvist <nathalie.lindqvist@scania.com>

--- packages/core/src/tegel-light/components/tl-header/tl-header.stories.tsx
+++ packages/core/src/tegel-light/components/tl-header/tl-header.stories.tsx
@@ -1,4 +1,5 @@
 import formatHtmlPreview from '../../../stories/formatHtmlPreview';
+import { iconsNames } from '../../../components/icon/iconsArray';
 
 export default {
   title: 'Tegel Light (CSS)/Header',
@@ -6,105 +7,263 @@ export default {
     layout: 'fullscreen',
   },
   argTypes: {
-    includeTitle: { control: 'boolean', name: 'Include title?' },
-    includeHamburger: { control: 'boolean', name: 'Include hamburger?' },
-    includeBento: { control: 'boolean', name: 'Include bento?' },
-    includeBrandSymbol: { control: 'boolean', name: 'Include brand symbol?' },
-    isHamburgerActive: { control: 'boolean', name: 'Hamburger active?' },
-    isHamburgerSelected: { control: 'boolean', name: 'Hamburger selected?' },
-    isBentoActive: { control: 'boolean', name: 'Bento active?' },
-    isBentoSelected: { control: 'boolean', name: 'Bento selected?' },
+    includeTitle: { control: 'boolean', name: 'Include title' },
+    includeHamburger: { control: 'boolean', name: 'Include hamburger' },
+    includeBentoGrid: { control: 'boolean', name: 'Include bento (Grid)' },
+    includeBentoList: { control: 'boolean', name: 'Include bento (List)' },
+    includeHeaderItem: { control: 'boolean', name: 'Include header item' },
+    includeHeaderItemIconOnly: { control: 'boolean', name: 'Include header item (icon only)' },
+    icon: {
+      name: 'Icon',
+      description: 'Sets icon to be displayed on the Link.',
+      control: { type: 'select' },
+      options: iconsNames,
+      if: { arg: 'includeHeaderItemIconOnly', truthy: true },
+    },
+    includeDropdown: { control: 'boolean', name: 'Include dropdown' },
+    isDropdownOpen: {
+      control: 'boolean',
+      name: 'Dropdown open',
+      if: { arg: 'includeDropdown', truthy: true },
+    },
+    isDropdownSelected: {
+      control: 'boolean',
+      name: 'Dropdown selected',
+      if: { arg: 'includeDropdown', truthy: true },
+    },
+    includeUserProfile: { control: 'boolean', name: 'Include user profile' },
+    includeBrandSymbol: { control: 'boolean', name: 'Include brand symbol' },
+    isHamburgerPressed: {
+      control: 'boolean',
+      name: 'Hamburger pressed',
+      if: { arg: 'includeHamburger', truthy: true },
+    },
+    isHamburgerSelected: {
+      control: 'boolean',
+      name: 'Hamburger selected',
+      if: { arg: 'includeHamburger', truthy: true },
+    },
   },
   args: {
     includeTitle: false,
     includeHamburger: false,
-    isHamburgerActive: false,
+    isHamburgerPressed: false,
     isHamburgerSelected: false,
-    includeBento: false,
-    isBentoActive: false,
-    isBentoSelected: false,
+    includeBentoGrid: false,
+    includeBentoList: false,
     includeBrandSymbol: false,
+    includeDropdown: false,
+    isDropdownOpen: false,
+    isDropdownSelected: false,
+    includeUserProfile: false,
+    includeHeaderItem: false,
+    includeHeaderItemIconOnly: false,
+    icon: 'placeholder',
   },
 };
 
 const Template = ({
   includeTitle,
   includeHamburger,
-  includeBento,
+  includeBentoGrid,
+  includeBentoList,
   includeBrandSymbol,
-  isHamburgerActive,
+  isHamburgerPressed,
   isHamburgerSelected,
-  isBentoActive,
-  isBentoSelected,
+  includeDropdown,
+  isDropdownOpen,
+  isDropdownSelected,
+  includeUserProfile,
+  includeHeaderItem,
+  includeHeaderItemIconOnly,
+  icon,
 }) =>
   formatHtmlPreview(`
 
-    <!-- Required stylesheet 
-        "@scania/tegel-light/tl-header.css";
-        ${includeTitle ? '@scania/tegel-light/tl-header-title.css' : ''}
-        ${
-          includeHamburger || includeBento || includeBrandSymbol
-            ? '@scania/tegel-light/tl-header-item.css'
-            : ''
-        }
-        ${includeBrandSymbol ? '@scania/tegel-light/tl-header-brand-symbol.css' : ''}
+<!-- Required stylesheet 
+  "@scania/tegel-light/global.css";
+  "@scania/tegel-light/tl-header.css";
+  "@scania/tegel-light/tl-icon.css";
 -->
-    
-    <header class="tl-header">
-      <nav class="tl-header__nav">
-        <ul class="tl-header__component-list">
-          
-          ${
-            includeHamburger
-              ? `<li class="tl-header-item ${isHamburgerActive ? 'tl-header-item--active' : ''} ${
-                  isHamburgerSelected ? 'tl-header-item--selected' : ''
+
+<header class="tl-header">
+  <nav class="tl-header__nav">
+    <ul class="tl-header__list">
+
+      ${
+        includeHamburger
+          ? `<li class="tl-header__item ${isHamburgerPressed ? 'tl-header__item--pressed' : ''} ${
+              isHamburgerSelected ? 'tl-header__item--selected' : ''
+            }">
+              <button class="tl-header__item-wrapper">
+                <span class="tl-icon tl-icon--burger tl-icon--20" aria-hidden="true"></span>
+              </button>
+            </li>`
+          : ''
+      }
+
+      ${
+        includeTitle
+          ? `<li class="tl-header__title"><h4 class="tl-header__title-text">Application Name</h4></li>`
+          : ''
+      }
+
+      ${
+        includeHeaderItem
+          ? `<li class="tl-header__item"><button class="tl-header__item-wrapper"><span>Header item</span></button></li>`
+          : ''
+      }
+
+      ${
+        includeDropdown
+          ? `<li class="tl-header__dropdown">
+              <button class="tl-header__dropdown-wrapper ${
+                isDropdownOpen ? 'tl-header__dropdown-wrapper--open' : ''
+              } ${isDropdownSelected ? 'tl-header__dropdown-wrapper--selected' : ''}">
+                <span>Dropdown</span>
+                <div class="tl-header__dropdown-icon ${
+                  isDropdownOpen ? 'tl-header__dropdown-icon--rotated' : ''
                 }">
-                    <button class="tl-header-item__wrapper">
-                        <svg fill="none" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.97 6.998a1 1 0 0 1 1-1h22.05a1 1 0 0 1 0 2H4.97a1 1 0 0 1-1-1ZM3.97 15.982a1 1 0 0 1 1-1h22.05a1 1 0 0 1 0 2H4.97a1 1 0 0 1-1-1ZM3.97 24.966a1 1 0 0 1 1-1h22.05a1 1 0 0 1 0 2H4.97a1 1 0 0 1-1-1Z" fill="currentColor"/>
-                        </svg>
-                    </button>
-                </li>`
-              : ''
-          }
+                  <span class="tl-icon tl-icon--chevron_down tl-icon--16" aria-hidden="true"></span>
+                </div>
+              </button>
+              <ul class="tl-header__dropdown-menu ${
+                isDropdownOpen ? 'tl-header__dropdown-menu--open' : ''
+              }">
+                <li class="tl-header__dropdown-menu-item"><a href="#">Menu Item</a></li>
+                <li class="tl-header__dropdown-menu-item tl-header__dropdown-menu-item--selected"><a href="#">Selected</a></li>
+              </ul>
+            </li>`
+          : ''
+      }
 
-          
-          ${
-            includeTitle
-              ? `<div class="tl-header-title">
-              <h4 class="tl-header-title__text"> Example: default </h4>
-              </div>`
-              : ''
-          }
-            
-            <li class="tl-header__middle-spacer"></li>
-            
-            ${
-              includeBento
-                ? `<li class="tl-header-item ${isBentoActive ? 'tl-header-item--active' : ''} ${
-                    isBentoSelected ? 'tl-header-item--selected' : ''
-                  }">
-                <button class="tl-header-item__wrapper">
-                      <svg width="20px" height="20px" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                      <path d="M3.98 5.978a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM3.98 16a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM3.98 26.02a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM14 5.978a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM14 16a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM14 26.02a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM24.019 5.978a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM24.019 16a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM24.019 26.02a2 2 0 1 0 4 0 2 2 0 0 0-4 0Z" fill="currentColor"/>
-                      </svg>
-                    </button>
-                </li>`
-                : ''
-            }
+      <li class="tl-header__middle-spacer"></li>
 
-            ${
-              includeBrandSymbol
-                ? `<li class="tl-header-item">
-                      <a class="tl-header-item__wrapper">
-                        <div class="tl-header-brand-symbol"></div>
-                    </a>
-                </li>`
-                : ''
-            }
-            </ul>
-      </nav>
-    </header>
-  `);
+      ${
+        includeHeaderItemIconOnly
+          ? `<li class="tl-header__item">
+              <button class="tl-header__item-wrapper">
+                <span class="tl-icon tl-icon--${icon} tl-icon--16" aria-hidden="true"></span>
+              </button>
+            </li>`
+          : ''
+      }
+
+      ${
+        includeBentoGrid
+          ? `<li class="tl-header__dropdown" style="display: block;">
+              <button class="tl-header__dropdown-wrapper">
+                <span class="tl-icon tl-icon--bento tl-icon--16"></span>
+              </button>
+              <ul class="tl-header__dropdown-menu tl-header__dropdown-menu--launcher-grid">
+                <li class="tl-header__dropdown-menu-launcher-title">title</li>
+                <ul class="tl-header__dropdown-menu-launcher">
+                  <li class="tl-header__dropdown-menu-launcher-item"><a href="#"><span class="tl-icon tl-icon--profile tl-icon--32"></span>My Profile</a></li>
+                  <li class="tl-header__dropdown-menu-launcher-item"><a href="#"><span class="tl-icon tl-icon--settings tl-icon--32"></span>My Settings</a></li>
+                  <li class="tl-header__dropdown-menu-launcher-item"><a href="#"><span class="tl-icon tl-icon--message tl-icon--32"></span>Support</a></li>
+                  <li class="tl-header__dropdown-menu-launcher-item"><a href="#"><span class="tl-icon tl-icon--truck tl-icon--32"></span>My Truck</a></li>
+                </ul>
+              </ul>
+            </li>`
+          : ''
+      }
+
+      ${
+        includeBentoList
+          ? `<li class="tl-header__dropdown" style="display: block;">
+              <button class="tl-header__dropdown-wrapper">
+                <span class="tl-icon tl-icon--bento tl-icon--16"></span>
+              </button>
+              <ul class="tl-header__dropdown-menu tl-header__dropdown-menu--launcher-list">
+                <li class="tl-header__dropdown-menu-launcher-title">title</li>
+                <li class="tl-header__dropdown-menu-item tl-header__dropdown-menu-item--selected"><a href="#">My Profile</a></li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">My settings</a></li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">Support</a></li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">My truck</a></li>
+              </ul>
+            </li>`
+          : ''
+      }
+
+      ${
+        includeUserProfile
+          ? `<li class="tl-header__dropdown">
+              <button class="tl-header__dropdown-wrapper tl-header__dropdown-wrapper--user">
+                <div class="tl-header__dropdown-menu-user-image">
+                  <img src="https://www.svgrepo.com/show/384676/account-avatar-profile-user-6.svg" alt="User avatar" />
+                </div>
+              </button>
+              <ul class="tl-header__dropdown-menu tl-header__dropdown-menu--user">
+                <li class="tl-header__dropdown-menu-user">
+                  <div class="tl-header__dropdown-menu-user-box">
+                    <div class="tl-header__dropdown-menu-user-content">
+                      <span class="tl-header__dropdown-menu-user-header">User Name</span>
+                      <span class="tl-header__dropdown-menu-user-subheader">user@example.com</span>
+                    </div>
+                  </div>
+                </li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">My Profile</a></li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">My Settings</a></li>
+                <li class="tl-header__dropdown-menu-item"><a href="#">Log out</a></li>
+              </ul>
+            </li>`
+          : ''
+      }
+
+      ${
+        includeBrandSymbol
+          ? `<li class="tl-header__item"><a class="tl-header__item-wrapper"><div class="tl-header__brand-symbol"></div></a></li>`
+          : ''
+      }
+
+    </ul>
+  </nav>
+</header>
+
+    <!-- The script below is just for demo purposes -->
+        <script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.tl-header__dropdown');
+
+    dropdowns.forEach(dropdown => {
+      const wrapper = dropdown.querySelector('.tl-header__dropdown-wrapper');
+      const menu = dropdown.querySelector('.tl-header__dropdown-menu');
+      const icon = dropdown.querySelector('.tl-header__dropdown-icon');
+
+      wrapper.addEventListener('click', (e) => {
+        e.stopPropagation();
+        const isOpen = wrapper.classList.contains('tl-header__dropdown-wrapper--open');
+
+        // close all dropdowns
+        document.querySelectorAll('.tl-header__dropdown-wrapper--open').forEach(w => w.classList.remove('tl-header__dropdown-wrapper--open'));
+        document.querySelectorAll('.tl-header__dropdown-menu--open').forEach(m => m.classList.remove('tl-header__dropdown-menu--open'));
+        document.querySelectorAll('.tl-header__dropdown-icon--rotated').forEach(i => i.classList.remove('tl-header__dropdown-icon--rotated'));
+
+        if (!isOpen) {
+          wrapper.classList.add('tl-header__dropdown-wrapper--open');
+          menu.classList.add('tl-header__dropdown-menu--open');
+          icon.classList.add('tl-header__dropdown-icon--rotated');
+        }
+      });
+    });
+
+    // close on outside click
+    document.addEventListener('click', () => {
+      document.querySelectorAll('.tl-header__dropdown-wrapper--open').forEach(w => w.classList.remove('tl-header__dropdown-wrapper--open'));
+      document.querySelectorAll('.tl-header__dropdown-menu--open').forEach(m => m.classList.remove('tl-header__dropdown-menu--open'));
+      document.querySelectorAll('.tl-header__dropdown-icon--rotated').forEach(i => i.classList.remove('tl-header__dropdown-icon--rotated'));
+    });
+
+    // close on escape
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        document.querySelectorAll('.tl-header__dropdown-wrapper--open').forEach(w => w.classList.remove('tl-header__dropdown-wrapper--open'));
+        document.querySelectorAll('.tl-header__dropdown-menu--open').forEach(m => m.classList.remove('tl-header__dropdown-menu--open'));
+        document.querySelectorAll('.tl-header__dropdown-icon--rotated').forEach(i => i.classList.remove('tl-header__dropdown-icon--rotated'));
+      }
+    });
+  });
+</script>
+`);
 
 export const Default = Template.bind({});
