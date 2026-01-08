bd67d0e4a24322e8c92cee637b2bd4c5be38d4e7
Anton Strandqvist <anton.strandqvist@umain.com>
2026-01-08 16:13:06 +0100

    fix(icons): added icons to asset and updated gulpfile

--- icons/gulpfile.js
+++ icons/gulpfile.js
@@ -184,6 +184,9 @@ async function generateIcons() {
       ` * Do not edit directly, this file was auto-generated.`,
       ` */`,
       '',
+      `// @use '.../scania-icons-primitive' with ($local-assets: '...').`,
+      `$local-assets: './assets' !default;`,
+      '',
       ':root {',
       ...iconsNamesArray.map((icon) => {
         const safeName = icon.replace(/\s+/g, '-');
