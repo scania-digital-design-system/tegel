e995d8cca2c2b6329c315fb859e37ec7734a451d
Anton Strandqvist <anton.strandqvist@umain.com>
2026-03-26 13:17:50 +0100

    fix(script): update imports to satisfy sonarqube

--- packages/core/scripts/verify-csp.mjs
+++ packages/core/scripts/verify-csp.mjs
@@ -19,9 +19,9 @@
  * the Stencil changelog for CSS loading changes.
  */
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const currentFilePath = fileURLToPath(import.meta.url);
 const packageRoot = path.resolve(path.dirname(currentFilePath), '..');
