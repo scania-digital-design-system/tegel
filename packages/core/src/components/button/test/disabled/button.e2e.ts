987ad6f2efcc96c245b7b42c102ddfdeba4979b3
NordbergAnna <nordberganna9@gmail.com>
2026-04-09 14:40:50 +0200

    feat(variables): implement button, tag, tooltip

--- packages/core/src/components/button/test/disabled/button.e2e.ts
+++ packages/core/src/components/button/test/disabled/button.e2e.ts
@@ -10,7 +10,7 @@ const componentTestPath = 'src/components/button/test/disabled/index.html';
 const componentName = 'tds-button';
 const testDescription = 'tds-button-disabled';
 
-testConfigurations.withModeVariants.forEach((config) => {
+testConfigurations.withModeVariantsAndBrands.forEach((config) => {
   test.describe.parallel(getTestDescribeText(config, testDescription), () => {
     test.beforeEach(async ({ page }) => {
       await setupPage(page, config, componentTestPath, componentName);
