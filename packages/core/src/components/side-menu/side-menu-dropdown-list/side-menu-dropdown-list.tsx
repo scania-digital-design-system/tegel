03290e51eb387a7c451edd0d994e711e51573ef2
mfilipadneto <57566868+mfilipadneto@users.noreply.github.com>
2026-06-16 14:16:57 +0100

    Fix(side-menu): shows text when initial state is `collapsed` (#1948)
    * fix(side-menu): fix `collapsed` value not applied when starting as true
    * test(side-menu): add test to verify that `collapsed` initial value is applied
    
    ---------
    
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- packages/core/src/components/side-menu/side-menu-dropdown-list/side-menu-dropdown-list.tsx
+++ packages/core/src/components/side-menu/side-menu-dropdown-list/side-menu-dropdown-list.tsx
@@ -21,7 +21,7 @@ export class TdsSideMenuDropdownList {
     this.collapsed = event.detail.collapsed;
   }
 
-  connectedCallback() {
+  componentWillLoad() {
     this.sideMenuEl = this.host.closest('tds-side-menu');
     this.collapsed = !!this.sideMenuEl?.collapsed;
   }
