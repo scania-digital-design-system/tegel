ce57bac10f9846e32a36b4bfc9646efc6d2e2c18
Filipa Neto <filipa.neto@scania.com>
2026-07-14 09:11:06 +0100

    test(variables): update snapshots for Tegel side menu component tests
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- packages/core/src/components/side-menu/test/collapse-true/side-menu.e2e.ts
+++ packages/core/src/components/side-menu/test/collapse-true/side-menu.e2e.ts
@@ -10,7 +10,7 @@ const componentTestPath = 'src/components/side-menu/test/collapse-true/index.htm
 const componentName = 'tds-side-menu';
 const testDescription = 'tds-side-menu-collapse-true';
 
-testConfigurations.basic.forEach((config) => {
+testConfigurations.basicWithBrandVariants.forEach((config) => {
   test.describe.parallel(getTestDescribeText(config, testDescription), () => {
     test.beforeEach(async ({ page }) => {
       await setupPage(page, config, componentTestPath, componentName);
