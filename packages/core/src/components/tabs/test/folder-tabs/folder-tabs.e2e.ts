78daab00c38c010aa172168424c3d57bf1ccf8d5
Filipa Neto <filipa.neto@scania.com>
2026-08-10 15:32:42 +0100

    test(tds-folder-tabs): regenerate snapshots
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- packages/core/src/components/tabs/test/folder-tabs/folder-tabs.e2e.ts
+++ packages/core/src/components/tabs/test/folder-tabs/folder-tabs.e2e.ts
@@ -9,7 +9,7 @@ import {
 const componentTestPath = 'src/components/tabs/test/folder-tabs/index.html';
 const componentName = 'tds-folder-tabs';
 
-testConfigurations.withModeVariants.forEach((config) => {
+testConfigurations.withModeVariantsAndBrands.forEach((config) => {
   test.describe.parallel(getTestDescribeText(config, componentName), () => {
     let folderTabs;
     let firstTabDiv;
