7f7b2090c28d38dd1c9a5628bbe41a28df7511bb
Filipa Neto <filipa.neto@scania.com>
2026-08-11 10:12:31 +0100

    fix(tds-tabs): prevent horizontal scroll from jumping right to last/first tabs
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- packages/core/src/components/tabs/navigation-tabs/navigation-tabs.tsx
+++ packages/core/src/components/tabs/navigation-tabs/navigation-tabs.tsx
@@ -109,7 +109,7 @@ export class TdsNavigationTabs {
     if (!this.navWrapperElement) return;
 
     const scroll = this.navWrapperElement.scrollLeft;
-    this.navWrapperElement.scrollLeft = scroll + this.buttonsWidth;
+    this.navWrapperElement.scrollLeft = scroll + this.componentWidth / 2;
     this.evaluateScrollButtons();
   }
 
@@ -117,7 +117,7 @@ export class TdsNavigationTabs {
     if (!this.navWrapperElement) return;
 
     const scroll = this.navWrapperElement.scrollLeft;
-    this.navWrapperElement.scrollLeft = scroll - this.buttonsWidth;
+    this.navWrapperElement.scrollLeft = scroll - this.componentWidth / 2;
     this.evaluateScrollButtons();
   }
 
