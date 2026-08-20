2a3d05cdae3ed02b0383b1f38020fcfbd54eef1b
Filipa Neto <filipa.neto@scania.com>
2026-08-20 15:36:19 +0100

    test(tds-dropdown): regenerate snapshots considering the brands
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- packages/core/src/components/dropdown/test/basic/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/basic/dropdown.e2e.ts
@@ -10,7 +10,7 @@ const componentTestPath = 'src/components/dropdown/test/basic/index.html';
 const componentName = 'tds-dropdown';
 const testDescription = 'tds-dropdown-basic';
 
-testConfigurations.withModeVariants.forEach((config) => {
+testConfigurations.withModeVariantsAndBrands.forEach((config) => {
   test.describe.parallel(getTestDescribeText(config, testDescription), () => {
     test.beforeEach(async ({ page }) => {
       await setupPage(page, config, componentTestPath, componentName);
--- packages/core/src/components/dropdown/test/filter/normalize-text-false/dropdown.e2e.ts
+++ packages/core/src/components/dropdown/test/filter/normalize-text-false/dropdown.e2e.ts
@@ -10,7 +10,7 @@ const componentTestPath = 'src/components/dropdown/test/filter/normalize-text-fa
 const componentName = 'tds-dropdown';
 const testDescription = 'tds-dropdown-filter';
 
-testConfigurations.withModeVariants.forEach((config) => {
+testConfigurations.withModeVariantsAndBrands.forEach((config) => {
   test.describe.parallel(getTestDescribeText(config, testDescription), () => {
     test.beforeEach(async ({ page }) => {
       await setupPage(page, config, componentTestPath, componentName);
