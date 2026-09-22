9a37853b4439fc067e1ffee41441eb882ab8ea42
João Gonçalves <joao.goncalves@vwgds.pt>
2026-09-22 10:10:00 +0100

    chore(color): use sass from pnpm catalog and fix compilation script

--- color/compile.js
+++ color/compile.js
@@ -1,24 +1,24 @@
-const sass = require("sass");
-const fs = require("fs-extra");
-const glob = require("glob");
-const path = require("path");
-const del = require("del");
-const outputFolder = "dist";
+const sass = require('sass');
+const fs = require('node:fs');
+const glob = require('glob');
+const path = require('node:path');
+const del = require('del');
+const outputFolder = 'dist';
 
-const { Bundler } = require("scss-bundle");
+const { Bundler } = require('scss-bundle');
 
 init();
 
 async function init() {
   await clean();
-  await createFolders();
-  await glob.sync("_color.scss").forEach(generateCss);
+  createFolders();
+  glob.sync('_color.scss').forEach(generateCss);
   (async () => {
     // Absolute project directory path.
-    const projectDirectory = path.resolve(__dirname, "./");
+    const projectDirectory = path.resolve(__dirname, './');
     const bundler = new Bundler(undefined, projectDirectory);
     // Relative file path to project directory path.
-    const result = await bundler.bundle("./_color.scss");
+    const result = await bundler.bundle('./_color.scss');
     fs.writeFileSync(`${outputFolder}/scss/color.scss`, result.bundledContent);
   })();
 
@@ -33,7 +33,7 @@ function clean() {
 
 // Setup up the folders
 function createFolders() {
-  console.log("creating folders...");
+  console.log('creating folders...');
   fs.mkdirSync(outputFolder);
   fs.mkdirSync(`${outputFolder}/css`);
   fs.mkdirSync(`${outputFolder}/scss`);
@@ -43,15 +43,12 @@ function createFolders() {
 function generateCss(file) {
   const name = path.parse(file).name;
   console.log(`file: ${file}`);
-  const data = fs.readFileSync(path.resolve(file), "utf8");
+  const data = fs.readFileSync(path.resolve(file), 'utf8');
 
   const content = sass.renderSync({
     data,
   });
-  if (data !== "") {
-    fs.writeFileSync(
-      `${outputFolder}/css/${name.replace("_", "")}.css`,
-      content.css
-    );
+  if (data !== '') {
+    fs.writeFileSync(`${outputFolder}/css/${name.replace('_', '')}.css`, content.css);
   }
 }
